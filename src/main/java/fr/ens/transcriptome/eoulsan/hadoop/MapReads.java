1f88a715218fd0b71b0336e9fb4209d92c9e3a0d
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-03-01 18:03:07 +0000

    Fix MapReads to concat in one time all the unmap files.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1276 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/hadoop/MapReads.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/hadoop/MapReads.java
@@ -24,12 +24,11 @@ package fr.ens.transcriptome.eoulsan.hadoop;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -52,8 +51,11 @@ public class MapReads {
       ".map.aln.temp.dir";
   private static final String UNMAP_READS_OUTPUT_DIR_EXTENSION =
       ".map.umap.temp.dir";
+  private static final String UNMAP_CHUNK_PREFIX = "soap-unmap-";
 
   public static void main(final String[] args) throws Exception {
+
+    System.out.println("MapReads arguments:\t" + Arrays.toString(args));
 
     if (args == null)
       throw new NullPointerException("The arguments of import data is null");
@@ -81,17 +83,15 @@ public class MapReads {
 
     final Path soapIndexZipPath = listSoapIndexZipPathsInBasePath.get(0);
 
-    final FileSystem fs = PathUtils.getFileSystem(inputDirPath, conf);
-    final FileStatus[] filesStatus =
-        fs.listStatus(inputDirPath, new PathUtils.SuffixPathFilter(
-            Common.READS_FILTERED_EXTENSION));
+    final List<Path> inputFilePaths =
+        PathUtils.listPathsBySuffix(inputDirPath,
+            Common.READS_FILTERED_EXTENSION, conf);
 
     final List<Job> jobs = new ArrayList<Job>();
 
     // Run the filter
-    for (FileStatus fst : filesStatus) {
+    for (Path inputPath : inputFilePaths) {
 
-      final Path inputPath = fst.getPath();
       final Path outputPath =
           PathUtils.newPathWithOtherExtension(inputPath,
               MAP_READS_OUTPUT_DIR_EXTENSION);
@@ -103,8 +103,8 @@ public class MapReads {
     MapReduceUtils.submitandWaitForJobs(jobs, Common.CHECK_COMPLETION_TIME);
 
     // Rename the outputs of map reduce
-    for (FileStatus fst : filesStatus) {
-      final Path inputPath = fst.getPath();
+    for (Path inputPath : inputFilePaths) {
+
       final Path outputPath =
           PathUtils.newPathWithOtherExtension(inputPath,
               MAP_READS_OUTPUT_DIR_EXTENSION);
@@ -119,15 +119,26 @@ public class MapReads {
     // Concat all unmap file into one at the root base dir
     //
 
-    // The list of unmap files
-    final List<Path> unmapPaths =
-        PathUtils.listPathsBySuffix(inputDirPath, Common.UNMAP_EXTENSION, conf);
+    // The list of unmap chunk files
+    final List<Path> unmapPaths = new ArrayList<Path>();
+    for (Path inputPath : inputFilePaths) {
+
+      unmapPaths.addAll(PathUtils.listPathsBySuffix(PathUtils
+          .newPathWithOtherExtension(inputPath,
+              UNMAP_READS_OUTPUT_DIR_EXTENSION), Common.FASTA_EXTENSION, conf));
+    }
 
     // Get the name of the genome file
     final Path genomePath = Common.getGenomeFilePath(basePath, conf);
+    final Path concatUnmapFile =
+        PathUtils.newPathWithOtherExtension(genomePath, Common.UNMAP_EXTENSION);
 
-    PathUtils.concat(unmapPaths, PathUtils.newPathWithOtherExtension(
-        genomePath, Common.UNMAP_EXTENSION), false, true, conf);
+    PathUtils.concat(unmapPaths, concatUnmapFile, true, true, conf);
+
+    // Remove unused unmaps directories
+    for (Path inputPath : inputFilePaths)
+      PathUtils.fullyDelete(PathUtils.newPathWithOtherExtension(inputPath,
+          UNMAP_READS_OUTPUT_DIR_EXTENSION), conf);
 
   }
 
@@ -149,16 +160,20 @@ public class MapReads {
     final Configuration jobConf = new Configuration(parentConf);
 
     // Force to use only one jvm by task
-    //jobConf.set("mapred.job.reuse.jvm.num.task", "1");
+    // jobConf.set("mapred.job.reuse.jvm.num.task", "1");
 
     // Set genome reference path
     jobConf.set(Globals.PARAMETER_PREFIX + ".soap.indexzipfilepath",
         soapIndexZipPath.toString());
 
     // Set unmap chuck dir path
-    jobConf.set(Globals.PARAMETER_PREFIX + ".soap.unmap.temp.dir", PathUtils
-        .newPathWithOtherExtension(inputPath, UNMAP_READS_OUTPUT_DIR_EXTENSION)
-        .toString());
+    jobConf.set(Globals.PARAMETER_PREFIX + ".soap.unmap.chunk.temp.dir",
+        PathUtils.newPathWithOtherExtension(inputPath,
+            UNMAP_READS_OUTPUT_DIR_EXTENSION).toString());
+
+    // Set unmap chuck prefix
+    jobConf.set(Globals.PARAMETER_PREFIX + ".soap.unmap.chunk.prefix",
+        UNMAP_CHUNK_PREFIX);
 
     // Set unmap output file path
     jobConf.set(Globals.PARAMETER_PREFIX + ".soap.unmap.path", PathUtils
@@ -167,10 +182,9 @@ public class MapReads {
 
     // Set the number of threads for soap
     jobConf.set(Globals.PARAMETER_PREFIX + ".soap.nb.threads", "1");
-    
-    
+
     // Debug
-    // jobConf.set("mapred.job.tracker", "local");
+    jobConf.set("mapred.job.tracker", "local");
 
     // Create the job and its name
     final Job job = new Job(jobConf, "Map reads in " + inputPath.getName());
--- src/main/java/fr/ens/transcriptome/eoulsan/hadoop/mapreads/SOAPReducer.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/hadoop/mapreads/SOAPReducer.java
@@ -24,41 +24,15 @@ package fr.ens.transcriptome.eoulsan.hadoop.mapreads;
 
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import fr.ens.transcriptome.eoulsan.Globals;
-import fr.ens.transcriptome.eoulsan.util.PathUtils;
-
 public class SOAPReducer extends Reducer<Text, Text, Text, Text> {
 
-  private Path unmapFilePath;
-  private Path unmapChunkFilesDirPath;
-
   @Override
-  protected void setup(Context context) throws IOException,
+  protected void setup(final Context context) throws IOException,
       InterruptedException {
 
-    // Get SOAP path
-    final String unmapFileUri =
-        context.getConfiguration().get(
-            Globals.PARAMETER_PREFIX + ".soap.unmap.path");
-
-    if (unmapFileUri == null)
-      throw new IOException("No path for unmap file defined");
-
-    this.unmapFilePath = new Path(unmapFileUri);
-
-    // Get unmap Files directory
-    final String unmapChunkFilesDir =
-        context.getConfiguration().get(
-            Globals.PARAMETER_PREFIX + ".soap.unmap.temp.dir");
-    if (unmapChunkFilesDir == null)
-      throw new IOException(
-          "The temporary directory path for unmap file is not set");
-    this.unmapChunkFilesDirPath = new Path(unmapChunkFilesDir);
   }
 
   @Override
@@ -76,7 +50,7 @@ public class SOAPReducer extends Reducer<Text, Text, Text, Text> {
       context.write(key, new Text("" + sum));
 
     } else if ("__COUNT_UNMAP_READS__".equals(k)) {
-      System.out.println("__COUNT_UNMAP_READS__");
+
       int sum = 0;
       for (Text t : values)
         sum += Integer.parseInt(t.toString());
@@ -85,14 +59,7 @@ public class SOAPReducer extends Reducer<Text, Text, Text, Text> {
 
     } else if ("__UNMAP_FILE__".equals(k)) {
 
-      
-      
-      System.out.println("== Merge unmap ==");
-
-      final Configuration conf = context.getConfiguration();
-      PathUtils
-          .copyMerge(this.unmapChunkFilesDirPath, this.unmapFilePath, conf);
-      PathUtils.fullyDelete(this.unmapChunkFilesDirPath, conf);
+      // Nothing to do
 
     } else
       for (Text t : values)
