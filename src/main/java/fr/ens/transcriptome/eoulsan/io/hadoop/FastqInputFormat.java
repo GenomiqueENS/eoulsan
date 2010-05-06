faec9310cd23a635f62b106794cc8742c869d0ce
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-05-06 09:18:04 +0000

    Add readers for fasta and fastq for hadoop.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1330 5a62d92b-e016-0410-9103-b89fe532bb34

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/io/hadoop/FastqInputFormat.java
@@ -0,0 +1,48 @@
+/*
+ *                      Nividic development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the microarray platform
+ * of the École Normale Supérieure and the individual authors.
+ * These should be listed in @author doc comments.
+ *
+ * For more information on the Nividic project and its aims,
+ * or to join the Nividic mailing list, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/nividic
+ *
+ */
+
+package fr.ens.transcriptome.eoulsan.io.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.InputSplit;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
+
+@SuppressWarnings("deprecation")
+public class FastqInputFormat extends
+    FileInputFormat<LongWritable, Text> {
+
+  public RecordReader<LongWritable, Text> getRecordReader(
+      InputSplit input, JobConf job, Reporter reporter)
+      throws IOException {
+
+    reporter.setStatus(input.toString());
+    return new FastqRecordReader(job, (FileSplit)input);
+  }
+  
+}
