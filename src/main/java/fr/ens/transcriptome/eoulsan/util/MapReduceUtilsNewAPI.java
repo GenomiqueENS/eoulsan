3d9aaab4a6dc00d7716482941ea3946eaff16b28
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-05-06 08:55:19 +0000

    MapReduceUtils is now for the old Hadoop map-reduce API MapReduceUtilsNewApi is for the new hadoop API
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1323 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/util/MapReduceUtilsNewAPI.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/util/MapReduceUtilsNewAPI.java
@@ -24,26 +24,14 @@ package fr.ens.transcriptome.eoulsan.util;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.RunningJob;
-import org.apache.hadoop.mapred.Counters.Counter;
-import org.apache.hadoop.mapred.Counters.Group;
+import org.apache.hadoop.mapreduce.Job;
 
-/**
- * This class provide utility methods to run map reduce jobs.
- * @author Laurent Jourdren
- */
-@SuppressWarnings("deprecation")
-public class MapReduceUtils {
+public final class MapReduceUtilsNewAPI {
 
   /**
    * Move the content of the result of a map reduce into a new place
@@ -97,25 +85,17 @@ public class MapReduceUtils {
    * @throws ClassNotFoundException if a class needed for map reduce execution
    *           is not found
    */
-  public static String submitandWaitForJobs(final Collection<JobConf> jobconfs,
-      final int waitTimeInMillis, final String counterGroup)
-      throws IOException, InterruptedException, ClassNotFoundException {
+  public static void submitandWaitForJobs(final Collection<Job> jobs,
+      final int waitTimeInMillis) throws IOException, InterruptedException,
+      ClassNotFoundException {
 
-    if (jobconfs == null)
+    if (jobs == null)
       throw new NullPointerException("The list of jobs is null");
 
-    final Set<RunningJob> jobs = new HashSet<RunningJob>();
-    JobClient jc = null;
+    for (Job j : jobs)
+      j.submit();
 
-    for (JobConf jconf : jobconfs) {
-      if (jconf == null)
-        continue;
-      if (jc == null)
-        jc = new JobClient(jconf);
-      jobs.add(jc.submitJob(jconf));
-    }
-
-    return waitForJobs(jobs, waitTimeInMillis, counterGroup);
+    waitForJobs(jobs, waitTimeInMillis);
   }
 
   /**
@@ -128,9 +108,8 @@ public class MapReduceUtils {
    * @throws ClassNotFoundException if a class needed for map reduce execution
    *           is not found
    */
-  public static String waitForJobs(final Collection<RunningJob> jobs,
-      final int waitTimeInMillis, final String counterGroup)
-      throws InterruptedException, IOException {
+  public static void waitForJobs(final Collection<Job> jobs,
+      final int waitTimeInMillis) throws InterruptedException, IOException {
 
     if (jobs == null)
       throw new NullPointerException("The list of jobs is null");
@@ -138,52 +117,21 @@ public class MapReduceUtils {
     final int totalJobs = jobs.size();
     int completedJobs = 0;
 
-    final StringBuilder sb = new StringBuilder();
-    final Set<RunningJob> completedJobsSet = new HashSet<RunningJob>();
-
     while (completedJobs != totalJobs) {
 
       Thread.sleep(waitTimeInMillis);
       completedJobs = 0;
-      for (RunningJob j : jobs) {
-        if (j.isComplete()) {
+      for (Job j : jobs)
+        if (j.isComplete())
           completedJobs++;
-
-          if (counterGroup != null && !completedJobsSet.contains(j)) {
-            sb.append(j.getJobName());
-            sb.append('\n');
-
-            final Group g = j.getCounters().getGroup(counterGroup);
-            if (g != null) {
-              final Iterator<Counter> it = g.iterator();
-              while (it.hasNext()) {
-
-                Counter counter = it.next();
-                sb.append('\t');
-                sb.append(counter.getName());
-                sb.append('=');
-                sb.append(counter.getValue());
-                sb.append('\n');
-
-              }
-
-            }
-
-          }
-
-          completedJobsSet.add(j);
-        }
-
-      }
     }
-    return sb.toString();
   }
 
   //
   // Constructor
   //
 
-  private MapReduceUtils() {
+  private MapReduceUtilsNewAPI() {
   }
 
 }
