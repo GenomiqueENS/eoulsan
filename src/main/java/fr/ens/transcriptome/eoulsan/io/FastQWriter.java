d74f076efb384bbcb940e73e247a3e45ed9bd7c3
Laurent Jourdren <jourdren@biologie.ens.fr>
2010-06-02 08:29:33 +0000

    Add new FastQ reader and writer.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1346 5a62d92b-e016-0410-9103-b89fe532bb34

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/io/FastQWriter.java
@@ -0,0 +1,102 @@
+/*
+ *                      Nividic development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the microarray platform
+ * of the École Normale Supérieure and the individual authors.
+ * These should be listed in @author doc comments.
+ *
+ * For more information on the Nividic project and its aims,
+ * or to join the Nividic mailing list, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/nividic
+ *
+ */
+
+package fr.ens.transcriptome.eoulsan.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Writer;
+
+import fr.ens.transcriptome.eoulsan.core.ReadSequence;
+import fr.ens.transcriptome.eoulsan.util.FileUtils;
+
+/**
+ * this class implements a FastQ writer.
+ * @author Laurent Jourdren
+ */
+public class FastQWriter extends ReadSequence {
+
+  private Writer writer;
+
+  /**
+   * Write the current entry.
+   * @throws IOException if an error occurs while writing data
+   */
+  public void write() throws IOException {
+
+    this.writer.write(toFastQ());
+  }
+
+  /**
+   * Close the writer
+   */
+  public void close() {
+
+    this.close();
+  }
+
+  //
+  // Constructors
+  //
+
+  /**
+   * Public constructor.
+   * @param writer Writer to use
+   */
+  public FastQWriter(final Writer writer) {
+
+    if (writer == null)
+      throw new NullPointerException("The writer is null.");
+
+    this.writer = writer;
+  }
+
+  /**
+   * Public constructor.
+   * @param os OutputStream to use
+   */
+  public FastQWriter(final OutputStream os) throws FileNotFoundException {
+
+    this.writer = FileUtils.createBufferedWriter(os);
+  }
+
+  /**
+   * Public constructor.
+   * @param outputFile file to use
+   */
+  public FastQWriter(final File outputFile) throws FileNotFoundException {
+
+    this.writer = FileUtils.createBufferedWriter(outputFile);
+  }
+
+  /**
+   * Public constructor.
+   * @param outputFilename name of the file to use
+   */
+  public FastQWriter(final String outputFilename) throws FileNotFoundException {
+
+    this.writer = FileUtils.createBufferedWriter(outputFilename);
+  }
+
+}
