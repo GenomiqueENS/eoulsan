f3bfe37dbb433a21d810c82d5eac4f04220f95c4
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-06-09 17:17:19 +0000

    Add differential analysis
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1354 5a62d92b-e016-0410-9103-b89fe532bb34

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/programs/anadiff/local/AnaDiffLocalMain.java
@@ -0,0 +1,162 @@
+/*
+ *                      Nividic development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the microarray platform
+ * of the École Normale Supérieure and the individual authors.
+ * These should be listed in @author doc comments.
+ *
+ * For more information on the Nividic project and its aims,
+ * or to join the Nividic mailing list, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/nividic
+ *
+ */
+
+package fr.ens.transcriptome.eoulsan.programs.anadiff.local;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import fr.ens.transcriptome.eoulsan.Common;
+import fr.ens.transcriptome.eoulsan.EoulsanException;
+import fr.ens.transcriptome.eoulsan.Globals;
+import fr.ens.transcriptome.eoulsan.MainCLI;
+import fr.ens.transcriptome.eoulsan.design.Design;
+import fr.ens.transcriptome.eoulsan.io.DesignReader;
+import fr.ens.transcriptome.eoulsan.io.EoulsanIOException;
+import fr.ens.transcriptome.eoulsan.io.SimpleDesignReader;
+import fr.ens.transcriptome.eoulsan.programs.anadiff.AnaDiff;
+
+public class AnaDiffLocalMain {
+
+  public static String PROGRAM_NAME = "anadiff";
+
+  private static void anaDiff(final String designFilename) {
+
+    try {
+      final long startTime = System.currentTimeMillis();
+      final StringBuilder log = new StringBuilder();
+
+      final DesignReader dr = new SimpleDesignReader(designFilename);
+      final Design design = dr.read();
+
+      final AnaDiff ad = new AnaDiff(design, new File("."));
+      ad.run();
+
+      // Write log file
+      Common.writeLog(new File("anadiff.log"), startTime, log.toString());
+
+    } catch (EoulsanException e) {
+      System.err.println("Error while analysis data: " + e.getMessage());
+      System.exit(1);
+    } catch (FileNotFoundException e) {
+      System.err.println("File not found: " + e.getMessage());
+      System.exit(1);
+    } catch (IOException e) {
+      System.err.println("error while filtering: " + e.getMessage());
+      System.exit(1);
+    }
+
+  }
+
+  /**
+   * Show command line help.
+   * @param options Options of the software
+   */
+  private static void help(final Options options) {
+
+    // Show help message
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp(Globals.APP_NAME_LOWER_CASE
+        + " [options] design [genome [genome_masked [output_dir]]]", options);
+
+    System.exit(0);
+  }
+
+  /**
+   * Create options for command line
+   * @return an Options object
+   */
+  private static Options makeOptions() {
+
+    // create Options object
+    final Options options = new Options();
+
+    options.addOption("version", false, "show version of the software");
+    options
+        .addOption("about", false, "display information about this software");
+    options.addOption("h", "help", false, "display this help");
+    options.addOption("license", false,
+        "display information about the license of this software");
+
+    return options;
+  }
+
+  /**
+   * Parse the options of the command line
+   * @param args command line arguments
+   * @return the number of optional arguments
+   */
+  private static int parseCommandLine(final String args[]) {
+
+    final Options options = makeOptions();
+    final CommandLineParser parser = new GnuParser();
+
+    int argsOptions = 0;
+
+    try {
+
+      // parse the command line arguments
+      CommandLine line = parser.parse(options, args);
+
+      if (line.hasOption("help"))
+        help(options);
+
+      if (line.hasOption("about"))
+        MainCLI.about();
+
+      if (line.hasOption("version"))
+        MainCLI.version();
+
+      if (line.hasOption("license"))
+        MainCLI.license();
+
+    } catch (ParseException e) {
+      System.err.println(e.getMessage());
+      System.exit(1);
+    }
+
+    return argsOptions;
+  }
+
+  /**
+   * Main method
+   * @param args command line arguments
+   */
+  public static void main(final String[] args) {
+
+    // Parse the command line
+    final int argsOptions = parseCommandLine(args);
+
+    final String designFilename = args[argsOptions];
+
+    anaDiff(designFilename);
+  }
+
+}
