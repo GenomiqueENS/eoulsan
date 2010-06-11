adc95c14639c546f16bdf39d68fb8fde1e7312bb
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-06-11 09:42:54 +0000

    Update anadiff.R
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1367 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/files/anadiff.R
+++ src/main/java/files/anadiff.R
@@ -32,36 +32,45 @@ norm_ss_rep = function(count,cond,condNames)
   tab = tab[rowSums(tab)>0,]
 
   #normalization thanks to the scaling factor of Anders and Hubert
-  K <- tab[apply(tab, 1, min) > 0,]
-  m <- ncol(K)
-  gmean <- (apply(K, 1, prod))^(1/m)
-  s <- NULL
-  for (j in 1:m) s <- c( s, median(K[,j]/gmean) )
-  normdeseq <- round( tab / matrix( s, nrow=nrow(tab), ncol=ncol(tab), byrow=T ) )
+#   K <- tab[apply(tab, 1, min) > 0,]
+#   m <- ncol(K)
+#   gmean <- (apply(K, 1, prod))^(1/m)
+#   s <- NULL
+#   for (j in 1:m) s <- c( s, median(K[,j]/gmean) )
+#   normdeseq <- round( tab / matrix( s, nrow=nrow(tab), ncol=ncol(tab), byrow=T ) )
 
   #return of normalization expression
-  return(normdeseq)
+#   return(normdeseq)
+  return(tab)
 }
 
 # statistical analysis of differentially expressed genes using a fisher exact test between two conditions
 # the p-values  are corrected thanks to the Benjamini&Hochberg methods
 # log2 fold change are also calculated on normalized expression values
 # input : 
-# 	* tabTest : normalized expression table (one colomne per condition, one raw per gene)
-#	*cond1, cond2: names of condition 1 and 2
-#	* out: the path to the ouput file, which will contain the result of condition 1 vs condition 2	
+#   * tabTest : normalized expression table (one colomne per condition, one raw per gene)
+#   *cond1, cond2: names of condition 1 and 2
+#   * out: the path to the ouput file, which will contain the result of condition 1 vs condition 2  
 ana_diff_ssrep = function(tabTest,cond1,cond2,out)
 {
   library(statmod)
+  library(edgeR)
 
+  tab = cbind(tabTest[,cond1],tabTest[,cond2])
+  colnames(tab) = c(cond1,cond2)
+
+  f1 <- calcNormFactors(as.matrix(tab))
+  cs <- colSums(tab)
+  sizeC1 = sum(tab[,cond1])/sqrt(f1[2])
+  sizeC2 = sum(tab[,cond2])*sqrt(f1[2])
   # calculation of p-values with a fisher exact test
-  pvaldeseq <- sage.test(tabTest[,cond1], tabTest[,cond2], n1=sum(tabTest[,cond1]), n2=sum(tabTest[,cond2]))
+  pvaldeseq <- sage.test(tabTest[,cond1], tabTest[,cond2], n1=sizeC1, n2=sizeC2)
   # correction of p-value using Benjamini&Hochberg method
   p.adjdeseq = p.adjust(pvaldeseq,"BH")
 
   # calculation of the log fold change and addition the results table
   logFC = log2(as.numeric(tabTest[,cond1])) - log2(as.numeric(tabTest[,cond2]))
-  
+   
   # creation of the results table   
   tabTest = cbind(tabTest,pvaldeseq,p.adjdeseq,logFC)
   tabTest = tabTest[order(data.frame(tabTest)$p.adjdeseq),]
@@ -70,37 +79,33 @@ ana_diff_ssrep = function(tabTest,cond1,cond2,out)
   colnames(tabTest) = c("Id",paste(cond1,"norm",sep=''),paste(cond2,"norm",sep=''),"pval","padj","logFC")
 
   # writing the results table in the file "out"
-  write.table(tabTest,out,sep='\t',row.names=F, quote=F)
+   write.table(tabTest,out,sep='\t',row.names=F, quote=F)
 }
-
 
 # Sum of technical replicat, normalization and variance estimation using a the DESeq software (Negativ Binomial model)
 # log2 fold change are also calculated on the mean expression in the condition 1 and 2 taking into account the new librairies sizes
 # input : 
-# 	* count: expression tab (one colomne per sample, with may be several sample per condition; one raw per gene)
-#	* cond : a vector of number indicating to which condition the colmnes are belonging to.
-#	* rep : a vector of charactere "T" or "B" indicating to which type of replicat the colmnes are corresponding to.
+#   * count: expression tab (one colomne per sample, with may be several sample per condition; one raw per gene)
+#   * cond : a vector of number indicating to which condition the colmnes are belonging to.
+#   * rep : a vector of charactere "T" or "B" indicating to which type of replicat the colmnes are corresponding to.
 # output:
-#	* cds: a CountDataSet object containing the counts, normalized counts and new librairies sizes
+#   * cds: a CountDataSet object containing the counts, normalized counts and new librairies sizes
 sum_rept_norm_acrepbio = function(count,cond,rep)
 {
-  
   # For each condition, sum of technical replicates and concatenation of biological replicats in a new expression table, tab
   tab = c()
   if (length(rep[which(rep == 'T')])>0)
   {
-    tab = cbind(rowSums(count[which(cond == 1 & rep =="T")]),count[which(cond == 1 & rep == "B")])
+    tab = cbind(rowSums(count[which(cond == 1 & rep == "T")]),count[which(cond == 1 & rep == "B")])
     cond2 = rep(1,dim(tab)[2])
     for (i in 2:length(unique(cond)))
     {
-      tab = cbind(tab,rowSums(count[which(cond == i & rep =="T")]),count[which(cond == i & rep == "B")])
+      tab = cbind(tab,rowSums(count[which(cond == i & rep == "T")]),count[which(cond == i & rep == "B")])
       # update of the cond vector by removing condition code of technical replicate
       cond2 = c(cond2,rep(i,dim(cbind(rowSums(count[which(cond == i & rep == "T")]),count[which(cond == i & rep == "B")]))[2]))
     }
     cond = cond2
-  }  else  {
-  tab = count
-  }
+  }  else     {tab = count}
   # deleting genes that are not expressed in any condition
   tab = tab[rowSums(tab)>0,]
 
@@ -109,8 +114,7 @@ sum_rept_norm_acrepbio = function(count,cond,rep)
   cds = estimateSizeFactors(cds)
   cds = estimateVarianceFunctions(cds)
 
-
   # return of the CountDataSet object, cds
-  return(cds)  
+  return(cds)
 }
 
