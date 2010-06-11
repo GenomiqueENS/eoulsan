4c7c41b80e9ace0d3d778d38bca201f03212b67b
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-06-11 09:42:18 +0000

    Update the shell script to launch eoulsan.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1366 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/bin/eoulsan.sh
+++ src/main/bin/eoulsan.sh
@@ -3,4 +3,32 @@
 #
 # This script set the right classpath and start the application
 #
+# Author : Laurent Jourdren
+#
 
+
+# Get the path to this script
+BASEDIR=`dirname $0`
+
+# Set the Teolenn libraries path
+LIBDIR=$BASEDIR/lib
+
+# Set the memory in MiB needed by Teolenn (only Java part, not external tools)
+# By Default 2048
+MEMORY=2048
+
+# Set the path to java
+JAVA_CMD=java
+
+LIBCP=
+for lib in `ls $LIBDIR`
+do
+    LIBCP=$LIBCP:$LIBDIR/$lib
+done
+
+
+# Launch Eoulsan
+$JAVA_CMD -server \
+          -Xmx${MEMORY}m \
+          -cp $LIBCP \
+          fr.ens.transcriptome.eoulsan.Main $*
