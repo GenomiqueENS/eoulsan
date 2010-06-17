64fafad3beaa9bba211f8d27b5cb4334b5279e85
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-06-17 16:32:26 +0000

    Now the Global class contains a list of available platform for eoulsan. Update BinariesInstaller to handle this list.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1376 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/util/BinariesInstaller.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/util/BinariesInstaller.java
@@ -24,14 +24,21 @@ package fr.ens.transcriptome.eoulsan.util;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import fr.ens.transcriptome.eoulsan.Globals;
 
+/**
+ * This class is used to install binaries bundled in the jar.
+ * @author Laurent Jourdren
+ */
 public class BinariesInstaller {
 
   private static Logger logger = Logger.getLogger(Globals.APP_NAME);
@@ -62,7 +69,7 @@ public class BinariesInstaller {
                 + outputDir.getAbsolutePath());
       FileUtils.setDirectoryWritable(outputDir, true, false);
     }
-    
+
     final File outputFile = new File(outputDir, file);
     OutputStream fos = FileUtils.createOutputStream(outputFile);
 
@@ -79,26 +86,52 @@ public class BinariesInstaller {
     FileUtils.setReadable(outputFile, true, false);
   }
 
-  public static String install(final String file) throws IOException {
+  
+  
+  /**
+   * Install a binary bundled in the jar in /tmp
+   * @param binaryFilename program to install
+   * @return a string with the path of the installed binary
+   * @throws IOException if an error occurs while installing binary
+   */
+  public static String install(final String binaryFilename) throws IOException {
 
-    final String os = System.getProperty("os.name");
-    final String arch = System.getProperty("os.arch");
+    if (!SystemUtils.isUnix())
+      throw new IOException("Can only install binaries on *nix systems.");
+    
+    String os = System.getProperty("os.name").toLowerCase();
+    String arch = System.getProperty("os.arch").toLowerCase();
 
-    logger
-        .fine("Try to install \"" + file + "\" for " + os + " (" + arch + ")");
+    logger.fine("Try to install \""
+        + binaryFilename + "\" for " + os + " (" + arch + ")");
 
-    final boolean linux = SystemUtils.isLinux();
-    final boolean macos = SystemUtils.isMacOsX();
+    final Map<String, Set<String>> archs = new HashMap<String, Set<String>>();
+    for (int i = 0; i < Globals.AVAILABLE_BINARY_ARCH.length; i++) {
 
-    if (!(linux || macos))
+      final String aOs = Globals.AVAILABLE_BINARY_ARCH[i][0];
+      final String aArch = Globals.AVAILABLE_BINARY_ARCH[i][1];
+      if (!archs.containsKey(aOs))
+        archs.put(aOs, new HashSet<String>());
+      archs.get(aOs).add(aArch);
+    }
+
+    if (!archs.containsKey(os) || !archs.get(os).contains(arch))
       throw new FileNotFoundException(
           "There is no executable for your plateform ("
               + os + ") included in " + Globals.APP_NAME);
 
-    if (!linux && !("i386".equals(arch) || "amd64".equals(arch)))
-      throw new FileNotFoundException(
-          "There is no executable for your architecture ("
-              + arch + ") included in " + Globals.APP_NAME);
+    // Change the os and arch if alias
+    for (int i = 0; i < Globals.AVAILABLE_BINARY_ARCH_ALIAS.length; i++) {
+
+      final String aOs = Globals.AVAILABLE_BINARY_ARCH_ALIAS[i][0];
+      final String aArch = Globals.AVAILABLE_BINARY_ARCH_ALIAS[i][0];
+
+      if (aOs.equals(os) && aArch.equals(aArch)) {
+        os = Globals.AVAILABLE_BINARY_ARCH_ALIAS[i][2];
+        arch = Globals.AVAILABLE_BINARY_ARCH_ALIAS[i][3];
+        break;
+      }
+    }
 
     final String inputPath = "/" + os.replace(" ", "") + "/" + arch;
 
@@ -106,16 +139,16 @@ public class BinariesInstaller {
         "/tmp/" + Globals.APP_NAME_LOWER_CASE + "/" + Globals.APP_VERSION;
 
     // Test if the file is allready installed
-    if (new File(outputPath, file).isFile()) {
-      logger.info(file + " is allready installed.");
-      return outputPath + "/" + file;
+    if (new File(outputPath, binaryFilename).isFile()) {
+      logger.info(binaryFilename + " is allready installed.");
+      return outputPath + "/" + binaryFilename;
     }
 
     // install the file
-    install(inputPath, file, outputPath);
+    install(inputPath, binaryFilename, outputPath);
 
-    logger.fine("Successful installation of " + file + " in " + outputPath);
-    return outputPath + "/" + file;
+    logger.fine("Successful installation of "
+        + binaryFilename + " in " + outputPath);
+    return outputPath + "/" + binaryFilename;
   }
-
 }
