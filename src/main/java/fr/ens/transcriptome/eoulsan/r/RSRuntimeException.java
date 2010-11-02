f9bd570adfc8581d029bd7efad462eae9dd8a723
Laurent Jourdren <jourdren@biologie.ens.fr>
2010-11-02 11:44:18 +0000

    New R package.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1552 5a62d92b-e016-0410-9103-b89fe532bb34

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/r/RSRuntimeException.java
@@ -0,0 +1,41 @@
+/*
+ *                      Nividic development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the microarray platform
+ * of the École Normale Supérieure and the individual authors.
+ * These should be listed in @author doc comments.
+ *
+ * For more information on the Nividic project and its aims,
+ * or to join the Nividic mailing list, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/nividic
+ *
+ */
+
+package fr.ens.transcriptome.eoulsan.r;
+
+import fr.ens.transcriptome.eoulsan.EoulsanRuntimeException;
+
+/**
+ * This class define a RServe RuntimeException.
+ * @author Laurent Jourdren
+ */
+public class RSRuntimeException extends EoulsanRuntimeException {
+
+  //
+  // Constructor
+  //
+
+  public RSRuntimeException(String message) {
+    super(message);
+  }
+
+}
