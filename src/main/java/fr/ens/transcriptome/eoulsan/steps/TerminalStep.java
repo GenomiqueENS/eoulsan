65c973fc567143624d85b0552a07fe2f3995fffd
Laurent Jourdren <jourdren@biologie.ens.fr>
2010-11-02 11:41:05 +0000

    Add a Terminal step.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1549 5a62d92b-e016-0410-9103-b89fe532bb34

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/steps/TerminalStep.java
@@ -0,0 +1,33 @@
+package fr.ens.transcriptome.eoulsan.steps;
+
+import fr.ens.transcriptome.eoulsan.core.AbstractStep;
+import fr.ens.transcriptome.eoulsan.core.ExecutorInfo;
+import fr.ens.transcriptome.eoulsan.core.StepResult;
+import fr.ens.transcriptome.eoulsan.design.Design;
+
+/**
+ * This class define a terminal step that do nothing. After this execution the
+ * workflow will stop.
+ * @author Laurent Jourdren
+ */
+public class TerminalStep extends AbstractStep {
+
+  @Override
+  public String getName() {
+
+    return "Terminal";
+  }
+
+  @Override
+  public StepResult execute(Design design, ExecutorInfo info) {
+
+    return new StepResult(this, System.currentTimeMillis(), "Terminal step.");
+  }
+
+  @Override
+  public boolean isTerminalStep() {
+
+    return true;
+  }
+
+}
