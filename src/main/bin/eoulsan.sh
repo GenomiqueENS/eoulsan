3267418ca04e8e5873213d8fd874fa1fb2443a5c
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-11-24 18:36:03 +0000

    Update pom.xml and eoulsan.sh for jar repackaging.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1580 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/bin/eoulsan.sh
+++ src/main/bin/eoulsan.sh
@@ -6,6 +6,19 @@
 # Author : Laurent Jourdren
 #
 
+# Function to create lib paths
+make_paths() {
+
+	local RESULT=
+	for lib in `ls $1`
+	do
+		if [ -f $1/$lib ]; then
+			RESULT=$RESULT:$1/$lib
+		fi
+	done
+
+	echo $RESULT
+}
 
 # Get the path to this script
 BASEDIR=`dirname $0`
@@ -23,15 +36,14 @@ PLUGINS=
 # Set the path to java
 JAVA_CMD=java
 
-LIBCP=
-for lib in `ls $LIBDIR`
-do
-    LIBCP=$LIBCP:$LIBDIR/$lib
-done
-
+COMMON_LIBS=$(make_paths $LIBDIR)
+LOCAL_LIBS=$(make_paths $LIBDIR/local)
+PLUGINS_LIBS=$(make_paths $EOULSAN_PLUGINS)
 
 # Launch Eoulsan
-$JAVA_CMD -server \
-          -Xmx${MEMORY}m \
-          -cp $LIBCP:$PLUGINS \
-          fr.ens.transcriptome.eoulsan.Main $*
+$JAVA_CMD \
+		-server \
+		-Xmx${MEMORY}m \
+		-cp $COMMON_LIBS:$LOCAL_LIBS:$PLUGINS:$PLUGINS_LIBS \
+		-Deoulsan.hadoop.libs=$COMMON_LIBS:$PLUGINS:$PLUGINS_LIB \
+		fr.ens.transcriptome.eoulsan.Main $*
