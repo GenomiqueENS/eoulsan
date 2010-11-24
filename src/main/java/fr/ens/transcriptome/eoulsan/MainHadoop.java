2d1fecad6042d592c85f1be23c6b6a11cdd2680c
Laurent Jourdren <jourdren@biologie.ens.fr>
2010-11-24 17:05:17 +0000

    Update: All the code of HadoopExecAction is now in MainHadoop.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1575 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/MainHadoop.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/MainHadoop.java
@@ -22,9 +22,33 @@
 
 package fr.ens.transcriptome.eoulsan;
 
-import org.apache.hadoop.util.ProgramDriver;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Set;
 
-import fr.ens.transcriptome.eoulsan.core.action.HadoopExecAction;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import fr.ens.transcriptome.eoulsan.core.Command;
+import fr.ens.transcriptome.eoulsan.core.CommonHadoop;
+import fr.ens.transcriptome.eoulsan.core.Executor;
+import fr.ens.transcriptome.eoulsan.core.HadoopAnalysisExecutor;
+import fr.ens.transcriptome.eoulsan.core.ParamParser;
+import fr.ens.transcriptome.eoulsan.core.Parameter;
+import fr.ens.transcriptome.eoulsan.core.Step;
+import fr.ens.transcriptome.eoulsan.datatypes.DataFile;
+import fr.ens.transcriptome.eoulsan.design.Design;
+import fr.ens.transcriptome.eoulsan.design.DesignUtils;
+import fr.ens.transcriptome.eoulsan.steps.mgmt.hadoop.CopyDesignAndParametersToOutputStep;
+import fr.ens.transcriptome.eoulsan.steps.mgmt.hadoop.InitGlobalLoggerStep;
+import fr.ens.transcriptome.eoulsan.steps.mgmt.newupload.HadoopUploadStep;
+import fr.ens.transcriptome.eoulsan.steps.mgmt.upload.HDFSDataDownloadStep;
+import fr.ens.transcriptome.eoulsan.util.PathUtils;
 
 /**
  * Main class in Hadoop mode.
@@ -32,28 +56,155 @@ import fr.ens.transcriptome.eoulsan.core.action.HadoopExecAction;
  */
 public final class MainHadoop {
 
+  private static final Set<Parameter> EMPTY_PARAMEMETER_SET =
+      Collections.emptySet();
+
+  /**
+   * Main method. This method is called by MainHadoop.
+   * @param args command line arguments
+   */
   public static void main(final String[] args) {
 
-    final ProgramDriver pgd = new ProgramDriver();
+    new MainHadoop().action(args);
+  }
+
+  public void action(final String[] args) {
+
+    if (args.length != 4)
+      Common.showErrorMessageAndExit("Invalid number of arguments.\n"
+          + "usage: " + Globals.APP_NAME_LOWER_CASE
+          + " exec param.xml design.txt");
+
+    if ("exec".equals(args[0])) {
+      Common.showErrorMessageAndExit("Unknow command: "
+          + args[0] + ".\n" + "usage: " + Globals.APP_NAME_LOWER_CASE
+          + " exec param.xml design.txt");
+    }
 
     try {
 
-      pgd.addClass("exec", HadoopExecAction.class, "Execute "
-          + Globals.APP_NAME);
+      // Initialize The application
+      final Configuration conf = init();
 
-      pgd.driver(args);
+      // Get the command line argumnts
+      final String paramPathname = args[1];
+      final String designPathname = args[2];
 
-    } catch (Throwable e) {
-      System.err.println(e);
-      e.printStackTrace();
+      // Define parameter URI
+      final URI paramURI;
+      if (paramPathname.indexOf("://") != -1)
+        paramURI = new URI(paramPathname);
+      else
+        paramURI = new File(paramPathname).getAbsoluteFile().toURI();
+
+      // Define design URI
+      final URI designURI;
+      if (designPathname.indexOf("://") != -1)
+        designURI = new URI(designPathname);
+      else
+        designURI = new File(designPathname).getAbsoluteFile().toURI();
+
+      // Define destination URI
+      final URI destURI = new URI(args[3]);
+
+      final Path paramPath = new Path(paramURI.toString());
+      final Path designPath = new Path(designURI.toString());
+
+      // Test if param file exists
+      FileSystem paramFs = paramPath.getFileSystem(conf);
+      if (!paramFs.exists(paramPath))
+        throw new FileNotFoundException(paramPath.toString());
+
+      // Test if design file exists
+      FileSystem designFs = designPath.getFileSystem(conf);
+      if (!designFs.exists(designPath))
+        throw new FileNotFoundException(designPath.toString());
+
+      // Read design file
+      final Design design =
+          DesignUtils.readAndCheckDesign(designFs.open(designPath));
+
+      // Create command object
+      final Command c = new Command();
+
+      // Add init global logger Step
+      c.addStep(InitGlobalLoggerStep.STEP_NAME, EMPTY_PARAMEMETER_SET);
+
+      // Add upload Step
+      // c.addStep(HDFSDataUploadStep.STEP_NAME, EMPTY_PARAMEMETER_SET);
+
+      // Add Copy design and parameter file Step
+      c.addStep(CopyDesignAndParametersToOutputStep.STEP_NAME,
+          EMPTY_PARAMEMETER_SET);
+
+      // Parse param file
+      final ParamParser pp =
+          new ParamParser(PathUtils.createInputStream(paramPath, conf));
+
+      pp.parse(c);
+
+      // Add download Step
+      c.addStep(HDFSDataDownloadStep.STEP_NAME, EMPTY_PARAMEMETER_SET);
+
+      // Execute
+      final Executor e =
+          new HadoopAnalysisExecutor(conf, c, design, designPath, paramPath);
+
+      e.execute(Collections.singletonList((Step) new HadoopUploadStep(
+          new DataFile(destURI.toString()), conf)), null);
+
+    } catch (FileNotFoundException e) {
+
+      Common.errorExit(e, "File not found: " + e.getMessage());
+
+    } catch (EoulsanException e) {
+
+      Common.errorExit(e, "Error while executing "
+          + Globals.APP_NAME_LOWER_CASE + ": " + e.getMessage());
+
+    } catch (IOException e) {
+
+      Common.errorExit(e, "Error: " + e.getMessage());
+
+    } catch (URISyntaxException e) {
+
+      Common.errorExit(e, "Error: " + e.getMessage());
     }
 
+  }
+
+  /**
+   * Configure the application.
+   * @return a Hadoop configuration object
+   * @throws EoulsanException if an error occurs while reading settings
+   */
+  private Configuration init() throws EoulsanException {
+
+    try {
+      // Create and load settings
+      final Settings settings = new Settings();
+
+      // Create Hadoop configuration object
+      final Configuration conf =
+          CommonHadoop.createConfigurationFromSettings(settings);
+
+      // Initialize runtime
+      HadoopEoulsanRuntime.newEoulsanRuntime(settings, conf);
+
+      return conf;
+    } catch (IOException e) {
+      throw new EoulsanException("Error while reading settings: "
+          + e.getMessage());
+    }
   }
 
   //
   // Constructor
   //
 
+  /**
+   * Private constructor.
+   */
   private MainHadoop() {
 
     throw new IllegalStateException();
