f1ef905eb6e089ec57ab78156b4e58d25b58e0c9
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2010-11-26 10:35:02 +0000

    Add: New protocols to handle s3/s3n (same behavior)
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1603 5a62d92b-e016-0410-9103-b89fe532bb34

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/data/protocols/S3NDataProtocol.java
@@ -0,0 +1,40 @@
+/*
+ *                      Nividic development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the microarray platform
+ * of the École Normale Supérieure and the individual authors.
+ * These should be listed in @author doc comments.
+ *
+ * For more information on the Nividic project and its aims,
+ * or to join the Nividic mailing list, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/nividic
+ *
+ */
+
+package fr.ens.transcriptome.eoulsan.data.protocols;
+
+import fr.ens.transcriptome.eoulsan.annotations.LocalOnly;
+
+/**
+ * This class define the s3n protocol in local mode.
+ * @author Laurent Jourdren
+ */
+@LocalOnly
+public class S3NDataProtocol extends S3DataProtocol {
+
+  @Override
+  public String getName() {
+
+    return "s3n";
+  }
+
+}
