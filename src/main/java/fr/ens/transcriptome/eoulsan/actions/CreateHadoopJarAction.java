8de4d691cb9de573fca1b2c44e5ffcd07c7d46c7
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2011-01-20 17:05:06 +0000

    Add: New action: createhadoopjar
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1681 5a62d92b-e016-0410-9103-b89fe532bb34

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/actions/CreateHadoopJarAction.java
@@ -0,0 +1,51 @@
+/*
+ *                      Nividic development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the microarray platform
+ * of the École Normale Supérieure and the individual authors.
+ * These should be listed in @author doc comments.
+ *
+ * For more information on the Nividic project and its aims,
+ * or to join the Nividic mailing list, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/nividic
+ *
+ */
+
+package fr.ens.transcriptome.eoulsan.actions;
+
+import java.io.IOException;
+
+import fr.ens.transcriptome.eoulsan.Common;
+import fr.ens.transcriptome.eoulsan.Globals;
+import fr.ens.transcriptome.eoulsan.util.HadoopJarRepackager;
+
+/**
+ * This class define an action to create hadoop jar file.
+ * @author Laurent Jourdren
+ */
+public class CreateHadoopJarAction implements Action {
+
+  @Override
+  public void action(final String[] arguments) {
+
+    try {
+
+      HadoopJarRepackager.repack();
+
+    } catch (IOException e) {
+      Common.errorExit(e, "Error while executing "
+          + Globals.APP_NAME_LOWER_CASE + ": " + e.getMessage());
+    }
+
+  }
+
+}
