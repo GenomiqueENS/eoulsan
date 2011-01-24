4705474f095de4c3fbc9541e5bd06cf9468c7746
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2011-01-24 21:33:37 +0000

    Update: HDFSDataDownloadStep is now configurable about the list of files to download (see the new step DefineDataFormatToDownload). By default download all generated files.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1704 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/steps/mgmt/upload/HDFSDataDownloadStep.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/steps/mgmt/upload/HDFSDataDownloadStep.java
@@ -23,14 +23,16 @@
 package fr.ens.transcriptome.eoulsan.steps.mgmt.upload;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.PathFilter;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import fr.ens.transcriptome.eoulsan.EoulsanException;
 import fr.ens.transcriptome.eoulsan.Globals;
@@ -38,9 +40,12 @@ import fr.ens.transcriptome.eoulsan.annotations.HadoopOnly;
 import fr.ens.transcriptome.eoulsan.core.CommonHadoop;
 import fr.ens.transcriptome.eoulsan.core.Context;
 import fr.ens.transcriptome.eoulsan.core.Parameter;
+import fr.ens.transcriptome.eoulsan.data.DataFile;
 import fr.ens.transcriptome.eoulsan.data.DataFormat;
-import fr.ens.transcriptome.eoulsan.data.DataFormats;
+import fr.ens.transcriptome.eoulsan.data.DataFormatRegistry;
 import fr.ens.transcriptome.eoulsan.design.Design;
+import fr.ens.transcriptome.eoulsan.design.Sample;
+import fr.ens.transcriptome.eoulsan.io.CompressionType;
 import fr.ens.transcriptome.eoulsan.steps.AbstractStep;
 import fr.ens.transcriptome.eoulsan.steps.StepResult;
 import fr.ens.transcriptome.eoulsan.util.PathUtils;
@@ -50,6 +55,13 @@ public class HDFSDataDownloadStep extends AbstractStep {
 
   // Logger
   private static Logger logger = Logger.getLogger(Globals.APP_NAME);
+
+  /**
+   * Key in the settings to use to save the list of DataFormat of the files to
+   * download.
+   */
+  public static final String DATAFORMATS_TO_DOWNLOAD_SETTING =
+      "dataformat.to.download";
 
   /** Step name. */
   public static final String STEP_NAME = "_download";
@@ -101,53 +113,58 @@ public class HDFSDataDownloadStep extends AbstractStep {
     try {
 
       final Path inPath = new Path(context.getBasePathname());
-      final Path outPath = new Path(context.getOutputPathname());
 
       if (!PathUtils.isExistingDirectoryFile(inPath, conf))
         throw new EoulsanException("The base directory is not a directory: "
             + inPath);
 
-      final FileSystem inFs = inPath.getFileSystem(conf);
-      final FileSystem outFs = outPath.getFileSystem(conf);
+      // Get the list of DataFormat to download
+      final List<DataFormat> formats = getDataFormats(context);
 
-      final FileStatus[] files = inFs.listStatus(inPath, new PathFilter() {
+      // Add files to download
+      final Map<DataFile, DataFile> files = Maps.newHashMap();
+      for (Sample sample : design.getSamples()) {
 
-        @Override
-        public boolean accept(final Path p) {
-
-          final String filename = p.getName();
-
-          final DataFormat df = DataFormats.EXPRESSION_RESULTS_TXT;
-
-          if (filename.startsWith(df.getType().getPrefix())
-              && filename.endsWith(df.getDefaultExtention()))
-            return true;
-
-          return false;
-
+        // If no DataFormat set, add all generated data
+        if (formats.size() > 0) {
+          for (DataFormat df : formats) {
+            addFile(context, df, sample, files);
+          }
+        } else {
+          for (DataFormat df : context.getWorkflow().getGlobalOutputDataFormat(
+              sample)) {
+            addFile(context, df, sample, files);
+          }
         }
-      });
+      }
 
-      // Create output path is does not exists
-      if (!outFs.exists(outPath))
-        outFs.mkdirs(outPath);
+      if (files.size() > 0) {
+        final Path jobPath =
+            PathUtils.createTempPath(new Path(context.getBasePathname()),
+                "distcp-", "", this.conf);
 
-      final StringBuilder logMsg = new StringBuilder();
+        DataSourceDistCp distCp = new DataSourceDistCp(this.conf, jobPath);
+        distCp.copy(files);
+      }
 
-      if (files != null)
-        for (FileStatus f : files) {
+      /*
+       * final FileSystem inFs = inPath.getFileSystem(conf); final FileSystem
+       * outFs = outPath.getFileSystem(conf); final FileStatus[] files =
+       * inFs.listStatus(inPath, new PathFilter() {
+       * @Override public boolean accept(final Path p) { final String filename =
+       * p.getName(); final DataFormat df = DataFormats.EXPRESSION_RESULTS_TXT;
+       * if (filename.startsWith(df.getType().getPrefix()) &&
+       * filename.endsWith(df.getDefaultExtention())) return true; return false;
+       * } }); // Create output path is does not exists if
+       * (!outFs.exists(outPath)) outFs.mkdirs(outPath); final StringBuilder
+       * logMsg = new StringBuilder(); if (files != null) for (FileStatus f :
+       * files) { final Path ip = f.getPath(); final Path op = new Path(outPath,
+       * ip.getName()); String msg = "Copy " + ip + " to " + op;
+       * logger.info(msg); logMsg.append(msg); logMsg.append("\n");
+       * PathUtils.copy(ip, op, conf); }
+       */
 
-          final Path ip = f.getPath();
-          final Path op = new Path(outPath, ip.getName());
-
-          String msg = "Copy " + ip + " to " + op;
-          logger.info(msg);
-          logMsg.append(msg);
-          logMsg.append("\n");
-          PathUtils.copy(ip, op, conf);
-        }
-
-      return new StepResult(context, startTime, logMsg.toString());
+      return new StepResult(context, startTime, files.keySet().toString());
 
     } catch (EoulsanException e) {
 
@@ -158,6 +175,69 @@ public class HDFSDataDownloadStep extends AbstractStep {
       return new StepResult(context, e, "Error while download results: "
           + e.getMessage());
     }
+  }
+
+  /**
+   * Add a file to the the list of file to download
+   * @param context current context
+   * @param df DataFormat of the file to download
+   * @param sample current sample
+   * @param files map of the files to download
+   */
+  private void addFile(final Context context, final DataFormat df,
+      final Sample sample, final Map<DataFile, DataFile> files) {
+
+    if (context == null || df == null || sample == null) {
+      return;
+    }
+
+    final DataFile inFile = context.getDataFile(df, sample);
+
+    if (!inFile.exists()) {
+      return;
+    }
+
+    final DataFile outFile =
+        new DataFile(new Path(new Path(context.getOutputPathname()),
+            inFile.getName() + CompressionType.BZIP2.getExtension()).toString());
+
+    files.put(inFile, outFile);
+  }
+
+  /**
+   * Get the list of DataFormat of the files to download
+   * @param context current context
+   * @return a list with the DataFormat of the files to download
+   */
+  private List<DataFormat> getDataFormats(final Context context) {
+
+    final List<DataFormat> result = Lists.newArrayList();
+
+    final String list =
+        context.getRuntime().getSettings()
+            .getSetting(DATAFORMATS_TO_DOWNLOAD_SETTING);
+
+    if (list == null) {
+      return result;
+    }
+
+    final String[] fields = list.split(",");
+
+    if (fields == null) {
+      return result;
+    }
+
+    final DataFormatRegistry registry = DataFormatRegistry.getInstance();
+
+    for (String dataFormatName : fields) {
+
+      final DataFormat df = registry.getDataFormat(dataFormatName);
+      if (df != null) {
+        result.add(df);
+      }
+    }
+
+    return result;
   }
 
 }
