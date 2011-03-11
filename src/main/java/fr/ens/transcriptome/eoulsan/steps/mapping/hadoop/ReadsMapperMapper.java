a5970817ffb249645a1b77c4d33ceee575d105cc
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2011-03-11 13:48:07 +0000

    TYPO: Set Copyrights.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1824 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/steps/mapping/hadoop/ReadsMapperMapper.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/steps/mapping/hadoop/ReadsMapperMapper.java
@@ -1,25 +1,26 @@
 /*
- *                      Nividic development code
+ *                  Eoulsan development code
  *
  * This code may be freely distributed and modified under the
- * terms of the GNU Lesser General Public Licence.  This should
- * be distributed with the code.  If you do not have a copy,
- * see:
+ * terms of the GNU Lesser General Public License version 2.1 or
+ * later and CeCILL-C. This should be distributed with the code.
+ * If you do not have a copy, see:
  *
- *      http://www.gnu.org/copyleft/lesser.html
+ *      http://www.gnu.org/licenses/lgpl-2.1.txt
+ *      http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt
  *
- * Copyright for this code is held jointly by the microarray platform
- * of the École Normale Supérieure and the individual authors.
- * These should be listed in @author doc comments.
+ * Copyright for this code is held jointly by the Genomic platform
+ * of the Institut de Biologie de l'École Normale Supérieure and
+ * the individual authors. These should be listed in @author doc
+ * comments.
  *
- * For more information on the Nividic project and its aims,
- * or to join the Nividic mailing list, visit the home page
+ * For more information on the Eoulsan project and its aims,
+ * or to join the Eoulsan Google group, visit the home page
  * at:
  *
- *      http://www.transcriptome.ens.fr/nividic
+ *      http://www.transcriptome.ens.fr/eoulsan
  *
  */
-
 package fr.ens.transcriptome.eoulsan.steps.mapping.hadoop;
 
 import static com.google.common.collect.Lists.newArrayList;
