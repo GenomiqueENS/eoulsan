951f4c8faf0bd377f021c7f1a3d78fc62fcb0d7b
Laurent Jourdren <jourdren@biologie.ens.fr>
2011-03-13 19:17:48 +0000

    Add: DataType and DataFormats for BAM index and filtered BAM index
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1850 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/data/DataFormats.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/data/DataFormats.java
@@ -30,83 +30,88 @@ package fr.ens.transcriptome.eoulsan.data;
  */
 public class DataFormats {
 
-  private static final DataFormatRegistry resgistry =
-      DataFormatRegistry.getInstance();
+  private static final DataFormatRegistry resgistry = DataFormatRegistry
+      .getInstance();
 
   /** Reads fastq data format. */
-  public static final DataFormat READS_FASTQ =
-      resgistry.getDataFormatFromName(ReadsFastqDataFormat.FORMAT_NAME);
+  public static final DataFormat READS_FASTQ = resgistry
+      .getDataFormatFromName(ReadsFastqDataFormat.FORMAT_NAME);
 
   /** Reads tfq data format. */
-  public static final DataFormat READS_TFQ =
-      resgistry.getDataFormatFromName(ReadsTfqDataFormat.FORMAT_NAME);
+  public static final DataFormat READS_TFQ = resgistry
+      .getDataFormatFromName(ReadsTfqDataFormat.FORMAT_NAME);
 
   /** Filtered reads fasta data format. */
-  public static final DataFormat FILTERED_READS_FASTQ =
-      resgistry.getDataFormatFromName(FilteredReadsFastqDataFormat.FORMAT_NAME);
+  public static final DataFormat FILTERED_READS_FASTQ = resgistry
+      .getDataFormatFromName(FilteredReadsFastqDataFormat.FORMAT_NAME);
 
   /** Filtered reads fasta data format. */
-  public static final DataFormat FILTERED_READS_TFQ =
-      resgistry.getDataFormatFromName(FilteredReadsTfqDataFormat.FORMAT_NAME);
+  public static final DataFormat FILTERED_READS_TFQ = resgistry
+      .getDataFormatFromName(FilteredReadsTfqDataFormat.FORMAT_NAME);
 
   /** SOAP index data format. */
-  public static final DataFormat SOAP_INDEX_ZIP =
-      resgistry.getDataFormatFromName(SOAPIndexZipDataFormat.FORMAT_NAME);
+  public static final DataFormat SOAP_INDEX_ZIP = resgistry
+      .getDataFormatFromName(SOAPIndexZipDataFormat.FORMAT_NAME);
 
   /** BWA index data format. */
-  public static final DataFormat BWA_INDEX_ZIP =
-      resgistry.getDataFormatFromName(BWAIndexZipDataFormat.FORMAT_NAME);
+  public static final DataFormat BWA_INDEX_ZIP = resgistry
+      .getDataFormatFromName(BWAIndexZipDataFormat.FORMAT_NAME);
 
   /** BWA index data format. */
-  public static final DataFormat BOWTIE_INDEX_ZIP =
-      resgistry.getDataFormatFromName(BowtieIndexZipDataFormat.FORMAT_NAME);
+  public static final DataFormat BOWTIE_INDEX_ZIP = resgistry
+      .getDataFormatFromName(BowtieIndexZipDataFormat.FORMAT_NAME);
 
   /** Filtered SAM data format. */
-  public static final DataFormat FILTERED_MAPPER_RESULTS_SAM =
-      resgistry
-          .getDataFormatFromName(FilteredMapperResultsSamDataFormat.FORMAT_NAME);
+  public static final DataFormat FILTERED_MAPPER_RESULTS_SAM = resgistry
+      .getDataFormatFromName(FilteredMapperResultsSamDataFormat.FORMAT_NAME);
 
   /** Filtered BAM data format. */
-  public static final DataFormat FILTERED_MAPPER_RESULTS_BAM =
+  public static final DataFormat FILTERED_MAPPER_RESULTS_BAM = resgistry
+      .getDataFormatFromName(FilteredMapperResultsBamDataFormat.FORMAT_NAME);
+
+  /** Filtered BAM index data format. */
+  public static final DataFormat FILTERED_MAPPER_RESULTS_BAM_INDEX =
       resgistry
-          .getDataFormatFromName(FilteredMapperResultsBamDataFormat.FORMAT_NAME);
+          .getDataFormatFromName(FilteredMapperResultsBamIndexDataFormat.FORMAT_NAME);
 
   /** SAM results data format. */
-  public static final DataFormat MAPPER_RESULTS_SAM =
-      resgistry.getDataFormatFromName(MapperResultsSamDataFormat.FORMAT_NAME);
+  public static final DataFormat MAPPER_RESULTS_SAM = resgistry
+      .getDataFormatFromName(MapperResultsSamDataFormat.FORMAT_NAME);
 
   /** BAM results data format. */
-  public static final DataFormat MAPPER_RESULTS_BAM =
-      resgistry.getDataFormatFromName(MapperResultsBamDataFormat.FORMAT_NAME);
+  public static final DataFormat MAPPER_RESULTS_BAM = resgistry
+      .getDataFormatFromName(MapperResultsBamDataFormat.FORMAT_NAME);
+
+  /** BAM index results data format. */
+  public static final DataFormat MAPPER_RESULTS_BAM_INDEX = resgistry
+      .getDataFormatFromName(MapperResultsBamIndexDataFormat.FORMAT_NAME);
 
   /** Expression results data format. */
-  public static final DataFormat EXPRESSION_RESULTS_TXT =
-      resgistry
-          .getDataFormatFromName(ExpressionResultsTxtDataFormat.FORMAT_NAME);
+  public static final DataFormat EXPRESSION_RESULTS_TXT = resgistry
+      .getDataFormatFromName(ExpressionResultsTxtDataFormat.FORMAT_NAME);
 
   /** Annotation data format. */
-  public static final DataFormat ANNOTATION_GFF =
-      resgistry.getDataFormatFromName(AnnotationGffDataFormat.FORMAT_NAME);
+  public static final DataFormat ANNOTATION_GFF = resgistry
+      .getDataFormatFromName(AnnotationGffDataFormat.FORMAT_NAME);
 
   /** Annotation data format. */
-  public static final DataFormat ANNOTATION_INDEX_SERIAL =
-      resgistry
-          .getDataFormatFromName(AnnotationIndexSerialDataFormat.FORMAT_NAME);
+  public static final DataFormat ANNOTATION_INDEX_SERIAL = resgistry
+      .getDataFormatFromName(AnnotationIndexSerialDataFormat.FORMAT_NAME);
 
   /** Anadiff results data format. */
-  public static final DataFormat ANADIF_RESULTS_TXT =
-      resgistry.getDataFormatFromName(AnadifResultsTxtDataFormat.FORMAT_NAME);
+  public static final DataFormat ANADIF_RESULTS_TXT = resgistry
+      .getDataFormatFromName(AnadifResultsTxtDataFormat.FORMAT_NAME);
 
   /** Genome data format. */
-  public static final DataFormat GENOME_FASTA =
-      resgistry.getDataFormatFromName(GenomeFastaDataFormat.FORMAT_NAME);
+  public static final DataFormat GENOME_FASTA = resgistry
+      .getDataFormatFromName(GenomeFastaDataFormat.FORMAT_NAME);
 
   /** Genome data format. */
-  public static final DataFormat GENOME_DESC_TXT =
-      resgistry.getDataFormatFromName(GenomeDescTxtDataFormat.FORMAT_NAME);
+  public static final DataFormat GENOME_DESC_TXT = resgistry
+      .getDataFormatFromName(GenomeDescTxtDataFormat.FORMAT_NAME);
 
   /** Unmap reads results data format. */
-  public static final DataFormat UNMAP_READS_FASTA =
-      resgistry.getDataFormatFromName(UnMapReadsFastaDataFormat.FORMAT_NAME);;
+  public static final DataFormat UNMAP_READS_FASTA = resgistry
+      .getDataFormatFromName(UnMapReadsFastaDataFormat.FORMAT_NAME);;
 
 }
