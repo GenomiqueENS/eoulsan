72c1f5b88996cc016b27783d5bdfd307929f1b31
Laurent Jourdren <jourdren@biologie.ens.fr>
2011-07-08 16:34:32 +0000

    Update: New implementation od FastQRecordReaderNew that handle fastq when PHRED 33 quality strings.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1963 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/bio/io/hadoop/FastQRecordReaderNew.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/bio/io/hadoop/FastQRecordReaderNew.java
@@ -25,202 +25,111 @@
 package fr.ens.transcriptome.eoulsan.bio.io.hadoop;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.DataOutputBuffer;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.CompressionCodecFactory;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-
-import fr.ens.transcriptome.eoulsan.Globals;
+import org.apache.hadoop.mapreduce.lib.input.LineRecordReader;
 
 public class FastQRecordReaderNew extends RecordReader<LongWritable, Text> {
-
-
-  /** Logger */
-  private static final Logger LOGGER = Logger.getLogger(Globals.APP_NAME);
-  
-  private long end;
-  private boolean stillInChunk = true;
 
   private LongWritable key = new LongWritable();
   private Text value = new Text();
 
-  private InputStream fsin;
-  private DataOutputBuffer buffer = new DataOutputBuffer();
+  private final String[] lines = new String[4];
+  private final long[] poss = new long[4];
 
-  private byte[] endTag = "\n@".getBytes();
-  private static final Pattern PATTERN = Pattern.compile("\n");
-  private static final StringBuilder sb = new StringBuilder();
+  private LineRecordReader lrr;
 
-  // TODO
-  private boolean startPos = true;
-  
+  @Override
+  public void close() throws IOException {
 
-  private long pos;
-  
-  public void initialize(InputSplit inputSplit,
-      TaskAttemptContext taskAttemptContext) throws IOException,
-      InterruptedException {
-
-    
-    FileSplit split = (FileSplit) inputSplit;
-    Configuration conf = taskAttemptContext.getConfiguration();
-    Path path = split.getPath();
-    FileSystem fs = path.getFileSystem(conf);
-
-    final CompressionCodecFactory compressionCodecs =
-        new CompressionCodecFactory(conf);
-    final CompressionCodec codec = compressionCodecs.getCodec(path);
-
-    // open the file and seek to the start of the split
-    FSDataInputStream fileIn = fs.open(split.getPath());
-    
-    long start = split.getStart();
-    
-    if (codec != null) {
-      this.fsin = codec.createInputStream(fileIn);
-      end = Long.MAX_VALUE;
-      
-      LOGGER.info("Compressed mode. " + this.fsin.getClass().getName());
-    } 
-    else {
-      
-      LOGGER.info("Non Compressed mode. " + this.fsin.getClass().getName());
-      if (start != 0) {
-//        skipFirstLine = true;
-//        --start;
-//        fileIn.seek(start);
-        end = split.getStart() + split.getLength();
-        fileIn.seek(start);
-        this.pos=start;
-        readUntilMatch(endTag, false);
-        startPos=false;
-      }
-      fsin = fileIn;;
-    }
-
-    
-
-//    fsin = fs.open(path);
-//    long start = split.getStart();
-//    end = split.getStart() + split.getLength();
-//    fsin.seek(start);
-
-//    if (start != 0) {
-//      readUntilMatch(endTag, false);
-//    }
+    this.lrr.close();
   }
 
-  public boolean nextKeyValue() throws IOException {
-    if (!stillInChunk)
-      return false;
-
-    //final long startPos = fsin.getPos();
-
-
-
-    boolean status = readUntilMatch(endTag, true);
-
-    final String data;
-
-    // If start of the file, ignore first '@'
-    if (startPos) {
-      data = new String(buffer.getData(), 1, buffer.getLength());
-      startPos=false;
-    }
-    else
-      data = new String(buffer.getData(), 0, buffer.getLength());
-
-    final String[] lines = PATTERN.split(data);
-
-    String id = "";
-    int count = 0;
-
-    for (int i = 0; i < lines.length; i++) {
-
-      final String line = lines[i].trim();
-
-      if ("".equals(line))
-        continue;
-
-      if (count == 0)
-        id = line;
-
-      if (count == 2 && !id.equals(line.substring(1)))
-        throw new IOException("Invalid record: id not equals "
-            + id + " " + line.substring(1));
-
-      if (count != 2 && count < 4) {
-        if (count > 0)
-          sb.append("\t");
-        sb.append(line);
-      }
-      count++;
-    }
-
-    // if (count != 5)
-    // throw new IOException("Invalid record: found " + count + " lines");
-
-    value = new Text(sb.toString());
-    // value.set(buffer.getData(), 0, buffer.getLength());
-    sb.setLength(0);
-
-    key = new LongWritable(pos);
-    buffer.reset();
-
-    if (!status) {
-      stillInChunk = false;
-    }
-
-    return true;
-  }
-
+  @Override
   public LongWritable getCurrentKey() throws IOException, InterruptedException {
+
     return key;
   }
 
+  @Override
   public Text getCurrentValue() throws IOException, InterruptedException {
+
     return value;
   }
 
+  @Override
   public float getProgress() throws IOException, InterruptedException {
-    return 0;
+
+    return this.lrr.getProgress();
   }
 
-  public void close() throws IOException {
-    fsin.close();
+  @Override
+  public void initialize(final InputSplit inputSplit,
+      final TaskAttemptContext taskAttemptContext) throws IOException,
+      InterruptedException {
+
+    this.lrr = new LineRecordReader();
+    this.lrr.initialize(inputSplit, taskAttemptContext);
   }
 
-  private boolean readUntilMatch( byte[] match, boolean withinBlock)
-      throws IOException {
-    int i = 0;
-    
-    while (true) {
-      int b = fsin.read();
-      if (b == -1)
+  @Override
+  public boolean nextKeyValue() throws IOException, InterruptedException {
+
+    int count = 0;
+    boolean found = false;
+
+    while (!found) {
+
+      if (!this.lrr.nextKeyValue())
         return false;
-      pos++;
-      if (withinBlock)
-        buffer.write(b);
-      if (b == match[i]) {
-        i++;
-        if (i >= match.length) {
-          return pos < end;
+
+      final String s = this.getCurrentValue().toString().trim();
+
+      // Prevent empty lines
+      if (s.length() == 0)
+        continue;
+
+      this.lines[count] = s;
+      this.poss[count] = this.lrr.getCurrentKey().get();
+
+      if (count < 3)
+        count++;
+      else {
+
+        if (this.lines[0].charAt(0) == '@' && this.lines[2].charAt(0) == '+')
+          found = true;
+        else {
+
+          // Shift lines
+          this.lines[0] = this.lines[1];
+          this.lines[1] = this.lines[2];
+          this.lines[2] = this.lines[3];
+          
+          // Shift positions
+          this.poss[0] = this.poss[1];
+          this.poss[1] = this.poss[2];
+          this.poss[2] = this.poss[3];
         }
-      } else
-        i = 0;
+      }
+
     }
+
+    // Set key
+    this.key = new LongWritable(this.poss[0]);
+    
+    // Set value
+    this.value =
+        new Text(this.lines[0]
+            + '\n' + this.lines[1] + '\n' + this.lines[2] + '\n'
+            + this.lines[3]);
+
+    // Clean array
+    this.lines[0] = this.lines[1] = this.lines[2] = this.lines[3] = null;
+
+    return true;
   }
 }
