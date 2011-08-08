02100e5e5387f6a2e15410e4e201df1510593fb0
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2011-08-08 21:12:12 +0000

    UPDATE: Move ReadsIndexGenerator to steps package.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@1980 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/data/SOAPIndexZipDataFormat.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/data/SOAPIndexZipDataFormat.java
@@ -24,8 +24,8 @@
 
 package fr.ens.transcriptome.eoulsan.data;
 
+import fr.ens.transcriptome.eoulsan.steps.ReadsIndexGeneratorStep;
 import fr.ens.transcriptome.eoulsan.steps.Step;
-import fr.ens.transcriptome.eoulsan.steps.mapping.hadoop.ReadsIndexGeneratorStep;
 
 /**
  * This class define a SOAP zip index DataFormat.
