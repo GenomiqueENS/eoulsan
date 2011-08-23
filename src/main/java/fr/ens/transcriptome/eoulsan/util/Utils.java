05584e14cddad02bafd4772a76708400f95f183c
jourdren <jourdren@biologie.ens.fr>
2011-08-23 21:06:45 +0200

    FIX: Utils

--- src/main/java/fr/ens/transcriptome/eoulsan/util/Utils.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/util/Utils.java
@@ -33,11 +33,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * This utility class contains common useful methods.
- * @author Laurent Jourdren
- *
- */
 public class Utils {
 
   /**
