e050f492f6659089aa36acde442ff4be5b612de3
Laurent Jourdren <jourdren@biologie.ens.fr>
2011-09-09 14:38:52 +0000

    UPDATE: In Design unit test remove simpleDesignReader/Writer code.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@2067 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/test/java/fr/ens/transcriptome/eoulsan/design/DesignTest.java
+++ src/test/java/fr/ens/transcriptome/eoulsan/design/DesignTest.java
@@ -26,51 +26,35 @@ package fr.ens.transcriptome.eoulsan.design;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-
 import org.junit.Test;
 
-import fr.ens.transcriptome.eoulsan.design.io.SimpleDesignReader;
-import fr.ens.transcriptome.eoulsan.design.io.SimpleDesignWriter;
 import fr.ens.transcriptome.eoulsan.io.EoulsanIOException;
-
 
 public class DesignTest {
 
-  
   @Test
   public void testCompareStringString() throws EoulsanIOException {
 
-    
     Design d = DesignFactory.createEmptyDesign();
-    
+
     d.addSample("G5_1");
-    assertEquals(true,d.isSample("G5_1"));
-    assertEquals(1,d.getSample("G5_1").getId());
+    assertEquals(true, d.isSample("G5_1"));
+    assertEquals(1, d.getSample("G5_1").getId());
     d.addSample("G3_1");
-    assertEquals(true,d.isSample("G5_1"));
-    assertEquals(true,d.isSample("G3_1"));
-    assertEquals(1,d.getSample("G5_1").getId());
-    assertEquals(2,d.getSample("G3_1").getId());
-    
+    assertEquals(true, d.isSample("G5_1"));
+    assertEquals(true, d.isSample("G3_1"));
+    assertEquals(1, d.getSample("G5_1").getId());
+    assertEquals(2, d.getSample("G3_1").getId());
+
     d.getSample("G5_1").setId(5);
-    assertEquals(5,d.getSample("G5_1").getId());
+    assertEquals(5, d.getSample("G5_1").getId());
 
     d.getSample("G5_1").getMetadata().setDescription("Test G5");
-    assertEquals("Test G5",d.getSample("G5_1").getMetadata().getDescription());
+    assertEquals("Test G5", d.getSample("G5_1").getMetadata().getDescription());
     d.getSample("G3_1").getMetadata().setDescription("Test G3");
-    assertEquals("Test G5",d.getSample("G5_1").getMetadata().getDescription());
-    assertEquals("Test G3",d.getSample("G3_1").getMetadata().getDescription());
-    assertEquals(5,d.getSample("G5_1").getId());
-    
-    
-    SimpleDesignReader sdr = new SimpleDesignReader(new File("/home/jourdren/tmp/d.txt"));
-    Design d2 = sdr.read();
-    
-    assertEquals(5,d.getSample("G5_1").getId());
-    
-    SimpleDesignWriter sdw = new SimpleDesignWriter(new File("/home/jourdren/tmp/d2.txt"));
-    sdw.write(d2);
-    
+    assertEquals("Test G5", d.getSample("G5_1").getMetadata().getDescription());
+    assertEquals("Test G3", d.getSample("G3_1").getMetadata().getDescription());
+    assertEquals(5, d.getSample("G5_1").getId());
+
   }
 }
