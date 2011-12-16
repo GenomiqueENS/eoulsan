4413a970914562fc1980cf049d13a4863de4f8e0
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2011-12-16 11:07:38 +0000

    ADD: The createDesignAction can now handle Casava design file for Eoulsan design creating.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/eoulsan@2166 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/java/fr/ens/transcriptome/eoulsan/design/DesignBuilder.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/design/DesignBuilder.java
@@ -24,6 +24,9 @@
 
 package fr.ens.transcriptome.eoulsan.design;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -46,10 +49,13 @@ import fr.ens.transcriptome.eoulsan.data.DataFileMetadata;
 import fr.ens.transcriptome.eoulsan.data.DataFormatRegistry;
 import fr.ens.transcriptome.eoulsan.data.DataType;
 import fr.ens.transcriptome.eoulsan.data.DataTypes;
+import fr.ens.transcriptome.eoulsan.illumina.CasavaDesign;
+import fr.ens.transcriptome.eoulsan.illumina.CasavaSample;
+import fr.ens.transcriptome.eoulsan.illumina.io.CasavaDesignCSVReader;
 import fr.ens.transcriptome.eoulsan.util.StringUtils;
 
 /**
- * This class allow to easyly build Design object from files paths.
+ * This class allow to easily build Design object from files paths.
  * @author Laurent Jourdren
  */
 public class DesignBuilder {
@@ -60,9 +66,79 @@ public class DesignBuilder {
   private static final int MAX_FASTQ_ENTRIES_TO_READ = 10000;
 
   private DataFormatRegistry dfr = DataFormatRegistry.getInstance();
-  private List<DataFile> fastqList = Lists.newArrayList();
+  private List<FastqEntry> fastqList = Lists.newArrayList();
   private DataFile genomeFile;
   private DataFile gffFile;
+
+  /**
+   * This inner class define a fastq entry.
+   * @author Laurent Jourdren
+   */
+  private static class FastqEntry {
+
+    private final DataFile path;
+    private final String sampleName;
+    private final String sampleDesc;
+    private final String sampleOperator;
+
+    @Override
+    public boolean equals(Object obj) {
+
+      return path.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+
+      return path.hashCode();
+    }
+
+    public String toString() {
+
+      final StringBuilder sb = new StringBuilder();
+
+      sb.append("FastqEntry(Sample: ");
+      sb.append(sampleName);
+
+      if (sampleDesc != null) {
+        sb.append(", Description: ");
+        sb.append(sampleDesc);
+      }
+
+      if (sampleOperator != null) {
+        sb.append(", Operator: ");
+        sb.append(sampleOperator);
+      }
+
+      sb.append(", Path: ");
+      sb.append(path);
+      sb.append(")");
+
+      return sb.toString();
+    }
+
+    //
+    // Constructors
+    //
+
+    public FastqEntry(final DataFile path) {
+
+      this.path = path;
+      this.sampleName = StringUtils.basename(path.getName());
+      this.sampleDesc = null;
+      this.sampleOperator = null;
+    }
+
+    public FastqEntry(final DataFile path, final String sampleName,
+        final String sampleDesc, final String sampleOperator) {
+
+      this.path = path;
+      this.sampleName = sampleName;
+      this.sampleDesc = sampleDesc;
+      this.sampleOperator = sampleOperator;
+    }
+
+  }
 
   /**
    * Add a file to the design builder
@@ -92,7 +168,7 @@ public class DesignBuilder {
 
       // Don't add previously added file
       if (!this.fastqList.contains(file))
-        this.fastqList.add(file);
+        this.fastqList.add(new FastqEntry(file));
 
     } else if (isDataTypeExtension(DataTypes.GENOME, extension, md))
       this.genomeFile = file;
@@ -117,6 +193,123 @@ public class DesignBuilder {
   }
 
   /**
+   * Add all the sample from a Casava design.
+   * @param casavaDesign The Casava design object
+   * @param projectName name of the project
+   * @param casavaOutputDir the output directory of Casava demultiplexing
+   * @throws EoulsanException if an error occurs while adding the casava design
+   */
+  public void addCasavaDesignProject(final CasavaDesign casavaDesign,
+      final String projectName, final File casavaOutputDir)
+      throws EoulsanException {
+
+    if (casavaDesign == null || casavaOutputDir == null)
+      return;
+
+    if (!casavaOutputDir.exists() || !casavaOutputDir.isDirectory())
+      throw new EoulsanException(
+          "The casava output directory does not exists: " + casavaOutputDir);
+
+    for (CasavaSample sample : casavaDesign) {
+
+      final String sampleProject = sample.getSampleProject();
+      final String sampleName = sample.getSampleId();
+      final String sampleDesc = sample.getDescription();
+      final String sampleOperator = sample.getOperator();
+
+      // Select only project samples
+      if (projectName != null && !projectName.equals(sampleProject))
+        continue;
+
+      final File dataDir =
+          new File(casavaOutputDir.getPath()
+              + "/Project_" + sampleProject + "/Sample_" + sampleName);
+
+      // Test if the directory with fastq files exists
+      if (!dataDir.exists() || !dataDir.isDirectory())
+        continue;
+
+      for (File fastqFile : dataDir.listFiles(new FileFilter() {
+
+        @Override
+        public boolean accept(File f) {
+
+          final String filename =
+              StringUtils.filenameWithoutCompressionExtension(f.getName());
+
+          if (filename.endsWith(".fastq") || filename.endsWith(".fq"))
+            return true;
+
+          return false;
+        }
+      }))
+        this.fastqList.add(new FastqEntry(new DataFile(fastqFile), sampleName,
+            sampleDesc, sampleOperator));
+    }
+  }
+
+  /**
+   * Add all the sample from a Casava design.
+   * @param casavaDesignFile the path to the Casava design
+   * @param projectName the name of the project
+   * @throws EoulsanException if an error occurs while reading the Casava design
+   */
+  public void addCasavaDesignProject(final File casavaDesignFile,
+      final String projectName) throws EoulsanException {
+
+    if (casavaDesignFile == null)
+      return;
+
+    LOGGER.info("Add Casava design file "
+        + casavaDesignFile
+        + " to design with "
+        + (projectName == null ? "no project filter." : projectName
+            + " project filter."));
+
+    final File baseDir;
+    final File file;
+
+    if (!casavaDesignFile.exists())
+      throw new EoulsanException("The casava design file does not exists: "
+          + casavaDesignFile);
+
+    if (casavaDesignFile.isDirectory()) {
+      baseDir = casavaDesignFile;
+
+      final File[] files = baseDir.listFiles(new FilenameFilter() {
+
+        @Override
+        public boolean accept(File dir, String filename) {
+          if (filename.endsWith(".csv"))
+            return true;
+          return false;
+        }
+      });
+
+      if (files == null || files.length == 0)
+        throw new EoulsanException("No Casava design file found in directory: "
+            + baseDir);
+
+      if (files.length > 1)
+        throw new EoulsanException(
+            "More than one Casava design file found in directory: " + baseDir);
+
+      file = files[0];
+    } else {
+      baseDir = casavaDesignFile.getParentFile();
+      file = casavaDesignFile;
+    }
+
+    try {
+      CasavaDesignCSVReader reader = new CasavaDesignCSVReader(file);
+      addCasavaDesignProject(reader.read(), projectName, baseDir);
+    } catch (IOException e) {
+      throw new EoulsanException(e.getMessage());
+    }
+
+  }
+
+  /**
    * Create design object.
    * @param pairEndMode true if the pair end mode is enabled
    * @return a new Design object
@@ -128,53 +321,103 @@ public class DesignBuilder {
     final FastqFormat defaultFastqFormat =
         EoulsanRuntime.getSettings().getDefaultFastqFormat();
 
-    for (List<DataFile> fqs : findPairEndFiles(pairEndMode)) {
+    for (List<FastqEntry> fes : findPairEndFiles()) {
 
-      // Convert the list of DataFiles to a list of filenames
-      final List<String> filenames = Lists.newArrayList();
-      for (DataFile f : fqs)
-        filenames.add(f.getSource());
+      final String sampleName = fes.get(0).sampleName;
+      final String desc = fes.get(0).sampleDesc;
+      final String operator = fes.get(0).sampleOperator;
 
-      final String sampleName = StringUtils.basename(fqs.get(0).getName());
+      if (pairEndMode) {
 
-      // Create the sample
-      result.addSample(sampleName);
-      final Sample s = result.getSample(sampleName);
-      final SampleMetadata smd = s.getMetadata();
+        // Convert the list of DataFiles to a list of filenames
+        final List<String> filenames = Lists.newArrayList();
+        for (FastqEntry fe : fes)
+          filenames.add(fe.path.getSource());
 
-      // Set the fastq file of the sample
+        addSample(result, sampleName, desc, operator, defaultFastqFormat,
+            filenames, fes.get(0).path);
 
-      smd.setReads(filenames);
+      } else {
 
-      // Set the genome file if exists
-      if (this.genomeFile != null)
-        smd.setGenome(this.genomeFile.toString());
+        int count = 0;
 
-      // Set the Annotation file
-      if (this.gffFile != null)
-        smd.setAnnotation(this.gffFile.toString());
+        for (FastqEntry fe : fes) {
 
-      // Identify Fastq format
-      FastqFormat format = null;
+          addSample(result, sampleName + StringUtils.toLetter(count), desc,
+              operator, defaultFastqFormat,
+              Collections.singletonList(fe.path.getSource()), fe.path);
 
-      try {
-        LOGGER.info("Check fastq format for " + fqs.get(0));
-        format =
-            FastqFormat.identifyFormat(fqs.get(0).open(),
-                MAX_FASTQ_ENTRIES_TO_READ);
-      } catch (IOException e) {
-        throw new EoulsanException(e.getMessage());
-      } catch (BadBioEntryException e) {
-        throw new EoulsanException(e.getMessage());
+          count++;
+        }
+
       }
-
-      smd.setFastqFormat(format == null ? defaultFastqFormat : format);
-      smd.setCondition(sampleName);
-      smd.setReplicatType("T");
-      smd.setUUID(UUID.randomUUID().toString());
     }
 
     return result;
+  }
+
+  /**
+   * Add a Sample to the Design object 
+   * @param design Design object
+   * @param sampleName name of the sample
+   * @param desc description of the sample
+   * @param operator operator for the sample
+   * @param defaultFastqFormat default fastq format
+   * @param filenames list of the fastq files for the sample
+   * @param fileToCheck DataFile of the file to use to check fastq format
+   * @throws EoulsanException if an error occurs while adding the sample
+   */
+  private void addSample(final Design design, final String sampleName,
+      final String desc, final String operator,
+      final FastqFormat defaultFastqFormat, final List<String> filenames,
+      final DataFile fileToCheck) throws EoulsanException {
+
+    if (design == null)
+      return;
+
+    // Create the sample
+    design.addSample(sampleName);
+    final Sample s = design.getSample(sampleName);
+    final SampleMetadata smd = s.getMetadata();
+
+    // Set the fastq file of the sample
+    smd.setReads(filenames);
+
+    // Set the description of the sample if exists
+    if (desc != null)
+      smd.setDescription(desc);
+
+    // Set the operator of the sample if exists
+    if (operator != null)
+      smd.setOperator(operator);
+
+    // Set the genome file if exists
+    if (this.genomeFile != null)
+      smd.setGenome(this.genomeFile.toString());
+
+    // Set the Annotation file
+    if (this.gffFile != null)
+      smd.setAnnotation(this.gffFile.toString());
+
+    // Identify Fastq format
+    FastqFormat format = null;
+
+    try {
+      LOGGER.info("Check fastq format for " + fileToCheck);
+      format =
+          FastqFormat.identifyFormat(fileToCheck.open(),
+              MAX_FASTQ_ENTRIES_TO_READ);
+    } catch (IOException e) {
+      throw new EoulsanException(e.getMessage());
+    } catch (BadBioEntryException e) {
+      throw new EoulsanException(e.getMessage());
+    }
+
+    smd.setFastqFormat(format == null ? defaultFastqFormat : format);
+    smd.setCondition(sampleName);
+    smd.setReplicatType("T");
+    smd.setUUID(UUID.randomUUID().toString());
+
   }
 
   private boolean isDataTypeExtension(final DataType dataType,
@@ -190,28 +433,19 @@ public class DesignBuilder {
 
   /**
    * Group pair end files.
-   * @param pairEnd true if files must be grouped as pair end files
    * @return a list of 1-2 pair end files
    * @throws EoulsanException if an error occurs while getting the id of first
    *           read of the fastq files
    */
-  private List<List<DataFile>> findPairEndFiles(final boolean pairEnd)
-      throws EoulsanException {
+  private List<List<FastqEntry>> findPairEndFiles() throws EoulsanException {
 
-    final Map<String, List<DataFile>> mapPrefix = Maps.newHashMap();
-    final Map<DataFile, Integer> mapPair = Maps.newHashMap();
-    final List<List<DataFile>> result = Lists.newArrayList();
+    final Map<String, List<FastqEntry>> mapPrefix = Maps.newHashMap();
+    final Map<FastqEntry, Integer> mapPair = Maps.newHashMap();
+    final List<List<FastqEntry>> result = Lists.newArrayList();
 
-    for (DataFile f : this.fastqList) {
+    for (FastqEntry fe : this.fastqList) {
 
-      // If not pair-end mode don't check files
-      if (!pairEnd) {
-
-        result.add(Collections.singletonList(f));
-        continue;
-      }
-
-      final String readId = getFirstReadSeqId(f);
+      final String readId = getFirstReadSeqId(fe.path);
 
       String prefix = readId;
 
@@ -224,21 +458,21 @@ public class DesignBuilder {
                 + irid.getXClusterCoordinateInTile() + "\t"
                 + irid.getYClusterCoordinateInTile();
 
-        mapPair.put(f, irid.getPairMember());
+        mapPair.put(fe, irid.getPairMember());
 
       } catch (EoulsanException e) {
 
         if (readId.endsWith("/1")) {
           prefix = readId.substring(0, readId.length() - 3);
-          mapPair.put(f, 1);
+          mapPair.put(fe, 1);
         } else if (readId.endsWith("/2")) {
           prefix = readId.substring(0, readId.length() - 3);
-          mapPair.put(f, 2);
+          mapPair.put(fe, 2);
         } else
-          mapPair.put(f, 1);
+          mapPair.put(fe, 1);
       }
 
-      final List<DataFile> list;
+      final List<FastqEntry> list;
 
       if (mapPrefix.containsKey(prefix))
         list = mapPrefix.get(prefix);
@@ -248,11 +482,11 @@ public class DesignBuilder {
         result.add(list);
       }
 
-      list.add(f);
+      list.add(fe);
     }
 
     // Order the pair end files
-    for (List<DataFile> list : result) {
+    for (List<FastqEntry> list : result) {
 
       // Check invalid number of files
       if (list.size() > 2)
@@ -279,7 +513,7 @@ public class DesignBuilder {
         // Change the order of the file if necessary
         if (member1 == 2 && member2 == 1) {
 
-          final DataFile tmp = list.get(0);
+          final FastqEntry tmp = list.get(0);
           list.set(0, list.get(1));
           list.set(1, tmp);
         }
