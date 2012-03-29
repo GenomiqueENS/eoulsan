3db2077a4d93cdaa8a9b48b67dcf672be8f52e18
Claire Wallon <wallon@biologie.ens.fr>
2012-03-29 09:47:48 +0200

    MODIFICATION of the function configure() to take into account the various filters available and not also the mapping quality score filter. ADDITION of the function convertCompatibilityFilterKey() to convert the old parameter name "mappingqualitythreshold" to "mappingQuality.threshold".

--- src/main/java/fr/ens/transcriptome/eoulsan/steps/mapping/AbstractSAMFilterStep.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/steps/mapping/AbstractSAMFilterStep.java
@@ -28,14 +28,22 @@ import static fr.ens.transcriptome.eoulsan.data.DataFormats.FILTERED_MAPPER_RESU
 import static fr.ens.transcriptome.eoulsan.data.DataFormats.GENOME_DESC_TXT;
 import static fr.ens.transcriptome.eoulsan.data.DataFormats.MAPPER_RESULTS_SAM;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
 import fr.ens.transcriptome.eoulsan.EoulsanException;
 import fr.ens.transcriptome.eoulsan.Globals;
+import fr.ens.transcriptome.eoulsan.bio.alignmentsfilters.MultiReadAlignmentsFilterBuilder;
+import fr.ens.transcriptome.eoulsan.bio.alignmentsfilters.QualityReadAlignmentsFilter;
+import fr.ens.transcriptome.eoulsan.bio.alignmentsfilters.ReadAlignmentsFilter;
+import fr.ens.transcriptome.eoulsan.bio.readsfilters.QualityReadFilter;
+import fr.ens.transcriptome.eoulsan.bio.readsfilters.ReadFilter;
+import fr.ens.transcriptome.eoulsan.bio.readsfilters.TrimReadFilter;
 import fr.ens.transcriptome.eoulsan.core.Parameter;
 import fr.ens.transcriptome.eoulsan.data.DataFormat;
 import fr.ens.transcriptome.eoulsan.steps.AbstractStep;
+import fr.ens.transcriptome.eoulsan.util.ReporterIncrementer;
 
 /**
  * This class define an abstract step for alignments filtering.
@@ -51,6 +59,8 @@ public abstract class AbstractSAMFilterStep extends AbstractStep {
 
   private static final int MAX_MAPPING_QUALITY_THRESHOLD = 255;
   protected static final String COUNTER_GROUP = "sam_filtering";
+  
+  private MultiReadAlignmentsFilterBuilder readAlignmentsFilterBuilder;
 
   private int mappingQualityThreshold = -1;
 
@@ -61,6 +71,11 @@ public abstract class AbstractSAMFilterStep extends AbstractStep {
   /**
    * Get the mapping quality threshold.
    * @return the quality mapping threshold
+   * 
+   * !!!!!!!!!!!! Problem for the class SAMFilterHadoopStep.java : 
+   * mappingQualityThreshold is no longer used here 
+   * (getMappingQualityThreshold() called in this class) (cf. comments in
+   * configure())
    */
   protected int getMappingQualityThreshold() {
 
@@ -96,8 +111,21 @@ public abstract class AbstractSAMFilterStep extends AbstractStep {
   @Override
   public void configure(final Set<Parameter> stepParameters)
       throws EoulsanException {
-
+    
+    final MultiReadAlignmentsFilterBuilder mrafb = 
+        new MultiReadAlignmentsFilterBuilder();
+    
     for (Parameter p : stepParameters) {
+      mrafb.addParameter(convertCompatibilityFilterKey(p.getName()), 
+          p.getStringValue());
+    }
+    
+    // Force parameter checking
+    mrafb.getAlignmentsFilter();
+    
+    this.readAlignmentsFilterBuilder = mrafb;
+
+    /*for (Parameter p : stepParameters) {
 
       if ("mappingqualitythreshold".equals(p.getName()))
         mappingQualityThreshold = p.getIntValue();
@@ -116,12 +144,51 @@ public abstract class AbstractSAMFilterStep extends AbstractStep {
         || this.mappingQualityThreshold > MAX_MAPPING_QUALITY_THRESHOLD) {
       throw new EoulsanException("Invalid mapping quality theshold: "
           + this.mappingQualityThreshold);
-    }
+    }*/
 
     // Log Step parameters
-    LOGGER.info("In "
-        + getName() + ", mappingQualityThreshold="
-        + this.mappingQualityThreshold);
+//    LOGGER.info("In "
+//        + getName() + ", mappingQualityThreshold="
+//        + this.mappingQualityThreshold);
+  }
+  
+  /**
+   * Convert old key names to new names
+   * @param key key to convert
+   * @return the new key name if necessary
+   */
+  static String convertCompatibilityFilterKey(final String key) {
+    
+    if (key == null)
+      return null;
+
+    if ("mappingqualitythreshold".equals(key))
+      return QualityReadAlignmentsFilter.FILTER_NAME + ".threshold";
+
+    return key;
+  }
+  
+  /**
+   * Get the ReadAlignmentsFilter object.
+   * @param incrementer incrementer to use
+   * @param counterGroup counter group for the incrementer
+   * @return a new ReadAlignmentsFilter object
+   * @throws EoulsanException if an error occurs while initialize one of the
+   *           filter
+   */
+  protected ReadAlignmentsFilter getAlignmentsFilter(final ReporterIncrementer incrementer,
+      final String counterGroup) throws EoulsanException {
+
+    return this.readAlignmentsFilterBuilder.getAlignmentsFilter(incrementer, counterGroup);
+  }
+
+  /**
+   * Get the parameters of the alignments filter.
+   * @return a map with all the parameters of the filter
+   */
+  protected Map<String, String> getAlignmentsFilterParameters() {
+
+    return this.readAlignmentsFilterBuilder.getParameters();
   }
 
 }
