a0fbcdb49e07627e9b0628dc1dd2b032ba3e8346
Vivien Deshaies <deshaies@biologie.ens.fr>
2012-06-22 17:29:31 +0200

    Modification to use new DiffAna class with Rserve server

--- src/main/java/fr/ens/transcriptome/eoulsan/steps/anadiff/local/DiffAnaLocalStep.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/steps/anadiff/local/DiffAnaLocalStep.java
@@ -25,8 +25,11 @@
 package fr.ens.transcriptome.eoulsan.steps.anadiff.local;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 import fr.ens.transcriptome.eoulsan.EoulsanException;
+import fr.ens.transcriptome.eoulsan.EoulsanRuntime;
+import fr.ens.transcriptome.eoulsan.Globals;
 import fr.ens.transcriptome.eoulsan.annotations.LocalOnly;
 import fr.ens.transcriptome.eoulsan.core.Context;
 import fr.ens.transcriptome.eoulsan.data.DataFormat;
@@ -45,6 +48,9 @@ import fr.ens.transcriptome.eoulsan.steps.anadiff.DiffAna;
 public class DiffAnaLocalStep extends AbstractStep {
 
   private static final String STEP_NAME = "diffana";
+
+  /** Logger */
+  private static final Logger LOGGER = Logger.getLogger(Globals.APP_NAME);
 
   //
   // Step methods
@@ -81,11 +87,17 @@ public class DiffAnaLocalStep extends AbstractStep {
 
       final DataFormat eDF = DataFormats.EXPRESSION_RESULTS_TXT;
 
+      LOGGER.info("Rserve enable : "
+          + EoulsanRuntime.getSettings().isRServeServerEnabled());
+      
+      String rServeName = null;
+      if (EoulsanRuntime.getRuntime().getSettings().isRServeServerEnabled())
+        rServeName =
+            EoulsanRuntime.getRuntime().getSettings().getRServeServername();
+
       final DiffAna ad =
           new DiffAna(design, new File("."), eDF.getType().getPrefix(),
-              eDF.getDefaultExtention(), context.getRuntime()
-                  .getTempDirectory());
-
+              eDF.getDefaultExtention(), new File("."), rServeName);
       ad.run();
 
       // Write log file
