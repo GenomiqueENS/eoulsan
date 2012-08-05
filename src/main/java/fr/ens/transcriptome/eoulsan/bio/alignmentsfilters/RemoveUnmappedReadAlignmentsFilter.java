30cbbe29c7dfa484099ae2b1a2ac1e301b208fe5
Laurent Jourdren <jourdren@biologie.ens.fr>
2012-08-05 12:29:38 +0200

    DOC: Add TODO for Claire

--- src/main/java/fr/ens/transcriptome/eoulsan/bio/alignmentsfilters/RemoveUnmappedReadAlignmentsFilter.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/bio/alignmentsfilters/RemoveUnmappedReadAlignmentsFilter.java
@@ -41,8 +41,9 @@ public class RemoveUnmappedReadAlignmentsFilter extends
     AbstractReadAlignmentsFilter {
 
   public static final String FILTER_NAME = "removeunmapped";
+  // TODO Claire : Why remove is not used ?
   private boolean remove = false;
-  
+
   private final List<SAMRecord> result = new ArrayList<SAMRecord>();
 
   @Override
@@ -66,33 +67,33 @@ public class RemoveUnmappedReadAlignmentsFilter extends
     // single-end mode
     if (!records.get(0).getReadPairedFlag()) {
       for (SAMRecord r : records)
-        
+
         // storage in 'result' of records that do not pass the filter
         if (r.getReadUnmappedFlag())
           this.result.add(r);
     }
-    
+
     // paired-end mode
     else {
-      for (int counterRecord = 0; counterRecord < records.size()-1; 
-          counterRecord += 2) {
-        
+      for (int counterRecord = 0; counterRecord < records.size() - 1; counterRecord +=
+          2) {
+
         // storage in 'result' of records that do not pass the filter
-        if (records.get(counterRecord).getReadUnmappedFlag() ||
-            records.get(counterRecord+1).getReadUnmappedFlag()) {
-          
+        if (records.get(counterRecord).getReadUnmappedFlag()
+            || records.get(counterRecord + 1).getReadUnmappedFlag()) {
+
           // records are stored 2 by 2 because of the paired-end mode
           this.result.add(records.get(counterRecord));
-          this.result.add(records.get(counterRecord+1));
+          this.result.add(records.get(counterRecord + 1));
         }
       }
     }
-    
+
     // all records that do not pass the filter are removed
     records.removeAll(result);
     result.clear();
   }
-  
+
   @Override
   public void setParameter(final String key, final String value)
       throws EoulsanException {
