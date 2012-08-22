095cb1f483cdec54e16eba49ef11d0e0399ee35f
Laurent Jourdren <jourdren@biologie.ens.fr>
2012-08-22 16:21:52 +0200

    UPDATE: update GenomicArrayTest

--- src/test/java/fr/ens/transcriptome/eoulsan/bio/GenomicArrayTest.java
+++ src/test/java/fr/ens/transcriptome/eoulsan/bio/GenomicArrayTest.java
@@ -26,6 +26,8 @@ package fr.ens.transcriptome.eoulsan.bio;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -34,55 +36,179 @@ import org.junit.Test;
  */
 public class GenomicArrayTest {
 
-    @Test
-    public void bidon() {
+  @Test
+  public void bidon() {
+  }
+
+  // /**
+  // * @throws java.lang.Exception
+  // */
+  // @Before
+  // public void setUp() throws Exception {
+  // }
+  //
+
+  private class FastGemomicInterval {
+
+    final String chromosome;
+    final char strand;
+
+    GenomicInterval iv(final int start, final int end) {
+
+      return new GenomicInterval(chromosome, start, end, strand);
     }
+
+    FastGemomicInterval(final String chromosome, final char strand) {
+
+      this.chromosome = chromosome;
+      this.strand = strand;
+    }
+
+  }
+
+  /**
+   * Test method for
+   * {fr.ens.transcriptome.eoulsan.bio.GenomicArray#addEntry(fr.ens
+   * .transcriptome.eoulsan.bio.GenomicInterval, java.lang.Object)}.
+   */
+  @Test
+  public void testAddEntry() {
+
+    GenomicArray<String> ga = new GenomicArray<String>();
+
+    final FastGemomicInterval fgi = new FastGemomicInterval("chr1", '.');
+    
+    
+
+    // Add [10,50]=a
+    GenomicInterval iv1 = fgi.iv(10, 50);
+    ga.addEntry(iv1, "a");
+    
+    Map<GenomicInterval,String> r = ga.getEntries(fgi.chromosome, 1, 100);
+    System.out.println(r);
+    
+    
+    assertTrue(r.containsKey(fgi.iv(10,50)));
+    assertFalse(r.containsKey(fgi.iv(60,70)));
+    
+    // Add [60,70]=a
+    ga.addEntry(fgi.iv(60, 70), "b");
+    
+    r = ga.getEntries(fgi.chromosome, 1, 100);
+    System.out.println(r);
+    
+    assertTrue(r.containsKey(fgi.iv(60,70)));
+
+    System.out.println(ga.getEntries(fgi.chromosome, 1, 100));
+    
+  }
+
   
-//  /**
-//   * @throws java.lang.Exception
-//   */
-//  @Before
-//  public void setUp() throws Exception {
-//  }
-//
-//  /**
-//   * Test method for
-//   * {fr.ens.transcriptome.eoulsan.bio.GenomicArray#addEntry(fr.ens
-//   * .transcriptome.eoulsan.bio.GenomicInterval, java.lang.Object)}.
-//   */
-//  @Test
-//  public void testAddEntry() {
-//    fail("Not yet implemented");
-//  }
-//
-//  /**
-//   * Test method for
-//   * {fr.ens.transcriptome.eoulsan.bio.GenomicArray#getEntries(fr
-//   * .ens.transcriptome.eoulsan.bio.GenomicInterval)}.
-//   */
-//  @Test
-//  public void testGetEntriesGenomicInterval() {
-//    fail("Not yet implemented");
-//  }
-//
-//  /**
-//   * Test method for
-//   * {fr.ens.transcriptome.eoulsan.bio.GenomicArray#getEntries(java.lang.String,
-//   * int, int)}.
-//   */
-//  @Test
-//  public void testGetEntriesStringIntInt() {
-//    fail("Not yet implemented");
-//  }
-//
-//  /**
-//   * Test method for
-//   * {fr.ens.transcriptome.eoulsan.bio.GenomicArray#containsChromosome
-//   * (java.lang.String)}.
-//   */
-//  @Test
-//  public void testContainsChromosome() {
-//    fail("Not yet implemented");
-//  }
+  @Test
+  public void testAddEntry2() {
+
+    System.out.println("=====");
+    
+    GenomicArray<String> ga = new GenomicArray<String>();
+
+    final FastGemomicInterval fgi = new FastGemomicInterval("chr1", '.');
+    
+    
+
+    // Add [10,20]=a
+    GenomicInterval iv1 = fgi.iv(10, 20);
+    ga.addEntry(iv1, "a");
+    
+    Map<GenomicInterval,String> r = ga.getEntries(fgi.chromosome, 1, 100);
+    System.out.println(r);
+    
+    
+    assertTrue(r.containsKey(fgi.iv(10,20)));
+    assertFalse(r.containsKey(fgi.iv(60,70)));
+    
+    // Add [15,25]=a
+    ga.addEntry(fgi.iv(15, 25), "b");
+    
+    r = ga.getEntries(fgi.chromosome, 1, 100);
+    System.out.println(r);
+    
+    assertTrue(r.containsKey(fgi.iv(10,14)));
+    assertTrue(r.containsKey(fgi.iv(15,20)));
+    assertTrue(r.containsKey(fgi.iv(21,25)));
+    
+
+    System.out.println(ga.getEntries(fgi.chromosome, 1, 100));
+    
+  }
+  
+  //@Test
+  public void testAddEntry3() {
+
+    System.out.println("=====");
+    
+    GenomicArray<String> ga = new GenomicArray<String>();
+
+    final FastGemomicInterval fgi = new FastGemomicInterval("chr1", '.');
+    
+    
+
+
+    GenomicInterval iv1 = fgi.iv(15, 20);
+    ga.addEntry(iv1, "a");
+    
+    Map<GenomicInterval,String> r = ga.getEntries(fgi.chromosome, 1, 100);
+    System.out.println(r);
+    
+    
+    assertTrue(r.containsKey(fgi.iv(15,20)));
+    assertFalse(r.containsKey(fgi.iv(60,70)));
+    
+    // Add [15,25]=a
+    ga.addEntry(fgi.iv(5, 10), "b");
+    
+    r = ga.getEntries(fgi.chromosome, 1, 100);
+    System.out.println(r);
+    
+    assertTrue(r.containsKey(fgi.iv(15,20)));
+    assertTrue(r.containsKey(fgi.iv(5,10)));
+  
+    
+
+    System.out.println(ga.getEntries(fgi.chromosome, 1, 100));
+    
+  }
+  
+  
+  //
+  // /**
+  // * Test method for
+  // * {fr.ens.transcriptome.eoulsan.bio.GenomicArray#getEntries(fr
+  // * .ens.transcriptome.eoulsan.bio.GenomicInterval)}.
+  // */
+  // @Test
+  // public void testGetEntriesGenomicInterval() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // /**
+  // * Test method for
+  // *
+  // {fr.ens.transcriptome.eoulsan.bio.GenomicArray#getEntries(java.lang.String,
+  // * int, int)}.
+  // */
+  // @Test
+  // public void testGetEntriesStringIntInt() {
+  // fail("Not yet implemented");
+  // }
+  //
+  // /**
+  // * Test method for
+  // * {fr.ens.transcriptome.eoulsan.bio.GenomicArray#containsChromosome
+  // * (java.lang.String)}.
+  // */
+  // @Test
+  // public void testContainsChromosome() {
+  // fail("Not yet implemented");
+  // }
 
 }
