c7638c5fedac9d384bc60a82e9a85223709e1896
Sandrine Perrin <sperrin@biologie.ens.fr>
2014-01-28 10:28:59 +0100

    Update: comparator directories to used DataSetAnalysis to launch comparison files.

--- src/main/java/fr/ens/transcriptome/eoulsan/io/ComparatorDirectories.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/io/ComparatorDirectories.java
@@ -1,72 +1,153 @@
 package fr.ens.transcriptome.eoulsan.io;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static fr.ens.transcriptome.eoulsan.util.StringUtils.toTimeHumanReadable;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.xml.bind.annotation.XmlElementDecl.GLOBAL;
 
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 
+import fr.ens.transcriptome.eoulsan.EoulsanException;
 import fr.ens.transcriptome.eoulsan.Globals;
 import fr.ens.transcriptome.eoulsan.bio.io.FastqCompareFiles;
 import fr.ens.transcriptome.eoulsan.bio.io.SAMCompareFiles;
+import fr.ens.transcriptome.eoulsan.data.DataFile;
+import fr.ens.transcriptome.eoulsan.data.DataSetAnalysis;
 import fr.ens.transcriptome.eoulsan.util.StringUtils;
 
 /**
  * @author sperrin
  */
-public class Comparator {
+public class ComparatorDirectories {
 
   /** LOGGER */
   private static final Logger LOGGER = Logger.getLogger(Globals.APP_NAME);
 
   private boolean useSerialization = false;
-  private final Stopwatch timer = Stopwatch.createUnstarted();
+  private boolean checkingFilename = false;
 
   private final Collection<String> allExtensionsTreated = Sets.newHashSet();
   private final Set<CompareFiles> typeComparatorFiles = Sets.newHashSet();
 
-  private final Multimap<Boolean, Comparator.ComparatorPairFile> resultComparaison =
+  private final Set<String> filesToNotCompare = Sets.newHashSet();
+  private final Multimap<Boolean, ComparatorDirectories.ComparatorPairFile> resultComparaison =
       HashMultimap.create();
 
+  private final StringBuilder report;
   private int numberComparaison = 0;
 
-  // TODO add in constructor
-  public void configure() {
-    // TODO
-    // retrieve DataGameAnalysis _expected
-    // path directory expected
-  }
+  public void compareDataSet(final DataSetAnalysis dataSetA,
+      final DataSetAnalysis dataSetB) throws EoulsanException {
 
-  public void collect(final String pathFileA, final String pathFileB)
-      throws Exception {
+    final DataSetAnalysis dataSetExpected;
+    final DataSetAnalysis dataSetTested;
 
-    // TODO
-    // set fileA from DGA expected, include in directory source
-    // set fileB from DGA tested
+    if (dataSetA.isExpected()) {
+      dataSetExpected = dataSetA;
+      dataSetTested = dataSetB;
 
-    // TODO use check...
-    if (pathFileA == null
-        || pathFileA.length() == 0 || pathFileB == null
-        || pathFileB.length() == 0) {
-      LOGGER.severe("Pathname(s) file invalid");
-      throw new Exception("Pathname file invalid");
+    } else if (dataSetB.isExpected()) {
+      dataSetExpected = dataSetB;
+      dataSetTested = dataSetA;
+
+    } else {
+      dataSetExpected = dataSetA;
+      dataSetTested = dataSetB;
     }
 
+    parsingDataSet(dataSetExpected, dataSetTested);
+
+  }
+
+  /**
+   * Parse DataSetAnalysis corresponding to directory result Eoulsan and launch
+   * comparison on each pair files.
+   * @param expected DatasetAnalysis represents source directory analysis
+   * @param tested DatasetAnalysis represents test directory analysis
+   */
+  private void parsingDataSet(final DataSetAnalysis expected,
+      final DataSetAnalysis tested) throws EoulsanException {
+
+    LOGGER.info("Start comparison between to result analysis");
+    final Stopwatch timer = Stopwatch.createStarted();
+
+    Map<String, Collection<DataFile>> mapExpected =
+        expected.getAllFilesInAnalysis();
+    Map<String, Collection<DataFile>> mapTested =
+        tested.getAllFilesInAnalysis();
+
+    // Compare size between map which contains all files include in directory
+    if (mapExpected.size() != mapTested.size()) {
+
+      loggerReport("Not same count extension, difference is "
+          + (mapExpected.size() - mapTested.size()) + " relative expected");
+    }
+
+    // Compare per extension type
+    for (Map.Entry<String, Collection<DataFile>> entry : mapExpected.entrySet()) {
+
+      String extension = entry.getKey();
+
+      // Check extension include in set extension treated
+      if (allExtensionsTreated.contains(extension)) {
+
+        Collection<DataFile> filesExpected = entry.getValue();
+        Collection<DataFile> filesTested = mapExpected.get(extension);
+
+        LOGGER.info("Comparison files with extension " + extension);
+
+        // Compare two lists size
+        if (filesExpected.size() != filesTested.size())
+          loggerReport("Not the same size for list files expected and tested");
+
+        for (DataFile fileExpected : filesExpected) {
+          DataFile fileTested = tested.getDataFileSameName(fileExpected);
+
+          if (fileTested == null)
+            loggerReport("File "
+                + fileExpected.getName() + " is missing in test analysis.");
+          else
+            // Launch comparison
+            execute(fileExpected.toFile().getAbsolutePath(), fileTested
+                .toFile().getAbsolutePath());
+        }
+      }
+    }
+
+    timer.stop();
+    loggerReport("all comparison in  "
+        + toTimeHumanReadable(timer.elapsed(TimeUnit.MILLISECONDS)));
+  }
+
+  /**
+   * Compare two files with corresponding CompareFiles from extension files.
+   * @param pathFileA first file to compare, it is the reference
+   * @param pathFileB second file to compare
+   * @throws EoulsanException it occurs if comparison fails
+   */
+  public void execute(final String pathFileA, final String pathFileB)
+      throws EoulsanException {
+
+    checkNotNull(pathFileA, "File " + pathFileA + "doesn't exist");
+    checkNotNull(pathFileB, "File " + pathFileB + "doesn't exist");
+
+    // Check files must be skip
+    if (filesToNotCompare.contains(pathFileA)
+        || filesToNotCompare.contains(pathFileA))
+      return;
+
     ComparatorPairFile comparePairFile =
-        new ComparatorPairFile(pathFileA, pathFileB, this.useSerialization);
+        new ComparatorPairFile(pathFileA, pathFileB, this.useSerialization,
+            this.checkingFilename);
 
     try {
       // Check if extension file included in type list
@@ -83,47 +164,52 @@ public class Comparator {
     }
   }
 
-  public void execute() {
+  public void computeReport() {
 
-    timer.stop();
-    buildFinalReport();
-  }
+    report.append("Comparator param: use serialization file "
+        + useSerialization);
+    report.append("\nComparator type files \n" + typeComparatorFiles);
 
-  public String buildFinalReport() {
-    final StringBuilder sb = new StringBuilder();
+    report.append("\nnumber pair files compared " + this.numberComparaison);
 
-    sb.append("Comparator param: use serialization file " + useSerialization);
-    sb.append("\nComparator type files \n" + typeComparatorFiles);
-
-    sb.append("\n number paire files compare " + this.numberComparaison);
-    sb.append("\n all comparaison in  "
-        + toTimeHumanReadable(this.timer.elapsed(TimeUnit.MILLISECONDS)));
-
-    sb.append("\n number paire files idem "
+    report.append("\n\tnumber pair files idem "
         + this.resultComparaison.get(true).size());
-
-    sb.append("\n number paire files different "
+    report.append("\n\tnumber pair files different "
         + this.resultComparaison.get(false).size());
-    sb.append(" detail false :");
+
+    report.append(" detail false :");
 
     boolean first = true;
+    for (ComparatorDirectories.ComparatorPairFile comp : resultComparaison
+        .get(false)) {
 
-    // for (Comparator.ComparatorPairFile comp : resultComparaison.get(false)) {
-    // if (first) {
-    // sb.append("\ndir "
-    // + comp.getFileA().getParent() + " vs "
-    // + comp.getFileB().getParent());
-    // first = false;
-    // }
-    // sb.append("\n\t" + comp.toString());
-    // // TODO for debug
-    // sb.append("\ndiff " + comp.getPathFileA() + " " + comp.getPathFileB());
-    // }
-    sb.append("\n\t ");
+      if (first) {
+        report.append("\ndir "
+            + comp.getFileA().getParent() + " vs "
+            + comp.getFileB().getParent());
+        first = false;
+      }
 
-    LOGGER.info("final report \n " + sb.toString());
+      report.append("\n\t" + comp.toString());
+      // TODO for debug
+      report
+          .append("\ndiff " + comp.getPathFileA() + " " + comp.getPathFileB());
+    }
+    report.append("\n\t ");
 
-    return sb.toString();
+    LOGGER.info("final report \n " + report.toString());
+
+  }
+
+  /**
+   * Add new entry in logger and save message for final report
+   * @param messag
+   */
+  private void loggerReport(final String messag) {
+    LOGGER.warning(messag);
+
+    report.append("\n");
+    report.append(messag);
   }
 
   public boolean isExtensionTreated(final String ext) {
@@ -133,40 +219,48 @@ public class Comparator {
   //
   // Getter
   //
+  public void setFilesToNotCompare(final String file) {
+    filesToNotCompare.add(file);
+  }
+
+  public void setFilesToNotCompare(final Collection<String> files) {
+    filesToNotCompare.addAll(files);
+  }
+
+  public void setFilesToNotCompare(final String... files) {
+    for (String file : files)
+      filesToNotCompare.add(file);
+  }
 
   //
   // Constructor
   //
 
-  public Comparator(final boolean useSerialization,
-      final String directorySourcePath, final boolean checkingFilename,
-      final boolean initLogger) {
-
-  }
-
   /**
    * @param useSerialization use bloom filter serialized on one file
+   * @throws EoulsanException
    */
-  public Comparator(final boolean useSerialization) {
+  public ComparatorDirectories(final boolean useSerialization,
+      final boolean checkingFilename) throws EoulsanException {
 
     this.useSerialization = useSerialization;
+    this.checkingFilename = checkingFilename;
 
-    LOGGER.config("Comparator param: use serialization file "
-        + useSerialization);
+    this.report = new StringBuilder();
 
     // Build map type files can been compare
     typeComparatorFiles.add(new FastqCompareFiles());
-    typeComparatorFiles.add(new SAMCompareFiles());
+    typeComparatorFiles.add(new SAMCompareFiles("PG"));
     typeComparatorFiles.add(new TextCompareFiles());
+    typeComparatorFiles.add(new LogCompareFiles());
 
     for (CompareFiles comp : typeComparatorFiles) {
       allExtensionsTreated.addAll(comp.getExtensionReaded());
     }
 
+    LOGGER.config("Comparator param: use serialization file "
+        + useSerialization);
     LOGGER.config("Comparator type files \n" + typeComparatorFiles);
-
-    this.timer.start();
-
   }
 
   //
@@ -175,10 +269,8 @@ public class Comparator {
 
   class ComparatorPairFile {
 
-    private final String pathFileA;
-    private final String pathFileB;
-    private final String filenameA;
-    private final String filenameB;
+    private final DataFile dataFileA;
+    private final DataFile dataFileB;
 
     private CompareFiles compareFile = null;
     private final boolean useSerialization;
@@ -193,7 +285,7 @@ public class Comparator {
 
       final Stopwatch timer = Stopwatch.createStarted();
       result =
-          this.compareFile.compareFiles(pathFileA, pathFileB,
+          this.compareFile.compareFiles(getPathFileA(), getPathFileB(),
               this.useSerialization);
 
       LOGGER
@@ -218,27 +310,28 @@ public class Comparator {
     }
 
     public String getPathFileA() {
-      return this.pathFileA;
+      return this.dataFileA.toFile().getAbsolutePath();
     }
 
     public String getPathFileB() {
-      return this.pathFileB;
+      return this.dataFileB.toFile().getAbsolutePath();
     }
 
     public File getFileA() {
-      return new File(this.pathFileA);
+      return dataFileA.toFile();
     }
 
     public File getFileB() {
-      return new File(this.pathFileB);
+      return dataFileB.toFile();
     }
 
     @Override
     public String toString() {
       return "result: "
-          + result + " " + compareFile.getName() + "\t" + this.filenameA + " ("
-          + getLengthFileA() + ") " + " vs \t" + this.filenameB + " ("
-          + getLengthFileB() + ") ";
+          + result + " " + compareFile.getName() + "\t"
+          + this.dataFileA.getName() + " (" + getLengthFileA() + ") "
+          + " vs \t" + this.dataFileB.getName() + " (" + getLengthFileB()
+          + ") ";
     }
 
     public String getLengthFileA() {
@@ -253,22 +346,21 @@ public class Comparator {
     // Constructor
     //
 
-    public ComparatorPairFile(final String pathFileA, final String pathFileB,
-        final boolean useSerialization) {
+    public ComparatorPairFile(final DataFile dataFileA,
+        final DataFile dataFileB, final boolean useSerialization,
+        final boolean checkingFilename) throws EoulsanException {
 
-      this.pathFileA = pathFileA;
-      this.pathFileB = pathFileB;
+      this.dataFileA = dataFileA;
+      this.dataFileB = dataFileB;
+
       this.useSerialization = useSerialization;
 
-      this.filenameA = new File(pathFileA).getName();
-      this.filenameB = new File(pathFileA).getName();
+      if (checkingFilename) {
+        if (!this.dataFileA.getName().equals(this.dataFileB.getName()))
+          throw new EoulsanException("Two files have not same filename.");
+      }
 
-      // if (!filename.equals(new File(pathFileB).getName()))
-      // throw new Exception("FileA are not the same name.");
-
-      final String extensionFile =
-          StringUtils.extension(StringUtils
-              .filenameWithoutCompressionExtension(filenameA));
+      final String extensionFile = this.dataFileA.getExtension();
 
       // Set Comparator file according to extension file
       for (CompareFiles compareFiles : typeComparatorFiles) {
@@ -277,6 +369,23 @@ public class Comparator {
           this.compareFile = compareFiles;
       }
 
+    }
+
+    public ComparatorPairFile(final String pathFileA, final String pathFileB,
+        final boolean useSerialization, final boolean checkingFilename)
+        throws EoulsanException {
+      this(new DataFile(pathFileA), new DataFile(pathFileB), useSerialization,
+          checkingFilename);
+    }
+
+    public ComparatorPairFile(final String pathFileA, final String pathFileB)
+        throws EoulsanException {
+      this(pathFileA, pathFileB, false, true);
+    }
+
+    public ComparatorPairFile(final String pathFileA, final String pathFileB,
+        final boolean useSerialization) throws EoulsanException {
+      this(pathFileA, pathFileB, useSerialization, true);
     }
   }
 }
