6043c9fbe9269334d6a40975c172d1ef9d3e66e8
Sandrine Perrin <sperrin@biologie.ens.fr>
2014-03-20 11:42:39 +0100

    ADD: automatic expected directory if doesn't exists: launch eoulsan with same parameter include test.txt

--- src/main/java/fr/ens/transcriptome/eoulsan/data/DataSetTest.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/data/DataSetTest.java
@@ -53,6 +53,10 @@ public class DataSetTest {
 
   private boolean isExecuted = false;
 
+  /**
+   * @throws EoulsanException
+   * @throws IOException
+   */
   public void executeTest() throws EoulsanException, IOException {
 
     if (isExecuted)
@@ -79,29 +83,18 @@ public class DataSetTest {
 
   }
 
-  public DataSetAnalysis getAnalysisExcepted() throws EoulsanException,
-      IOException {
-    // TODO check
-    if (!isExecuted)
-      executeTest();
-    return this.dsaExpected;
-  }
-
-  public DataSetAnalysis getAnalysisTest() throws EoulsanException, IOException {
-    // TODO check
-    if (!isExecuted)
-      executeTest();
-    return this.dsaTested;
-  }
-
-  public String getFilesToIngore() {
-    return this.props.getProperty(FILES_IGNORED_FOR_COMPARISON_KEY);
-  }
-
   //
   // Private methods
   //
 
+  /**
+   * @param dsa
+   * @param eoulsanPath
+   * @param outputDirectory
+   * @param isDataSetExpected
+   * @throws IOException
+   * @throws EoulsanException
+   */
   private void launchAnalysis(final DataSetAnalysis dsa,
       final File eoulsanPath, final File outputDirectory,
       final boolean isDataSetExpected) throws IOException, EoulsanException {
@@ -109,34 +102,37 @@ public class DataSetTest {
     String script = this.props.getProperty("script_generated_data_expected");
 
     // Data expected must be generated
-    if (isDataSetExpected && script != null && script.trim().length() > 0) {
-      // Check if a script is define in test.txt
-      executeScript(SCRIPT_GENERATED_DATA_EXPECTED_KEY, outputDirectory);
+    if (isDataSetExpected) {
+      if (script == null || script.trim().length() == 0) {
+        // Execute Eoulsan
+        executeEoulsan(dsa, eoulsanPath, outputDirectory);
 
-    } else {
-      // Call Eoulsan
-      checkExistingFile(eoulsanPath, "Eoulsan executable doesn't exists.");
-
-      // Optional scripts
-      executeScript(SCRIPT_PRETREATMENT_KEY, outputDirectory);
-
-      // TODO
-      // Launch eoulsan
-      int exitValue =
-          ProcessUtils.sh(buildCommandLine(dsa, eoulsanPath), outputDirectory);
-
-      // Check exitvalue
-      if (exitValue != 0) {
-        throw new IOException("Bad error result for dataset "
-            + outputDirectory.getAbsolutePath() + " execution: " + exitValue);
+      } else {
+        // Check if a script is define in test.txt
+        executeScript(SCRIPT_GENERATED_DATA_EXPECTED_KEY, outputDirectory);
       }
 
-      // If exists, launch postScript analysis
+    } else {
+      // Generated test directory
+      // Optional script, pre-treatment before launch Eoulsan
+      executeScript(SCRIPT_PRETREATMENT_KEY, outputDirectory);
+
+      // Execute Eoulsan
+      executeEoulsan(dsa, eoulsanPath, outputDirectory);
+
+      // Optional script, post-treatment after Eoulsan and before comparison
+      // between directories
       executeScript(SCRIPT_POSTTREATMENT_KEY, outputDirectory);
 
     }
   }
 
+  /**
+   * @param dsa
+   * @param eoulsanPath
+   * @return
+   * @throws EoulsanException
+   */
   private List<String> buildCommandLine(final DataSetAnalysis dsa,
       final File eoulsanPath) throws EoulsanException {
 
@@ -183,6 +179,38 @@ public class DataSetTest {
     return cmd;
   }
 
+  /**
+   * @param dsa
+   * @param eoulsanPath
+   * @param outputDirectory
+   * @throws IOException
+   * @throws EoulsanException
+   */
+  private void executeEoulsan(final DataSetAnalysis dsa,
+      final File eoulsanPath, final File outputDirectory) throws IOException,
+      EoulsanException {
+
+    // Check file
+    checkExistingFile(eoulsanPath, "Eoulsan executable doesn't exists.");
+    checkExistingDirectoryFile(outputDirectory,
+        outputDirectory.getAbsolutePath());
+
+    // Launch Eoulsan with command line
+    int exitValue =
+        ProcessUtils.sh(buildCommandLine(dsa, eoulsanPath), outputDirectory);
+
+    // Check exitvalue
+    if (exitValue != 0) {
+      throw new IOException("Bad error result for dataset expected "
+          + outputDirectory.getAbsolutePath() + " execution: " + exitValue);
+    }
+  }
+
+  /**
+   * @param script_key
+   * @param outputDirectory
+   * @throws IOException
+   */
   private void executeScript(final String script_key, File outputDirectory)
       throws IOException {
 
@@ -209,6 +237,9 @@ public class DataSetTest {
 
   }
 
+  /**
+   * @throws IOException
+   */
   private void initProperties() throws IOException {
 
     checkExistingFile(testConfigurationFile,
@@ -249,6 +280,25 @@ public class DataSetTest {
   //
   // Getter and Setter
   //
+
+  public DataSetAnalysis getAnalysisExcepted() throws EoulsanException,
+      IOException {
+    // TODO check
+    if (!isExecuted)
+      executeTest();
+    return this.dsaExpected;
+  }
+
+  public DataSetAnalysis getAnalysisTest() throws EoulsanException, IOException {
+    // TODO check
+    if (!isExecuted)
+      executeTest();
+    return this.dsaTested;
+  }
+
+  public String getFilesToIngore() {
+    return this.props.getProperty(FILES_IGNORED_FOR_COMPARISON_KEY);
+  }
 
   public File getExpectedDirectory() {
     return this.expectedDirectory;
