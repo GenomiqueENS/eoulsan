983904fb960f390ff639840f8f68949e15879657
Laurent Jourdren <jourdren@biologie.ens.fr>
2014-04-06 12:34:37 +0200

    ADD: Add stub code to launch integration tests from Maven.

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/it/EoulsanITRuntimeException.java
@@ -0,0 +1,42 @@
+/*
+ *                  Eoulsan development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public License version 2.1 or
+ * later and CeCILL-C. This should be distributed with the code.
+ * If you do not have a copy, see:
+ *
+ *      http://www.gnu.org/licenses/lgpl-2.1.txt
+ *      http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt
+ *
+ * Copyright for this code is held jointly by the Genomic platform
+ * of the Institut de Biologie de l'École Normale Supérieure and
+ * the individual authors. These should be listed in @author doc
+ * comments.
+ *
+ * For more information on the Eoulsan project and its aims,
+ * or to join the Eoulsan Google group, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/eoulsan
+ *
+ */
+
+package fr.ens.transcriptome.eoulsan.it;
+
+import fr.ens.transcriptome.eoulsan.EoulsanRuntimeException;
+
+public class EoulsanITRuntimeException extends EoulsanRuntimeException {
+
+  private static final long serialVersionUID = -1897738767737351557L;
+
+  /**
+   * Constructor.
+   * @param message Exception message
+   */
+  public EoulsanITRuntimeException(final String message) {
+
+    super(message);
+  }
+
+}
