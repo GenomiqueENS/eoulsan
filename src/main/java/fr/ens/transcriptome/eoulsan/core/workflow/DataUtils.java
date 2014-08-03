8013fdbb0dff52391ea76ee0da2d33eab329cc71
Laurent Jourdren <jourdren@biologie.ens.fr>
2014-08-03 22:27:06 +0200

    UPDATE: Add Eoulsan copyright header in files where it was missing.

--- src/main/java/fr/ens/transcriptome/eoulsan/core/workflow/DataUtils.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/core/workflow/DataUtils.java
@@ -1,3 +1,27 @@
+/*
+ *                  Eoulsan development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public License version 2.1 or
+ * later and CeCILL-C. This should be distributed with the code.
+ * If you do not have a copy, see:
+ *
+ *      http://www.gnu.org/licenses/lgpl-2.1.txt
+ *      http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt
+ *
+ * Copyright for this code is held jointly by the Genomic platform
+ * of the Institut de Biologie de l'École Normale Supérieure and
+ * the individual authors. These should be listed in @author doc
+ * comments.
+ *
+ * For more information on the Eoulsan project and its aims,
+ * or to join the Eoulsan Google group, visit the home page
+ * at:
+ *
+ *      http://www.transcriptome.ens.fr/eoulsan
+ *
+ */
+
 package fr.ens.transcriptome.eoulsan.core.workflow;
 
 import java.util.Collections;
@@ -16,7 +40,6 @@ import fr.ens.transcriptome.eoulsan.data.DataFile;
  */
 public final class DataUtils {
 
-
   /**
    * Change the DataFile in a Data object
    * @param data Data object to modify
@@ -27,7 +50,8 @@ public final class DataUtils {
     Preconditions.checkNotNull(data, "data cannot be null");
 
     if (data.isList()) {
-      throw new IllegalArgumentException("data list are not handled by this method");
+      throw new IllegalArgumentException(
+          "data list are not handled by this method");
     }
 
     final DataElement modifiableData = (DataElement) data;
@@ -40,16 +64,18 @@ public final class DataUtils {
    * @param fileIndex file index
    * @param dataFile new DataFile
    */
-  public static void setDataFile(final Data data, final int fileIndex, final DataFile dataFile) {
+  public static void setDataFile(final Data data, final int fileIndex,
+      final DataFile dataFile) {
 
     Preconditions.checkNotNull(data, "data cannot be null");
 
     if (data.isList()) {
-      throw new IllegalArgumentException("data list are not handled by this method");
+      throw new IllegalArgumentException(
+          "data list are not handled by this method");
     }
 
     final DataElement modifiableData = (DataElement) data;
-    modifiableData.setDataFile(fileIndex,  dataFile);
+    modifiableData.setDataFile(fileIndex, dataFile);
   }
 
   /**
@@ -71,5 +97,6 @@ public final class DataUtils {
   // Private constructor
   //
 
-  private DataUtils() {}
+  private DataUtils() {
+  }
 }
