729b5119d25ebb3a117c9ee27af264e9765557d2
Laurent Jourdren <jourdren@biologie.ens.fr>
2014-08-17 21:26:37 +0200

    ADD: A new class and its test junit test class that define workflow file nomenclature: FileNaming

--- src/main/java/fr/ens/transcriptome/eoulsan/core/workflow/FileNaming.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/core/workflow/FileNaming.java
@@ -28,10 +28,15 @@ import static com.google.common.base.CharMatcher.inRange;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.io.File;
+
 import com.google.common.base.CharMatcher;
 
+import fr.ens.transcriptome.eoulsan.EoulsanRuntimeException;
+import fr.ens.transcriptome.eoulsan.data.Data;
 import fr.ens.transcriptome.eoulsan.data.DataFile;
 import fr.ens.transcriptome.eoulsan.data.DataFormat;
+import fr.ens.transcriptome.eoulsan.data.DataFormatRegistry;
 import fr.ens.transcriptome.eoulsan.io.CompressionType;
 
 /**
@@ -42,24 +47,274 @@ import fr.ens.transcriptome.eoulsan.io.CompressionType;
 public class FileNaming {
 
   private static final char SEPARATOR = '_';
+  private static final String FILE_INDEX_PREFIX = "file";
+  private static final String PART_INDEX_PREFIX = "part";
+
   private static final CharMatcher ASCII_LETTER_OR_DIGIT = inRange('a', 'z')
       .or(inRange('A', 'Z')).or(inRange('0', '9'));
 
+  private String stepId;
+  private String portName;
+  private String dataName;
+  private DataFormat format;
+  private int fileIndex = -1;
+  private int part = -1;;
+  private CompressionType compression = CompressionType.NONE;
+
+  //
+  // Getters
+  //
+
   /**
-   * Create the prefix of a file.
+   * Get Step Id.
+   * @return the step Id
+   */
+  public String getStepId() {
+    return stepId;
+  }
+
+  /**
+   * Get the port name.
+   * @return the port name
+   */
+  public String getPortName() {
+    return portName;
+  }
+
+  /**
+   * Get the data name.
+   * @return the data name
+   */
+  public String getDataName() {
+    return dataName;
+  }
+
+  /**
+   * Get the format.
+   * @return the format
+   */
+  public DataFormat getFormat() {
+    return format;
+  }
+
+  /**
+   * Get the file index.
+   * @return the file index
+   */
+  public int getFileIndex() {
+    return fileIndex;
+  }
+
+  /**
+   * Get the file part.
+   * @return the file part
+   */
+  public int getPart() {
+    return part;
+  }
+
+  /**
+   * Get the compression.
+   * @return the compression
+   */
+  public CompressionType getCompression() {
+    return compression;
+  }
+
+  //
+  // Setters
+  //
+
+  /**
+   * Set the step id.
+   * @param stepId the step id
+   */
+  public void setStepId(final String stepId) {
+
+    checkStepId(stepId);
+    this.stepId = stepId;
+  }
+
+  /**
+   * Set the port name.
+   * @param portName the port name
+   */
+  public void setPortName(final String portName) {
+
+    checkPortName(portName);
+    this.portName = portName;
+  }
+
+  /**
+   * Set the data name.
+   * @param dataName the data name
+   */
+  public void setDataName(final String dataName) {
+
+    checkDataName(dataName);
+    this.dataName = dataName;
+  }
+
+  /**
+   * Set the format.
+   * @param format the format
+   */
+  public void setFormat(final DataFormat format) {
+
+    checkNotNull(format, "format argument cannot be null");
+    this.format = format;
+  }
+
+  /**
+   * Set the file index.
+   * @param fileIndex the file index
+   */
+  public void setFileIndex(final int fileIndex) {
+
+    this.fileIndex = fileIndex < 0 ? -1 : fileIndex;
+  }
+
+  /**
+   * Set the part number.
+   * @param part the part number
+   */
+  public void setPart(final int part) {
+
+    this.part = part < 0 ? -1 : part;
+  }
+
+  /**
+   * Set the compression
+   * @param compression the compression type
+   */
+  public void setCompression(final CompressionType compression) {
+
+    checkNotNull(compression, "compression argument cannot be null");
+    this.compression = compression;
+  }
+
+  /**
+   * Set several field of the object from a workflow output port.
+   * @param port the workflow output port
+   */
+  private void set(final WorkflowOutputPort port) {
+
+    checkNotNull(port, "port argument cannot be null");
+
+    setStepId(port.getStep().getId());
+    setFormat(port.getFormat());
+    setCompression(port.getCompression());
+  }
+
+  /**
+   * Set several field of the object from a Data object.
+   * @param port the data object
+   */
+  private void set(final Data data) {
+
+    checkNotNull(data, "port argument cannot be null");
+
+    setDataName(data.getName());
+    setPart(data.getPart());
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * Return the file prefix.
+   * @return a string with the file prefix
+   */
+  public String filePrefix() {
+
+    checkNotNull(this.stepId, "stepId has not been set");
+    checkNotNull(this.portName, "portName has not been set");
+    checkNotNull(this.format, "format has not been set");
+
+    return filePrefix(this.stepId, this.portName, this.format.getPrefix());
+  }
+
+  /**
+   * Return the file suffix.
+   * @return q string with the file suffix
+   */
+  public String fileSuffix() {
+
+    checkNotNull(this.format, "format has not been set");
+    checkNotNull(this.compression, "compression has not been set");
+
+    return fileSuffix(this.format.getDefaultExtention(),
+        this.compression.getExtension());
+  }
+
+  /**
+   * Return the middle string of the filename.
+   * @return a string with the middle string of the filename
+   */
+  public String fileMiddle() {
+
+    checkNotNull(this.dataName, "datName has not been set");
+
+    checkFormatAndFileIndex();
+
+    return fileMiddle(this.dataName, this.fileIndex, this.part);
+  }
+
+  /**
+   * Return the filename.
+   * @return a string with the filename
+   */
+  public String filename() {
+
+    checkNotNull(this.stepId, "stepId has not been set");
+    checkNotNull(this.portName, "portName has not been set");
+    checkNotNull(this.format, "format has not been set");
+    checkNotNull(this.dataName, "datName has not been set");
+    checkNotNull(this.compression, "compression has not been set");
+
+    checkFormatAndFileIndex();
+
+    return filename(this.stepId, this.portName, this.format, this.dataName,
+        this.fileIndex, this.part, this.compression);
+  }
+
+  /**
+   * Check if the file index is valid for the current format
+   */
+  private void checkFormatAndFileIndex() {
+
+    if (this.format.getMaxFilesCount() == 1) {
+      checkArgument(this.fileIndex == -1,
+          "Invalid fileIndex argument for format "
+              + this.format.getName() + ": " + this.fileIndex);
+    } else {
+      checkArgument(fileIndex < format.getMaxFilesCount(),
+          "Invalid fileIndex argument for format "
+              + this.format.getName() + ": " + this.fileIndex);
+    }
+
+  }
+
+  //
+  // Static methods
+  //
+
+  /**
+   * Create the prefix of a filename.
    * @param port output port that generate the file
    * @return a String with the prefix of the file
    */
   public static String filePrefix(final WorkflowOutputPort port) {
 
-    checkNotNull(port, "port argument cannot be null");
+    final FileNaming f = new FileNaming();
+    f.set(port);
 
-    return filePrefix(port.getStep().getId(), port.getName(), port.getFormat()
-        .getPrefix());
+    return f.filePrefix();
   }
 
   /**
-   * Create the prefix of a file.
+   * Create the prefix of a filename.
    * @param stepId step id
    * @param portName port name
    * @param format format of the file
@@ -68,13 +323,16 @@ public class FileNaming {
   public static String filePrefix(final String stepId, final String portName,
       final DataFormat format) {
 
-    checkNotNull(format, "format argument cannot be null");
+    final FileNaming f = new FileNaming();
+    f.setStepId(stepId);
+    f.setPortName(portName);
+    f.setFormat(format);
 
-    return filePrefix(stepId, portName, format.getPrefix());
+    return f.filePrefix();
   }
 
   /**
-   * Create the prefix of a file.
+   * Create the prefix of a filename.
    * @param stepId step id
    * @param portName port name
    * @param formatPrefix format prefix of the file
@@ -105,39 +363,96 @@ public class FileNaming {
   }
 
   //
-  // Suffix creation
+  // Middle creation
   //
 
   /**
-   * Create the suffix of a file.
+   * Create the middle of a filename.
    * @param port output port that generate the file
    * @param data the data
    * @param fileIndex file index
    * @return a String with the suffix of a file
    */
-  public static String fileSuffix(final WorkflowOutputPort port,
+  public static String fileMiddle(final WorkflowOutputPort port,
       final DataElement data, final int fileIndex) {
 
-    checkNotNull(data, "data argument cannot be null");
+    final FileNaming f = new FileNaming();
+    f.set(port);
+    f.set(data);
+    f.setFileIndex(fileIndex);
 
-    return fileSuffix(port, data.getName(), fileIndex, data.getPart());
+    return f.fileMiddle();
   }
 
   /**
-   * Create the suffix of a file.
+   * Create the middle of a filename.
    * @param port output port that generate the file
    * @param fileIndex file index
    * @param part file part
    * @return a String with the suffix of a file
    */
-  public static String fileSuffix(final WorkflowOutputPort port,
+  public static String fileMiddle(final WorkflowOutputPort port,
       final String dataName, final int fileIndex, final int part) {
 
-    checkNotNull(port, "port argument cannot be null");
+    final FileNaming f = new FileNaming();
+    f.set(port);
+    f.setDataName(dataName);
+    f.setPart(part);
+    f.setFileIndex(fileIndex);
 
-    return fileSuffix(dataName, fileIndex, part, port.getFormat(),
-        port.getCompression());
+    return f.fileMiddle();
+  }
 
+  /**
+   * Create the middle of a filename.
+   * @param dataName data name
+   * @param fileIndex file index
+   * @param part file part
+   * @param extension file extension
+   * @param compression file compression
+   * @return a String with the suffix of a file
+   */
+  public static String fileMiddle(final String dataName, final int fileIndex,
+      final int part) {
+
+    checkDataName(dataName);
+
+    StringBuilder sb = new StringBuilder();
+
+    // Set the name of the date
+    sb.append(dataName);
+
+    // Set the file index if needed
+    if (fileIndex >= 0) {
+      sb.append(SEPARATOR);
+      sb.append(FILE_INDEX_PREFIX);
+      sb.append(fileIndex);
+    }
+
+    if (part > -1) {
+      sb.append(SEPARATOR);
+      sb.append(PART_INDEX_PREFIX);
+      sb.append(part);
+    }
+
+    return sb.toString();
+  }
+
+  //
+  // Suffix creation
+  //
+
+  /**
+   * Create the suffix of a filename.
+   * @param port a workflow port
+   * @return a string with the suffix that correspond to the filename
+   */
+  public static String fileSuffix(final WorkflowOutputPort port) {
+
+    final FileNaming f = new FileNaming();
+    f.set(port);
+
+    return f.fileSuffix();
   }
 
   /**
@@ -149,23 +464,14 @@ public class FileNaming {
    * @param compression file compression
    * @return a String with the suffix of a file
    */
-  public static String fileSuffix(final String dataName, final int fileIndex,
-      final int part, final DataFormat format, final CompressionType compression) {
+  public static String fileSuffix(final DataFormat format,
+      final CompressionType compression) {
 
-    checkNotNull(format, "format argument cannot be null");
-    checkNotNull(compression, "compression argument cannot be null");
+    final FileNaming f = new FileNaming();
+    f.setFormat(format);
+    f.setCompression(compression);
 
-    if (format.getMaxFilesCount() == 1) {
-      checkArgument(fileIndex == -1, "Invalid fileIndex argument for format "
-          + format.getName() + ": " + fileIndex);
-    } else {
-      checkArgument(fileIndex < format.getMaxFilesCount(),
-          "Invalid fileIndex argument for format "
-              + format.getName() + ": " + fileIndex);
-    }
-
-    return fileSuffix(dataName, fileIndex, part, format.getDefaultExtention(),
-        compression.getExtension());
+    return f.fileSuffix();
   }
 
   /**
@@ -177,30 +483,13 @@ public class FileNaming {
    * @param compression file compression
    * @return a String with the suffix of a file
    */
-  public static String fileSuffix(final String dataName, final int fileIndex,
-      final int part, final String extension, final String compression) {
+  public static String fileSuffix(final String extension,
+      final String compression) {
 
-    checkDataName(dataName);
     checkExtension(extension);
     checkCompression(compression);
 
     StringBuilder sb = new StringBuilder();
-
-    // Set the name of the date
-    sb.append(dataName);
-
-    // Set the file index if needed
-    if (fileIndex >= 0) {
-      sb.append(SEPARATOR);
-      sb.append("file");
-      sb.append(fileIndex);
-    }
-
-    if (part > -1) {
-      sb.append(SEPARATOR);
-      sb.append("part");
-      sb.append(part);
-    }
 
     // Set the extension
     sb.append(extension);
@@ -218,16 +507,145 @@ public class FileNaming {
   //
 
   /**
+   * Create the filename from several parameters.
+   * @param stepId the step id
+   * @param portName the port name
+   * @param format the format
+   * @param dataName the data name
+   * @param fileIndex the file index
+   * @param part the part
+   * @param compression the compression type
+   * @return a string with the filename
+   */
+  public static String filename(final String stepId, final String portName,
+      final DataFormat format, final String dataName, final int fileIndex,
+      final int part, final CompressionType compression) {
+
+    return filePrefix(stepId, portName, format)
+        + fileMiddle(dataName, fileIndex, part)
+        + fileSuffix(format, compression);
+  }
+
+  /**
    * Create the name of a data file of the workflow.
    * @param port port that generate the data
    * @param data data
    * @param fileIndex file index
    * @return a String with the name of the file
    */
-  public static String name(final WorkflowOutputPort port,
+  public static String filename(final WorkflowOutputPort port,
       final DataElement data, final int fileIndex) {
 
-    return filePrefix(port) + fileSuffix(port, data, fileIndex);
+    FileNaming f = new FileNaming();
+    f.set(port);
+    f.set(data);
+    f.setFileIndex(fileIndex);
+
+    return f.filename();
+  }
+
+  //
+  // File name parsing
+  //
+
+  /**
+   * Create a FileNaming object from a File object.
+   * @param file the file
+   * @return a new FileNaming object
+   */
+  public static FileNaming parse(final File file) {
+
+    checkNotNull(file, "file argument cannot be null");
+
+    return parse(file.getName());
+  }
+
+  /**
+   * Create a FileNaming object from a DataFile object.
+   * @param file the file
+   * @return a new FileNaming object
+   */
+  public static FileNaming parse(final DataFile file) {
+
+    checkNotNull(file, "file argument cannot be null");
+
+    return parse(file.getName());
+  }
+
+  /**
+   * Create a FileNaming object from a filename.
+   * @param file the filename
+   * @return a new FileNaming object
+   */
+  public static FileNaming parse(final String filename) {
+
+    checkNotNull(filename, "filename argument cannot be null");
+
+    final FileNaming result = new FileNaming();
+
+    final String[] extensions = filename.split("\\.");
+
+    if (extensions.length < 2 || extensions.length > 3) {
+      throw new EoulsanRuntimeException("Invalid filename: " + filename);
+    }
+
+    // Get format extension
+    final String formatExtension = '.' + extensions[1];
+
+    // Get compression
+    if (extensions.length == 3) {
+      result.setCompression(CompressionType
+          .getCompressionTypeByExtension('.' + extensions[2]));
+    }
+
+    final String[] fields = extensions[0].split("_");
+
+    if (fields.length < 4) {
+      throw new EoulsanRuntimeException("Invalid filename: " + filename);
+    }
+
+    result.setStepId(fields[0]);
+    result.setPortName(fields[1]);
+
+    result.setFormat(DataFormatRegistry.getInstance()
+        .getDataFormatFromFilename(fields[2], formatExtension));
+
+    result.setDataName(fields[3]);
+
+    for (int i = 4; i < fields.length; i++) {
+
+      if (fields[i].startsWith(FILE_INDEX_PREFIX)) {
+
+        if (result.getFileIndex() != -1) {
+          throw new EoulsanRuntimeException("Invalid filename: " + filename);
+        }
+        try {
+          result.setFileIndex(Integer.parseInt(fields[i]
+              .substring(FILE_INDEX_PREFIX.length())));
+        } catch (NumberFormatException e) {
+          throw new EoulsanRuntimeException("Invalid filename: " + filename);
+        }
+      } else if (fields[i].startsWith(PART_INDEX_PREFIX)) {
+
+        if (result.getPart() != -1) {
+          throw new EoulsanRuntimeException("Invalid filename: " + filename);
+        }
+        try {
+          result.setPart(Integer.parseInt(fields[i].substring(PART_INDEX_PREFIX
+              .length())));
+        } catch (NumberFormatException e) {
+          throw new EoulsanRuntimeException("Invalid filename: " + filename);
+        }
+      }
+
+    }
+
+    if (result.getFormat().getMaxFilesCount() > 1
+        && result.getFileIndex() == -1) {
+      throw new EoulsanRuntimeException("Invalid filename: " + filename);
+    }
+
+    return result;
   }
 
   //
@@ -244,8 +662,8 @@ public class FileNaming {
   public static DataFile file(final WorkflowOutputPort port,
       final DataElement data, final int fileIndex) {
 
-    return new DataFile(port.getStep().getStepWorkingDir(), name(port, data,
-        fileIndex));
+    return new DataFile(port.getStep().getStepWorkingDir(), filename(port,
+        data, fileIndex));
   }
 
   //
@@ -301,6 +719,46 @@ public class FileNaming {
 
     return !(name == null || name.isEmpty() || !ASCII_LETTER_OR_DIGIT
         .matchesAllOf(name));
+  }
+
+  /**
+   * Test if a filename is valid.
+   * @param file the file to test.
+   * @return true if the filename is valid
+   */
+  public static final boolean isFilenameValid(final DataFile file) {
+
+    checkNotNull(file, "file argument cannot be null");
+
+    return isFilenameValid(file.getName());
+  }
+
+  /**
+   * Test if a filename is valid.
+   * @param file the file to test.
+   * @return true if the filename is valid
+   */
+  public static final boolean isFilenameValid(final File file) {
+
+    checkNotNull(file, "file argument cannot be null");
+
+    return isFilenameValid(file.getName());
+  }
+
+  /**
+   * Test if a filename is valid.
+   * @param filename the file to test.
+   * @return true if the filename is valid
+   */
+  public static final boolean isFilenameValid(final String filename) {
+
+    try {
+      FileNaming.parse(filename);
+    } catch (EoulsanRuntimeException e) {
+      return false;
+    }
+
+    return true;
   }
 
   /**
