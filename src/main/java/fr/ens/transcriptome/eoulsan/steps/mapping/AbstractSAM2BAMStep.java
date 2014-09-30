1e007468a0dcaa32da3b454828b2a60dc531ced8
Laurent Jourdren <jourdren@biologie.ens.fr>
2014-09-30 16:35:36 +0200

    ADD: Add the local mode sam2bam step.

---
+++ src/main/java/fr/ens/transcriptome/eoulsan/steps/mapping/AbstractSAM2BAMStep.java
@@ -0,0 +1,99 @@
+package fr.ens.transcriptome.eoulsan.steps.mapping;
+
+import static fr.ens.transcriptome.eoulsan.core.InputPortsBuilder.singleInputPort;
+import static fr.ens.transcriptome.eoulsan.data.DataFormats.MAPPER_RESULTS_BAM;
+import static fr.ens.transcriptome.eoulsan.data.DataFormats.MAPPER_RESULTS_INDEX_BAI;
+import static fr.ens.transcriptome.eoulsan.data.DataFormats.MAPPER_RESULTS_SAM;
+
+import java.util.Set;
+
+import fr.ens.transcriptome.eoulsan.EoulsanException;
+import fr.ens.transcriptome.eoulsan.Globals;
+import fr.ens.transcriptome.eoulsan.core.InputPorts;
+import fr.ens.transcriptome.eoulsan.core.OutputPorts;
+import fr.ens.transcriptome.eoulsan.core.OutputPortsBuilder;
+import fr.ens.transcriptome.eoulsan.core.Parameter;
+import fr.ens.transcriptome.eoulsan.steps.AbstractStep;
+import fr.ens.transcriptome.eoulsan.util.Version;
+
+/**
+ * This class define a step for converting SAM files into BAM.
+ * @since 2.0
+ * @author Laurent Jourdren
+ */
+public abstract class AbstractSAM2BAMStep extends AbstractStep {
+
+  private static final String STEP_NAME = "sam2bam";
+
+  protected static final String COUNTER_GROUP = "sam2bam";
+
+  private int compressionLevel;
+
+  //
+  // Getters
+  //
+
+  /**
+   * Get the compression level to use.
+   * @return the compression level to use
+   */
+  protected int getCompressionLevel() {
+    return this.compressionLevel;
+  }
+
+  //
+  // Step methods
+  //
+
+  @Override
+  public String getName() {
+
+    return STEP_NAME;
+  }
+
+  @Override
+  public String getDescription() {
+
+    return "This step sam convert SAM files to BAM files.";
+  }
+
+  @Override
+  public Version getVersion() {
+
+    return Globals.APP_VERSION;
+  }
+
+  @Override
+  public InputPorts getInputPorts() {
+    return singleInputPort(MAPPER_RESULTS_SAM);
+  }
+
+  @Override
+  public OutputPorts getOutputPorts() {
+
+    return new OutputPortsBuilder().addPort("bam", MAPPER_RESULTS_BAM)
+        .addPort("bai", MAPPER_RESULTS_INDEX_BAI).create();
+  }
+
+  @Override
+  public void configure(final Set<Parameter> stepParameters)
+      throws EoulsanException {
+
+    for (Parameter p : stepParameters) {
+
+      if ("compression.level".equals(p.getName())) {
+
+        final int level = p.getIntValue();
+        if (level < 0 || level > 9)
+          throw new EoulsanException("Invalid compression level [0-9]: "
+              + level + " step: " + p.getName());
+
+        this.compressionLevel = level;
+      } else {
+        throw new EoulsanException("Unknown parameter for "
+            + getName() + " step: " + p.getName());
+      }
+    }
+  }
+
+}
---
+++ src/main/java/fr/ens/transcriptome/eoulsan/steps/mapping/local/SAM2BAMLocalStep.java
@@ -0,0 +1,121 @@
+package fr.ens.transcriptome.eoulsan.steps.mapping.local;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.sf.samtools.SAMFileHeader.SortOrder;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMFileWriterFactory;
+import net.sf.samtools.SAMRecord;
+import fr.ens.transcriptome.eoulsan.annotations.LocalOnly;
+import fr.ens.transcriptome.eoulsan.core.StepContext;
+import fr.ens.transcriptome.eoulsan.core.StepResult;
+import fr.ens.transcriptome.eoulsan.core.StepStatus;
+import fr.ens.transcriptome.eoulsan.data.Data;
+import fr.ens.transcriptome.eoulsan.data.DataFile;
+import fr.ens.transcriptome.eoulsan.data.DataFormats;
+import fr.ens.transcriptome.eoulsan.steps.mapping.AbstractSAM2BAMStep;
+import fr.ens.transcriptome.eoulsan.util.LocalReporter;
+import fr.ens.transcriptome.eoulsan.util.Reporter;
+
+/**
+ * This class define a step for converting SAM files into BAM.
+ * @since 2.0
+ * @author Laurent Jourdren
+ */
+@LocalOnly
+public class SAM2BAMLocalStep extends AbstractSAM2BAMStep {
+
+  @Override
+  public StepResult execute(final StepContext context, final StepStatus status) {
+
+    try {
+
+      // Create the reporter
+      final Reporter reporter = new LocalReporter();
+
+      // Get input SAM data
+      final Data inData = context.getInputData(DataFormats.MAPPER_RESULTS_SAM);
+
+      // Get output BAM data
+      final Data outBAMData =
+          context.getOutputData(DataFormats.MAPPER_RESULTS_BAM, inData);
+
+      // Get output BAM data
+      final Data outBAIData =
+          context.getOutputData(DataFormats.MAPPER_RESULTS_INDEX_BAI, inData);
+
+      final DataFile samFile = inData.getDataFile();
+      final DataFile bamFile = outBAMData.getDataFile();
+      final DataFile bamIndexFile = outBAIData.getDataFile();
+
+      convert(samFile, bamFile, bamIndexFile, getCompressionLevel(), reporter,
+          context.getSettings().getTempDirectoryFile());
+
+      // Set the description of the context
+      status.setDescription("Convert alignments ("
+          + inData.getName() + ", " + samFile.getName() + ", "
+          + bamFile.getName() + "," + bamIndexFile.getName() + ")");
+
+      // Add counters for this sample to log file
+      status.setCounters(reporter, COUNTER_GROUP);
+
+      return status.createStepResult();
+
+    } catch (IOException e) {
+
+      return status.createStepResult(e);
+    }
+  }
+
+  /**
+   * Convert SAM file to sorted BAM with picard
+   * @param samDataFile input SAM file
+   * @param bamDataFile output SAM file
+   * @param bamIndexDataFile output index file
+   * @param compressionLevel compression level
+   * @param reporter reporter
+   * @param tmpDir temporary directory
+   * @throws IOException if an error occurs
+   */
+  private static final void convert(final DataFile samDataFile,
+      final DataFile bamDataFile, final DataFile bamIndexDataFile,
+      int compressionLevel, final Reporter reporter, final File tmpDir)
+      throws IOException {
+
+    checkArgument(compressionLevel >= 0 && compressionLevel <= 9,
+        "Invalid compression level [0-9]: " + compressionLevel);
+
+    // Open sam file
+    final SAMFileReader samReader = new SAMFileReader(samDataFile.open());
+
+    // Forse sort
+    samReader.getFileHeader().setSortOrder(SortOrder.coordinate);
+
+    // Get Bam file
+    final File bamFile = bamDataFile.toFile();
+
+    // Open Bam file
+    final SAMFileWriter samWriter =
+        new SAMFileWriterFactory()
+            .setCreateIndex(true)
+            .setTempDirectory(tmpDir)
+            .makeBAMWriter(samReader.getFileHeader(), false, bamFile,
+                compressionLevel);
+
+    for (SAMRecord samRecord : samReader) {
+      samRecord.setReadName("");
+      samRecord.setReadString("");
+      samRecord.setBaseQualityString("");
+      samWriter.addAlignment(samRecord);
+      reporter.incrCounter(COUNTER_GROUP, "sorted records", 1);
+    }
+
+    samReader.close();
+    samWriter.close();
+  }
+
+}
