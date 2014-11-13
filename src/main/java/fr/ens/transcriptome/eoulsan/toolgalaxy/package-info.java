02eb7afc721a8c438f3f466c3649a8611dccf0f8
Sandrine Perrin <sperrin@biologie.ens.fr>
2014-11-13 13:36:00 +0100

    DOC: add header in java file and package-info.

--- src/main/java/fr/ens/transcriptome/eoulsan/toolgalaxy/package-info.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/toolgalaxy/package-info.java
@@ -22,18 +22,10 @@
  *
  */
 
+/**
+ * This package contain classes related to read tool xml file from galaxy.
+ * @since 2.0
+ */
+
 package fr.ens.transcriptome.eoulsan.toolgalaxy;
 
-public interface ToolElement {
-
-  public final static String SEP = ".";
-
-  String getName();
-
-  void setParameterEoulsan(final String value);
-
-  boolean isSetting();
-
-  String getParameterEoulsan();
-
-}
