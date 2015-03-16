61b2feb01ad84a45e05cfc240c38601bca6f6f9f
Laurent Jourdren <jourdren@biologie.ens.fr>
2015-03-16 15:13:29 +0100

    FIX: In GenomeChecker, when an IOException is throw by GenomeDescriptionCreator.createGenomeDescription(), now add the exception message in the EoulsanException thrown.

--- src/main/java/fr/ens/transcriptome/eoulsan/checkers/GenomeChecker.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/checkers/GenomeChecker.java
@@ -100,7 +100,7 @@ public class GenomeChecker implements Checker {
 
     } catch (IOException e) {
       throw new EoulsanException("Error while reading genome "
-          + genomeFile.getSource() + " for checking");
+          + genomeFile.getSource() + " for checking: " + e.getMessage());
     } catch (BadBioEntryException e) {
       throw new EoulsanException("Found bad read entry in genome "
           + genomeFile.getSource() + ": " + e.getMessage());
