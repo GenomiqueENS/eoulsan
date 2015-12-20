6fed5d37243749cfaeb799843beba05a800d6f51
Laurent Jourdren <jourdren@biologie.ens.fr>
2015-12-20 16:12:02 +0100

    UPDATE: In CommonHadoop, remove unused methods.

--- src/main/java/fr/ens/transcriptome/eoulsan/core/CommonHadoop.java
+++ src/main/java/fr/ens/transcriptome/eoulsan/core/CommonHadoop.java
@@ -24,19 +24,11 @@
 
 package fr.ens.transcriptome.eoulsan.core;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 
-import fr.ens.transcriptome.eoulsan.Common;
 import fr.ens.transcriptome.eoulsan.EoulsanRuntime;
 import fr.ens.transcriptome.eoulsan.Globals;
 import fr.ens.transcriptome.eoulsan.Settings;
-import fr.ens.transcriptome.eoulsan.util.hadoop.PathUtils;
 
 /**
  * This class define common constants and other methods specific to Hadoop mode.
@@ -58,107 +50,6 @@ public class CommonHadoop {
 
   public static final String COUNTER_GROUP_KEY =
       Globals.PARAMETER_PREFIX + ".counter.group";
-
-  /**
-   * Retrieve the genome file name from the files of a directory
-   * @param basePath Base path directory
-   * @param conf Hadoop configuration
-   * @return the genome file path
-   * @throws IOException if the genome file can't be identified
-   */
-  // public static Path getGenomeFilePath(final Path basePath,
-  // final Configuration conf) throws IOException {
-  //
-  // if (basePath == null)
-  // throw new NullPointerException("Base path is null");
-  //
-  // final List<Path> genomePaths =
-  // PathUtils.listPathsBySuffix(basePath, Common.FASTA_EXTENSION, conf);
-  // if (genomePaths.size() == 0)
-  // throw new IOException("Genome file not found.");
-  // if (genomePaths.size() > 1)
-  // throw new IOException("More than one genome file found.");
-  //
-  // return genomePaths.get(0);
-  // }
-
-  /**
-   * Write log data.
-   * @param logPath Path of the log file
-   * @param data data to write
-   * @throws IOException if an error occurs while writing log file
-   */
-  public static void writeLog(final Path logPath, final long startTime,
-      final String data) throws IOException {
-
-    final FileSystem fs = logPath.getFileSystem(new Configuration());
-    Common.writeLog(fs.create(logPath), startTime, data);
-  }
-
-  /**
-   * Return the path of entry data either a directory or a file with an
-   * extension
-   * @param path the path of the data
-   * @param extension the extension of the file
-   * @return the path of the data
-   * @throws IOException if an error occurs while testing files
-   */
-  public static Path selectDirectoryOrFile(final Path path,
-      final String extension) throws IOException {
-
-    final Configuration conf = new Configuration();
-
-    if (PathUtils.isExistingDirectoryFile(path, conf)) {
-      return path;
-    }
-
-    final Path filePath =
-        new Path(path.getParent(), path.getName() + extension);
-
-    if (PathUtils.isFile(filePath, conf)) {
-      return filePath;
-    }
-
-    return null;
-  }
-
-  /**
-   * Print the contents of a Configuration object
-   * @param conf Configuration object
-   */
-  public static void printConfiguration(final Configuration conf) {
-
-    printConfiguration(conf, null);
-  }
-
-  /**
-   * Print the contents of a Configuration object
-   * @param conf Configuration object
-   * @param prefix prefix used for keys filtering
-   */
-  public static void printConfiguration(final Configuration conf,
-      final String prefix) {
-
-    if (conf == null) {
-      return;
-    }
-
-    final Iterator<Map.Entry<String, String>> it = conf.iterator();
-
-    System.out.println("=== Start print configuration ===");
-
-    while (it.hasNext()) {
-
-      final Map.Entry<String, String> e = it.next();
-
-      if (prefix == null || e.getKey().startsWith(prefix)) {
-        System.out.println(e.getKey() + "\t" + e.getValue());
-      }
-
-    }
-    System.out.println("=== End print configuration ===");
-
-  }
 
   /**
    * Create a new Configuration object from Eoulsan runtime settings.
