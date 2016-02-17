39283c99f06bc5f7d7ae1f424afa9da123013023
Laurent Jourdren <jourdren@biologie.ens.fr>
2016-02-17 18:18:12 +0100

    UPDATE: Rename the DEseq2Executor class to DEseq2. UPDATE: DESeq2Step and DESeq2 classes now use the RExecutor system to execute R code.

--- src/main/java/fr/ens/biologie/genomique/eoulsan/steps/diffana/local/DESeq2Step.java
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/steps/diffana/local/DESeq2Step.java
@@ -26,13 +26,15 @@ package fr.ens.biologie.genomique.eoulsan.steps.diffana.local;
 
 import static fr.ens.biologie.genomique.eoulsan.core.InputPortsBuilder.DEFAULT_SINGLE_INPUT_PORT_NAME;
 import static fr.ens.biologie.genomique.eoulsan.data.DataFormats.EXPRESSION_RESULTS_TSV;
-import static fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2Executor.FitType.PARAMETRIC;
-import static fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2Executor.SizeFactorsType.RATIO;
-import static fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2Executor.StatisticTest.WALD;
+import static fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2.FitType.PARAMETRIC;
+import static fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2.SizeFactorsType.RATIO;
+import static fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2.StatisticTest.WALD;
+import static java.util.Collections.unmodifiableSet;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -50,12 +52,14 @@ import fr.ens.biologie.genomique.eoulsan.data.Data;
 import fr.ens.biologie.genomique.eoulsan.design.Design;
 import fr.ens.biologie.genomique.eoulsan.design.Experiment;
 import fr.ens.biologie.genomique.eoulsan.design.ExperimentMetadata;
+import fr.ens.biologie.genomique.eoulsan.requirements.Requirement;
 import fr.ens.biologie.genomique.eoulsan.steps.AbstractStep;
-import fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2Executor;
-import fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2Executor.FitType;
-import fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2Executor.SizeFactorsType;
-import fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2Executor.StatisticTest;
+import fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2;
+import fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2.FitType;
+import fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2.SizeFactorsType;
+import fr.ens.biologie.genomique.eoulsan.steps.diffana.DEseq2.StatisticTest;
 import fr.ens.biologie.genomique.eoulsan.util.Version;
+import fr.ens.biologie.genomique.eoulsan.util.r.RExecutor;
 
 /**
  * Class to run the differential analysis with DEseq2
@@ -68,6 +72,9 @@ public class DESeq2Step extends AbstractStep {
 
   // Step name
   private static final String STEP_NAME = "deseq2";
+
+  static final String DESEQ2_DOCKER_IMAGE =
+      "bioconductor/release_sequencing:3.1";
 
   // DEseq2 options names
   private static final String NORMALIZATION_FIGURES = "norm.fig";
@@ -86,6 +93,9 @@ public class DESeq2Step extends AbstractStep {
   private SizeFactorsType sizeFactorsType = RATIO;
   private FitType fitType = PARAMETRIC;
   private StatisticTest statisticTest = WALD;
+
+  private Set<Requirement> requirements = new HashSet<>();
+  private RExecutor executor;
 
   //
   // Step methods
@@ -106,15 +116,27 @@ public class DESeq2Step extends AbstractStep {
   @Override
   public InputPorts getInputPorts() {
 
-    return new InputPortsBuilder().addPort(DEFAULT_SINGLE_INPUT_PORT_NAME,
-        true, EXPRESSION_RESULTS_TSV).create();
+    return new InputPortsBuilder()
+        .addPort(DEFAULT_SINGLE_INPUT_PORT_NAME, true, EXPRESSION_RESULTS_TSV)
+        .create();
+  }
+
+  @Override
+  public Set<Requirement> getRequirements() {
+
+    return unmodifiableSet(this.requirements);
   }
 
   @Override
   public void configure(final StepConfigurationContext context,
       final Set<Parameter> stepParameters) throws EoulsanException {
 
-    for (Parameter p : stepParameters) {
+    // Parse R executor parameters
+    final Set<Parameter> parameters = new HashSet<>(stepParameters);
+    this.executor = CommonConfiguration.parseRExecutorParameter(context,
+        parameters, this.requirements, DESEQ2_DOCKER_IMAGE);
+
+    for (Parameter p : parameters) {
 
       switch (p.getName()) {
 
@@ -135,21 +157,21 @@ public class DESeq2Step extends AbstractStep {
         break;
 
       case SIZE_FACTORS_TYPE:
-        this.sizeFactorsType = DEseq2Executor.SizeFactorsType.get(p);
+        this.sizeFactorsType = DEseq2.SizeFactorsType.get(p);
         break;
 
       case FIT_TYPE:
-        this.fitType = DEseq2Executor.FitType.get(p.getStringValue());
+        this.fitType = DEseq2.FitType.get(p.getStringValue());
         break;
 
       case STATISTIC_TEST:
         this.statisticTest =
-            DEseq2Executor.StatisticTest.get(p.getStringValue());
+            DEseq2.StatisticTest.get(p.getStringValue());
         break;
 
       default:
-        throw new EoulsanException("Unkown parameter for step "
-            + getName() + " : " + p.getName());
+        throw new EoulsanException(
+            "Unkown parameter for step " + getName() + " : " + p.getName());
       }
     }
   }
@@ -178,16 +200,11 @@ public class DESeq2Step extends AbstractStep {
   }
 
   @Override
-  public StepResult execute(final StepContext context, final StepStatus status) {
+  public StepResult execute(final StepContext context,
+      final StepStatus status) {
 
     // Get the design
     final Design design = context.getWorkflow().getDesign();
-
-    // Get the output directory
-    final File outputDir = context.getStepOutputDirectory().toFile();
-
-    // Get the temporary directory
-    final File tempDir = context.getLocalTempDirectory();
 
     // Get the experiment data
     final Data expressionData = context.getInputData(EXPRESSION_RESULTS_TSV);
@@ -202,9 +219,6 @@ public class DESeq2Step extends AbstractStep {
 
     try {
 
-      // Extract R scripts
-      DEseq2Executor.extractRScripts(tempDir);
-
       for (Experiment e : design.getExperiments()) {
 
         // Do nothing if the experiment is skipped
@@ -214,9 +228,10 @@ public class DESeq2Step extends AbstractStep {
         }
 
         // run DEseq2
-        new DEseq2Executor(stepId, design, e, sampleFiles, outputDir, tempDir,
-            normFig, diffanaFig, normDiffana, diffana, sizeFactorsType,
-            fitType, statisticTest).runDEseq2();
+        new DEseq2(this.executor, stepId, design, e, sampleFiles,
+            this.normFig, this.diffanaFig, this.normDiffana, this.diffana,
+            this.sizeFactorsType, this.fitType, this.statisticTest,
+            context.getSettings().isSaveRscripts()).runDEseq2();
 
       }
     } catch (IOException | EoulsanException e) {
