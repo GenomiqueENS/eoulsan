ef54c8862315cddb4fd962156ae965fa300e9b59
Laurent Jourdren <jourdren@biologie.ens.fr>
2016-03-01 17:35:01 +0100

    ADD: Add support for PBS Pro, SLURM and TORQUE schedulers. UPDATE: Rename bpipe-htcondor script to bpipe-htcondor.sh.

---
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/core/schedulers/clusters/SLURMTaskScheduler.java
@@ -0,0 +1,24 @@
+package fr.ens.biologie.genomique.eoulsan.core.schedulers.clusters;
+
+/**
+ * This class define a SLURM cluster scheduler using a Bpipe script.
+ * @author Laurent Jourdren
+ * @since 2.0
+ */
+public class SLURMTaskScheduler extends BundledScriptBpipeTaskScheduler {
+
+  public static final String SCHEDULER_NAME = "slurm";
+  private static final String COMMAND_WRAPPER_SCRIPT = "bpipe-slurm.sh";
+
+  //
+  // Constructor
+  //
+
+  /**
+   * Constructor.
+   */
+  public SLURMTaskScheduler() {
+    super(SCHEDULER_NAME, COMMAND_WRAPPER_SCRIPT);
+  }
+
+}
---
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/core/schedulers/clusters/TORQUETaskScheduler.java
@@ -0,0 +1,24 @@
+package fr.ens.biologie.genomique.eoulsan.core.schedulers.clusters;
+
+/**
+ * This class define a TORQUE cluster scheduler using a Bpipe script.
+ * @author Laurent Jourdren
+ * @since 2.0
+ */
+public class TORQUETaskScheduler extends BundledScriptBpipeTaskScheduler {
+
+  public static final String SCHEDULER_NAME = "torque";
+  private static final String COMMAND_WRAPPER_SCRIPT = "bpipe-torque.sh";
+
+  //
+  // Constructor
+  //
+
+  /**
+   * Constructor.
+   */
+  public TORQUETaskScheduler() {
+    super(SCHEDULER_NAME, COMMAND_WRAPPER_SCRIPT);
+  }
+
+}
