7ea55b4aa7927d28130deb391467ce5714bfbc18
Laurent Jourdren <jourdren@biologie.ens.fr>
2016-03-17 17:13:09 +0100

    UPDATE: Various small fixes using IDEA inspect tool.

--- src/main/java/fr/ens/biologie/genomique/eoulsan/modules/expression/hadoop/HTSeqCountMapper.java
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/modules/expression/hadoop/HTSeqCountMapper.java
@@ -39,9 +39,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
