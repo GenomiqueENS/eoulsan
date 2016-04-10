19106062c25ed9cb98230611527fc6218764887c
Laurent Jourdren <jourdren@biologie.ens.fr>
2016-04-10 21:18:16 +0200

    UPDATE: In the galaxytools.executorinterpreters, refactor code to avoid duplicate code and use everywhere the SimpleProcess API.

--- src/main/java/fr/ens/biologie/genomique/eoulsan/galaxytools/executorinterpreters/DockerExecutorInterpreter.java
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/galaxytools/executorinterpreters/DockerExecutorInterpreter.java
@@ -1,15 +1,11 @@
 package fr.ens.biologie.genomique.eoulsan.galaxytools.executorinterpreters;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.io.File;
 import java.util.List;
 
 import com.google.common.base.Objects;
-import com.spotify.docker.client.DockerException;
 
-import fr.ens.biologie.genomique.eoulsan.galaxytools.ToolExecutorResult;
 import fr.ens.biologie.genomique.eoulsan.util.SimpleProcess;
 import fr.ens.biologie.genomique.eoulsan.util.StringUtils;
 import fr.ens.biologie.genomique.eoulsan.util.docker.DockerSimpleProcess;
@@ -19,7 +15,7 @@ import fr.ens.biologie.genomique.eoulsan.util.docker.DockerSimpleProcess;
  * @author Laurent Jourdren
  * @since 2.0
  */
-public class DockerExecutorInterpreter implements ExecutorInterpreter {
+public class DockerExecutorInterpreter extends AbstractExecutorInterpreter {
 
   public static final String INTERPRETER_NAME = "docker";
 
@@ -40,36 +36,10 @@ public class DockerExecutorInterpreter implements ExecutorInterpreter {
   }
 
   @Override
-  public ToolExecutorResult execute(final List<String> commandLine,
-      final File executionDirectory, final File temporaryDirectory,
-      final File stdoutFile, final File stderrFile) {
+  protected SimpleProcess newSimpleProcess() {
 
-    checkNotNull(commandLine, "commandLine argument cannot be null");
-    checkNotNull(executionDirectory,
-        "executionDirectory argument cannot be null");
-    checkNotNull(stdoutFile, "stdoutFile argument cannot be null");
-    checkNotNull(stderrFile, "stderrFile argument cannot be null");
-
-    checkArgument(executionDirectory.isDirectory(),
-        "execution directory does not exists or is not a directory: "
-            + executionDirectory.getAbsolutePath());
-
-    try {
-
-      SimpleProcess process = new DockerSimpleProcess(this.dockerImage);
-      final int exitValue = process.execute(commandLine, executionDirectory,
-          temporaryDirectory, stdoutFile, stderrFile);
-
-      return new ToolExecutorResult(commandLine, exitValue);
-
-    } catch (DockerException | InterruptedException e) {
-      return new ToolExecutorResult(commandLine, e);
-    }
+    return new DockerSimpleProcess(this.dockerImage);
   }
-
-  //
-  // Docker methods
-  //
 
   //
   // Object methods
