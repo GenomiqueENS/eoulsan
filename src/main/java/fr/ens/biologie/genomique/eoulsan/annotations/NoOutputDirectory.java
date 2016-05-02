cba2618684ecbe61cde43d56ba95b630ff0e6045
Laurent Jourdren <jourdren@biologie.ens.fr>
2016-05-02 12:34:30 +0200

    FIX: The annotation class NoOutputDirectory has not been addedd to the git repository.

---
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/annotations/NoOutputDirectory.java
@@ -0,0 +1,14 @@
+package fr.ens.biologie.genomique.eoulsan.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * This annotation is used to mark a step class that requires no output
+ * directory.
+ * @since 2.0
+ * @author Laurent Jourdren
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoOutputDirectory {
+}
