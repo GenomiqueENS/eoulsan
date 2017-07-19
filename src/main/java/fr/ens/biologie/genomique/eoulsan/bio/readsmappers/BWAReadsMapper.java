cb91ecc26a370e1cdc442945c593325a8c75a2f4
Laurent Jourdren <jourdren@biologie.ens.fr>
2017-07-19 20:43:40 +0200

    FIX: Fix BWA mem wrapper, the previous code cannot work as there is not need for "bwa samse/sampe" with "bwa mem".

--- src/main/java/fr/ens/biologie/genomique/eoulsan/bio/readsmappers/BWAReadsMapper.java
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/bio/readsmappers/BWAReadsMapper.java
@@ -30,6 +30,7 @@ import static fr.ens.biologie.genomique.eoulsan.bio.FastqFormat.FASTQ_ILLUMINA_1
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.google.common.collect.Lists;
@@ -202,280 +203,351 @@ public class BWAReadsMapper extends AbstractSequenceReadsMapper {
   private MapperProcess createMapperProcessSE(final String bwaPath,
       final String indexPath) throws IOException {
 
+    // Get the BWA algorithm to use
+    boolean bwaAln = !"mem".equals(getMapperFlavorToUse());
+
+    if (bwaAln) {
+
+      // BWA aln
+      return new MapperProcess(this, false) {
+
+        private File saiFile;
+        private File fastqFile;
+        private FastqWriterThread writer;
+
+        protected void additionalInit() throws IOException {
+
+          final File tmpDir = EoulsanRuntime.getRuntime().getTempDirectory();
+
+          final String uuid = getUUID();
+
+          this.saiFile =
+              new File(tmpDir, PREFIX_FILES + "-sai-" + uuid + SAI_EXTENSION);
+
+          this.fastqFile = new File(tmpDir,
+              PREFIX_FILES + "-fastq-" + uuid + FASTQ_EXTENSION);
+
+          // Create named pipes
+          FileUtils.createNamedPipe(this.saiFile);
+
+          // Add fastq copy file and sai file to files to remove
+          addFilesToRemove(saiFile, this.fastqFile);
+
+          // Create FASTQ writer
+          this.writer = new FastqWriterThread(this.fastqFile,
+              "BWA sampe writeFirstPairEntries thread");
+        }
+
+        @Override
+        public void writeEntry(final String name, final String sequence,
+            final String quality) throws IOException {
+
+          super.writeEntry(name, sequence, quality);
+          this.writer
+              .write(ReadSequence.toFastQ(name, sequence, quality) + '\n');
+        }
+
+        @Override
+        public void writeEntry1(final ReadSequence read) throws IOException {
+
+          super.writeEntry1(read);
+          this.writer.write(read.toFastQ() + '\n');
+        }
+
+        @Override
+        public void closeEntriesWriter() throws IOException {
+
+          super.closeWriter1();
+          this.writer.close();
+        }
+
+        @Override
+        public void closeWriter1() throws IOException {
+
+          super.closeWriter1();
+          this.writer.close();
+        }
+
+        @Override
+        protected List<List<String>> createCommandLines() {
+
+          final boolean illuminaFastq = getFastqFormat() == FASTQ_ILLUMINA
+              || getFastqFormat() == FASTQ_ILLUMINA_1_5;
+
+          final List<String> cmd1 = new ArrayList<>();
+          cmd1.add(bwaPath);
+
+          // Select the flavor/algorithm to use
+          cmd1.add("aln");
+
+          if (illuminaFastq) {
+            cmd1.add("-I");
+          }
+
+          // Set the user options
+          cmd1.addAll(getListMapperArguments());
+
+          cmd1.add("-t");
+          cmd1.add(getThreadsNumber() + "");
+          cmd1.add("-f");
+          cmd1.add(this.saiFile.getAbsolutePath());
+          cmd1.add(indexPath);
+          cmd1.add(getNamedPipeFile1().getAbsolutePath());
+
+          final List<String> cmd2 = new ArrayList<>();
+
+          // Build the command line
+          cmd2.add(bwaPath);
+          cmd2.add("samse");
+          cmd2.add(indexPath);
+          cmd2.add(this.saiFile.getAbsolutePath());
+          cmd2.add(this.fastqFile.getAbsolutePath());
+
+          final List<List<String>> result = new ArrayList<>();
+          result.add(cmd1);
+          result.add(cmd2);
+
+          return result;
+        }
+
+      };
+    }
+
+    // BWA mem
     return new MapperProcess(this, false) {
-
-      private File saiFile;
-      private File fastqFile;
-      private FastqWriterThread writer;
-
-      protected void additionalInit() throws IOException {
-
-        final File tmpDir = EoulsanRuntime.getRuntime().getTempDirectory();
-
-        final String uuid = getUUID();
-
-        this.saiFile =
-            new File(tmpDir, PREFIX_FILES + "-sai-" + uuid + SAI_EXTENSION);
-
-        this.fastqFile =
-            new File(tmpDir, PREFIX_FILES + "-fastq-" + uuid + FASTQ_EXTENSION);
-
-        // Create named pipes
-        FileUtils.createNamedPipe(this.saiFile);
-
-        // Add fastq copy file and sai file to files to remove
-        addFilesToRemove(saiFile, this.fastqFile);
-
-        // Create FASTQ writer
-        this.writer = new FastqWriterThread(this.fastqFile,
-            "BWA sampe writeFirstPairEntries thread");
-      }
-
-      @Override
-      public void writeEntry(final String name, final String sequence,
-          final String quality) throws IOException {
-
-        super.writeEntry(name, sequence, quality);
-        this.writer.write(ReadSequence.toFastQ(name, sequence, quality) + '\n');
-      }
-
-      @Override
-      public void writeEntry1(final ReadSequence read) throws IOException {
-
-        super.writeEntry1(read);
-        this.writer.write(read.toFastQ() + '\n');
-      }
-
-      @Override
-      public void closeEntriesWriter() throws IOException {
-
-        super.closeWriter1();
-        this.writer.close();
-      }
-
-      @Override
-      public void closeWriter1() throws IOException {
-
-        super.closeWriter1();
-        this.writer.close();
-      }
 
       @Override
       protected List<List<String>> createCommandLines() {
 
-        final boolean illuminaFastq = getFastqFormat() == FASTQ_ILLUMINA
-            || getFastqFormat() == FASTQ_ILLUMINA_1_5;
-
-        final List<String> cmd1 = new ArrayList<>();
-        cmd1.add(bwaPath);
+        final List<String> cmd = new ArrayList<>();
+        cmd.add(bwaPath);
 
         // Select the flavor/algorithm to use
-        if ("mem".equals(getMapperFlavorToUse())) {
-          cmd1.add("mem");
-        } else {
-          cmd1.add("aln");
-        }
-
-        if (illuminaFastq) {
-          cmd1.add("-I");
-        }
+        cmd.add("mem");
 
         // Set the user options
-        cmd1.addAll(getListMapperArguments());
+        cmd.addAll(getListMapperArguments());
 
-        cmd1.add("-t");
-        cmd1.add(getThreadsNumber() + "");
-        cmd1.add("-f");
-        cmd1.add(this.saiFile.getAbsolutePath());
-        cmd1.add(indexPath);
-        cmd1.add(getNamedPipeFile1().getAbsolutePath());
+        cmd.add("-t");
+        cmd.add(getThreadsNumber() + "");
 
-        final List<String> cmd2 = new ArrayList<>();
+        // Set the index path
+        cmd.add(indexPath);
 
-        // Build the command line
-        cmd2.add(bwaPath);
-        cmd2.add("samse");
-        cmd2.add(indexPath);
-        cmd2.add(this.saiFile.getAbsolutePath());
-        cmd2.add(this.fastqFile.getAbsolutePath());
+        // Set the input file
+        cmd.add(getNamedPipeFile1().getAbsolutePath());
 
-        final List<List<String>> result = new ArrayList<>();
-        result.add(cmd1);
-        result.add(cmd2);
-
-        return result;
+        return Collections.singletonList(cmd);
       }
 
     };
-
   }
 
   private MapperProcess createMapperProcessPE(final String bwaPath,
       final String indexPath) throws IOException {
 
-    return new MapperProcess(this, true) {
+    // Get the BWA algorithm to use
+    boolean bwaAln = !"mem".equals(getMapperFlavorToUse());
 
-      private File saiFile1;
-      private File saiFile2;
-      private File fastqFile1;
-      private File fastqFile2;
-      private FastqWriterThread writer1;
-      private FastqWriterThread writer2;
+    if (bwaAln) {
 
-      protected void additionalInit() throws IOException {
+      // BWA aln
+      return new MapperProcess(this, true) {
 
-        final File tmpDir = EoulsanRuntime.getRuntime().getTempDirectory();
+        private File saiFile1;
+        private File saiFile2;
+        private File fastqFile1;
+        private File fastqFile2;
+        private FastqWriterThread writer1;
+        private FastqWriterThread writer2;
 
-        final String uuid = getUUID();
+        protected void additionalInit() throws IOException {
 
-        this.saiFile1 =
-            new File(tmpDir, PREFIX_FILES + "-sai1-" + uuid + SAI_EXTENSION);
-        this.saiFile2 =
-            new File(tmpDir, PREFIX_FILES + "-sai2-" + uuid + SAI_EXTENSION);
+          final File tmpDir = EoulsanRuntime.getRuntime().getTempDirectory();
 
-        this.fastqFile1 = new File(tmpDir,
-            PREFIX_FILES + "-fastq1-" + uuid + FASTQ_EXTENSION);
+          final String uuid = getUUID();
 
-        this.fastqFile2 = new File(tmpDir,
-            PREFIX_FILES + "-fastq2-" + uuid + FASTQ_EXTENSION);
+          this.saiFile1 =
+              new File(tmpDir, PREFIX_FILES + "-sai1-" + uuid + SAI_EXTENSION);
+          this.saiFile2 =
+              new File(tmpDir, PREFIX_FILES + "-sai2-" + uuid + SAI_EXTENSION);
 
-        // Create named pipes
-        FileUtils.createNamedPipe(this.saiFile1);
-        FileUtils.createNamedPipe(this.saiFile2);
+          this.fastqFile1 = new File(tmpDir,
+              PREFIX_FILES + "-fastq1-" + uuid + FASTQ_EXTENSION);
 
-        // Add fastq copy file and sai file to files to remove
-        addFilesToRemove(this.saiFile1, this.saiFile2, this.fastqFile1,
-            this.fastqFile2);
+          this.fastqFile2 = new File(tmpDir,
+              PREFIX_FILES + "-fastq2-" + uuid + FASTQ_EXTENSION);
 
-        // Create writer on FASTQ files
-        this.writer1 = new FastqWriterThread(this.fastqFile1,
-            "BWA sampe writeFirstPairEntries thread");
-        this.writer2 = new FastqWriterThread(this.fastqFile2,
-            "BWA sampe writeSecondPairEntries thread");
-      }
+          // Create named pipes
+          FileUtils.createNamedPipe(this.saiFile1);
+          FileUtils.createNamedPipe(this.saiFile2);
 
-      @Override
-      public void writeEntry(final String name1, final String sequence1,
-          final String quality1, final String name2, final String sequence2,
-          final String quality2) throws IOException {
+          // Add fastq copy file and sai file to files to remove
+          addFilesToRemove(this.saiFile1, this.saiFile2, this.fastqFile1,
+              this.fastqFile2);
 
-        super.writeEntry(name1, sequence1, quality1, name2, sequence2,
-            quality2);
-        this.writer1
-            .write(ReadSequence.toFastQ(name1, sequence1, quality2) + '\n');
-        this.writer2
-            .write(ReadSequence.toFastQ(name2, sequence2, quality2) + '\n');
-      }
-
-      @Override
-      public void writeEntry1(final ReadSequence read) throws IOException {
-
-        super.writeEntry1(read);
-        this.writer1.write(read.toFastQ() + '\n');
-      }
-
-      @Override
-      public void writeEntry2(final ReadSequence read) throws IOException {
-
-        super.writeEntry2(read);
-        this.writer2.write(read.toFastQ() + '\n');
-      }
-
-      @Override
-      public void closeEntriesWriter() throws IOException {
-
-        super.closeWriter1();
-        super.closeWriter2();
-        this.writer1.close();
-        this.writer2.close();
-      }
-
-      @Override
-      public void closeWriter1() throws IOException {
-
-        super.closeWriter1();
-        this.writer1.close();
-      }
-
-      @Override
-      public void closeWriter2() throws IOException {
-
-        super.closeWriter2();
-        this.writer2.close();
-      }
-
-      @Override
-      protected List<List<String>> createCommandLines() {
-
-        final boolean illuminaFastq = getFastqFormat() == FASTQ_ILLUMINA
-            || getFastqFormat() == FASTQ_ILLUMINA_1_5;
-
-        final List<String> cmd1 = new ArrayList<>();
-        cmd1.add(bwaPath);
-
-        // Select the flavor/algorithm to use
-        if ("mem".equals(getMapperFlavorToUse())) {
-          cmd1.add("mem");
-        } else {
-          cmd1.add("aln");
+          // Create writer on FASTQ files
+          this.writer1 = new FastqWriterThread(this.fastqFile1,
+              "BWA sampe writeFirstPairEntries thread");
+          this.writer2 = new FastqWriterThread(this.fastqFile2,
+              "BWA sampe writeSecondPairEntries thread");
         }
 
-        if (illuminaFastq) {
-          cmd1.add("-I");
+        @Override
+        public void writeEntry(final String name1, final String sequence1,
+            final String quality1, final String name2, final String sequence2,
+            final String quality2) throws IOException {
+
+          super.writeEntry(name1, sequence1, quality1, name2, sequence2,
+              quality2);
+          this.writer1
+              .write(ReadSequence.toFastQ(name1, sequence1, quality2) + '\n');
+          this.writer2
+              .write(ReadSequence.toFastQ(name2, sequence2, quality2) + '\n');
         }
 
-        // There are 2 bwa aln processes in paired-end mode, so we divided the
-        // number of threads by 2
-        final int threadNumber =
-            getThreadsNumber() > 1 ? getThreadsNumber() / 2 : 1;
+        @Override
+        public void writeEntry1(final ReadSequence read) throws IOException {
 
-        // Set the user options
-        cmd1.addAll(getListMapperArguments());
-
-        cmd1.add("-t");
-        cmd1.add(threadNumber + "");
-        cmd1.add("-f");
-        cmd1.add(this.saiFile1.getAbsolutePath());
-        cmd1.add(indexPath);
-        cmd1.add(getNamedPipeFile1().getAbsolutePath());
-
-        final List<String> cmd2 = new ArrayList<>();
-        cmd2.add(bwaPath);
-        cmd2.add("aln");
-        if (illuminaFastq) {
-          cmd2.add("-I");
+          super.writeEntry1(read);
+          this.writer1.write(read.toFastQ() + '\n');
         }
 
-        // Set the user options
-        cmd2.addAll(getListMapperArguments());
+        @Override
+        public void writeEntry2(final ReadSequence read) throws IOException {
 
-        cmd2.add("-t");
-        cmd2.add(threadNumber + "");
-        cmd2.add("-f");
-        cmd2.add(this.saiFile2.getAbsolutePath());
-        cmd2.add(indexPath);
-        cmd2.add(getNamedPipeFile2().getAbsolutePath());
+          super.writeEntry2(read);
+          this.writer2.write(read.toFastQ() + '\n');
+        }
 
-        final List<String> cmd3 = new ArrayList<>();
+        @Override
+        public void closeEntriesWriter() throws IOException {
 
-        // Build the command line
-        cmd3.add(bwaPath);
-        cmd3.add("sampe");
-        cmd3.add(indexPath);
-        cmd3.add(this.saiFile1.getAbsolutePath());
-        cmd3.add(this.saiFile2.getAbsolutePath());
-        cmd3.add(this.fastqFile1.getAbsolutePath());
-        cmd3.add(this.fastqFile2.getAbsolutePath());
+          super.closeWriter1();
+          super.closeWriter2();
+          this.writer1.close();
+          this.writer2.close();
+        }
 
-        final List<List<String>> result = new ArrayList<>();
-        result.add(cmd1);
-        result.add(cmd2);
-        result.add(cmd3);
+        @Override
+        public void closeWriter1() throws IOException {
 
-        return result;
-      }
+          super.closeWriter1();
+          this.writer1.close();
+        }
 
-    };
+        @Override
+        public void closeWriter2() throws IOException {
+
+          super.closeWriter2();
+          this.writer2.close();
+        }
+
+        @Override
+        protected List<List<String>> createCommandLines() {
+
+          final boolean illuminaFastq = getFastqFormat() == FASTQ_ILLUMINA
+              || getFastqFormat() == FASTQ_ILLUMINA_1_5;
+
+          final List<String> cmd1 = new ArrayList<>();
+          cmd1.add(bwaPath);
+
+          // Select the flavor/algorithm to use
+          if ("mem".equals(getMapperFlavorToUse())) {
+            cmd1.add("mem");
+          } else {
+            cmd1.add("aln");
+          }
+
+          if (illuminaFastq) {
+            cmd1.add("-I");
+          }
+
+          // There are 2 bwa aln processes in paired-end mode, so we divided the
+          // number of threads by 2
+          final int threadNumber =
+              getThreadsNumber() > 1 ? getThreadsNumber() / 2 : 1;
+
+          // Set the user options
+          cmd1.addAll(getListMapperArguments());
+
+          cmd1.add("-t");
+          cmd1.add(threadNumber + "");
+          cmd1.add("-f");
+          cmd1.add(this.saiFile1.getAbsolutePath());
+          cmd1.add(indexPath);
+          cmd1.add(getNamedPipeFile1().getAbsolutePath());
+
+          final List<String> cmd2 = new ArrayList<>();
+          cmd2.add(bwaPath);
+          cmd2.add("aln");
+          if (illuminaFastq) {
+            cmd2.add("-I");
+          }
+
+          // Set the user options
+          cmd2.addAll(getListMapperArguments());
+
+          cmd2.add("-t");
+          cmd2.add(threadNumber + "");
+          cmd2.add("-f");
+          cmd2.add(this.saiFile2.getAbsolutePath());
+          cmd2.add(indexPath);
+          cmd2.add(getNamedPipeFile2().getAbsolutePath());
+
+          final List<String> cmd3 = new ArrayList<>();
+
+          // Build the command line
+          cmd3.add(bwaPath);
+          cmd3.add("sampe");
+          cmd3.add(indexPath);
+          cmd3.add(this.saiFile1.getAbsolutePath());
+          cmd3.add(this.saiFile2.getAbsolutePath());
+          cmd3.add(this.fastqFile1.getAbsolutePath());
+          cmd3.add(this.fastqFile2.getAbsolutePath());
+
+          final List<List<String>> result = new ArrayList<>();
+          result.add(cmd1);
+          result.add(cmd2);
+          result.add(cmd3);
+
+          return result;
+        }
+
+      };
+
+    } else {
+
+      // BWA mem
+      return new MapperProcess(this, true) {
+
+        @Override
+        protected List<List<String>> createCommandLines() {
+
+          final List<String> cmd = new ArrayList<>();
+          cmd.add(bwaPath);
+
+          // Select the flavor/algorithm to use
+          cmd.add("mem");
+
+          // Set the user options
+          cmd.addAll(getListMapperArguments());
+
+          cmd.add("-t");
+          cmd.add(getThreadsNumber() + "");
+
+          // Set the index path
+          cmd.add(indexPath);
+
+          // Set the input files
+          cmd.add(getNamedPipeFile1().getAbsolutePath());
+          cmd.add(getNamedPipeFile2().getAbsolutePath());
+
+          return Collections.singletonList(cmd);
+        }
+
+      };
+    }
   }
 
   //
