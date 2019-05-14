3a259e29b3df6715755d21eab17094e54b901c24
Laurent Jourdren <jourdren@biologie.ens.fr>
2019-05-14 23:01:27 +0200

    FIX: Fix import in Utils class.

--- src/main/java/fr/ens/biologie/genomique/eoulsan/util/Utils.java
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/util/Utils.java
@@ -35,6 +35,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Objects;
 
 /**
  * This class contains some useful methods about collection, hashcode
