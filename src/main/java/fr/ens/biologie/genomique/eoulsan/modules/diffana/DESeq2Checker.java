68560e5b904b4312076f081b19319c2d878359d1
Charlotte Berthelier <bertheli@biologie.ens.fr>
2019-08-29 16:07:02 +0200

    Creation of unit tests in the checkExperimentDesign class

--- src/main/java/fr/ens/biologie/genomique/eoulsan/modules/diffana/DESeq2Checker.java
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/modules/diffana/DESeq2Checker.java
@@ -24,6 +24,7 @@ public class DESeq2Checker {
      * @param experiment experiment to check
      * @throws EoulsanException if the experiment design is not correct
      */
+
     static boolean checkExperimentDesign(Experiment experiment) throws EoulsanException {
         return checkExperimentDesign (experiment,true);
     }
@@ -35,12 +36,48 @@ public class DESeq2Checker {
      * @param throwsException if true throw an exception
      * @throws EoulsanException if the experiment design is not correct
      */
+
     static boolean checkExperimentDesign(Experiment experiment, boolean throwsException) throws EoulsanException {
 
         requireNonNull(experiment, "Experiment argument cannot be null");
         final ExperimentMetadata emd = experiment.getMetadata();
-        // Check the comparison string, if exists it is a complex design
+        // Get the name of all keys
+        List<String> metakeys = DesignUtils.getExperimentSampleAllMetadataKeys(experiment);
+        List<String> keys = getAllSamplesMetadataKeys(experiment.getDesign());
+        List<String> allkeys= new ArrayList<>(metakeys);
+        allkeys.addAll(keys);
+
+        /* Check if there is an empty cell in the experiment */
+        // For every key, check if there is no empty data
+        for (String key : allkeys) {
+            for (ExperimentSample es : experiment.getExperimentSamples()) {
+                String value = DesignUtils.getMetadata(es, key);
+                if(value.equals(null) || value.equals("")){
+                    return error("There is an empty cell" + experiment.getName(), throwsException);
+                }
+            }
+        }
+
+        /* Check if the column Condition is missing for the experiment */
+        if (!getExperimentSampleAllMetadataKeys(experiment)
+                .contains(CONDITION_KEY)
+                && !getAllSamplesMetadataKeys(experiment.getDesign()).contains(CONDITION_KEY)) {
+            return error("Condition column missing for experiment: "
+                    + experiment.getName(), throwsException);
+        }
+
+        /* Check if the column RepTechGroup is missing for the experiment */
+        if (!getExperimentSampleAllMetadataKeys(experiment)
+                .contains(REP_TECH_GROUP_KEY)
+                && !getAllSamplesMetadataKeys(experiment.getDesign()).contains(REP_TECH_GROUP_KEY)) {
+            return error("RepTechGroup column missing for experiment: "
+                    + experiment.getName(), throwsException);
+        }
+
+        /* Check if the comparison string is correct */
+        // If there is a comparison string, it is a complex design
         if (emd.containsComparisons()) {
+            List<String> comparisionNames = new ArrayList<String>();
             // Check if the comparison value is correct
             for (String c : emd.getComparisons().split(";")) {
                 String[] splitC = c.split(":");
@@ -50,21 +87,23 @@ public class DESeq2Checker {
                             + experiment.getName()
                             + " experiment, comparison cannot have more than 1 value: " + c, throwsException);
                 }
-
-                // Get the name of all Metadata keys
-                String[] metakeys = DesignUtils.getExperimentSampleAllMetadataKeys(experiment).toArray(new String[0]);
+                // Get the name of each comparison
+                for (String string : splitC){
+                    if (!string.contains("_vs_")){
+                        comparisionNames.add(string);
+                    }
+                }
                 // Get each condition in the comparison string
                 String[] listConditionsInComparisonString = splitC[1].split("(%)|(_vs_)");
-                // Get every sample value for each Metadata key, and get every possible condition by merging the strings
+                // Get every sample value for each key, and get every possible condition by merging the strings
                 ArrayList<String> possibleConditions = new ArrayList<String>();
-                for (String key : metakeys) {
+                for (String key : allkeys) {
                     for (ExperimentSample es : experiment.getExperimentSamples()) {
                         String value = DesignUtils.getMetadata(es, key);
                         possibleConditions.add(key+value);
                     }
                 }
-
-                // Check if each condition exists
+                // Check if each conditions in the comparison string exist in the Condition column
                 for (String condi : listConditionsInComparisonString){
                     Boolean exist = false;
                     for (int i=0;i<possibleConditions.size();i++){
@@ -79,30 +118,27 @@ public class DESeq2Checker {
                     }
                 }
             }
-        }
-
-        // Check if the column Condition is missing for the experiment
-        if (!getExperimentSampleAllMetadataKeys(experiment)
-                .contains(CONDITION_KEY)
-                && !getAllSamplesMetadataKeys(experiment.getDesign()).contains(CONDITION_KEY)) {
-            return error("Condition column missing for experiment: "
-                    + experiment.getName(), throwsException);
-        }
-
-        // Check if the column RepTechGroup is missing for the experiment
-        if (!getExperimentSampleAllMetadataKeys(experiment)
-                .contains(REP_TECH_GROUP_KEY)
-                && !getAllSamplesMetadataKeys(experiment.getDesign()).contains(REP_TECH_GROUP_KEY)) {
-            return error("RepTechGroup column missing for experiment: "
-                    + experiment.getName(), throwsException);
+            // Check if the name of each comparison is unique
+            Set<String> set=new HashSet<>();
+            Set<String> duplicateElements=new HashSet<>();
+            for (String element : comparisionNames) {
+                if(!set.add(element)){
+                    duplicateElements.add(element);
+                }
+            }
+            if (!duplicateElements.isEmpty()){
+                return error("Error in " + experiment.getName() + " experiment, there is one or more " +
+                        "duplicates in the comparison string names", throwsException);
+            }
         }
 
         /* Check if there is no numeric character at the begin of a row in the column Condition
         for a complex design model */
-        if (getExperimentSampleAllMetadataKeys(experiment).contains(CONDITION_KEY)) {
+        if (getExperimentSampleAllMetadataKeys(experiment).contains(CONDITION_KEY) ||
+                getAllSamplesMetadataKeys(experiment.getDesign()).contains(CONDITION_KEY)) {
             for (ExperimentSample es : experiment.getExperimentSamples()) {
                 String s = DesignUtils.getMetadata(es, CONDITION_KEY);
-                // Condition column contains an invalid numeric character as first character
+                // If a condition column contains an invalid numeric character as first character
                 if ( Character.isDigit(s.charAt(0)) && emd.getComparisons() != null){
                     return error("One or more Condition rows start with a numeric character : "
                             + experiment.getName(), throwsException);
@@ -110,8 +146,9 @@ public class DESeq2Checker {
             }
         }
 
-        // Check if there is no "-" in the column Condition when the contrast mode is activate
-        if (getExperimentSampleAllMetadataKeys(experiment).contains(CONDITION_KEY)) {
+        /* Check if there is no "-" in the column Condition when the contrast mode is activate */
+        if (getExperimentSampleAllMetadataKeys(experiment).contains(CONDITION_KEY) ||
+                getAllSamplesMetadataKeys(experiment.getDesign()).contains(CONDITION_KEY)) {
             for (ExperimentSample es : experiment.getExperimentSamples()) {
                 String s = DesignUtils.getMetadata(es, CONDITION_KEY);
                 if (s.contains("-") && emd.isContrast()){
@@ -121,25 +158,25 @@ public class DESeq2Checker {
             }
         }
 
-        // Verify consistency between the values in the columns Reference and Condition for non complex mode
+        /* Verify consistency between the values in the columns Reference and Condition for non complex mode */
         // If mode is not complex
         if (!emd.containsComparisons()) {
             // If Exp.exp1.Condition and Exp.exp1.Reference exist
-            if (getExperimentSampleAllMetadataKeys(experiment).contains(REFERENCE_KEY) && getExperimentSampleAllMetadataKeys(experiment).contains(CONDITION_KEY)){
+            if (getExperimentSampleAllMetadataKeys(experiment).contains(REFERENCE_KEY)
+                    && getExperimentSampleAllMetadataKeys(experiment).contains(CONDITION_KEY)){
                 Map<String, String> lhm = new LinkedHashMap<>();
-                // Get all values of the Condition and Reference columns
+                // Get all samples values of the Condition and Reference columns
                 for (ExperimentSample es : experiment.getExperimentSamples()) {
                     String conditions = DesignUtils.getMetadata(es, CONDITION_KEY);
                     String references = DesignUtils.getMetadata(es, REFERENCE_KEY);
                     // If one condition is associated with more than one reference, error
-                    ArrayList<String> possibleConditionsReferences = new ArrayList<String>();
+                    List<String> possibleConditionsReferences = new ArrayList<>();
                     possibleConditionsReferences.add(conditions+references);
-                    // if condition already in dictionnary check if value is equals to reference and if not error
                     for (String key : lhm.keySet()) {
                         String value = lhm.get(key);
                         if (key.equals(conditions) && !value.equals(references)){
-                            return error("There is an inconsistency between the conditions and the references : "
-                                    + experiment.getName(), throwsException);
+                            return error("There is an inconsistency between the conditions " +
+                                    "and the references : " + experiment.getName(), throwsException);
                         }
                     }
                     lhm.put(conditions,references);
@@ -147,8 +184,27 @@ public class DESeq2Checker {
             }
         }
 
-        // Verify consistency between "Exp.exp1" in the header of the file and the ExperimentSample columns name
-        // TO DO
+        /* Check if there is no combination column-name that is equal to another column-name or to a column name */
+        // Get every sample value for each key, and get every possible combination by merging the strings
+        ArrayList<String> possibleCombination = new ArrayList<String>();
+        for (String key : allkeys) {
+            for (ExperimentSample es : experiment.getExperimentSamples()) {
+                String value = DesignUtils.getMetadata(es, key);
+                String possibleCombinationvalueone = key+value;
+                String possibleCombinationvaluetwo = value+key;
+                possibleCombination.add(possibleCombinationvalueone);
+                possibleCombination.add(possibleCombinationvaluetwo);
+            }
+        }
+        // Error if a possible combination equals to a key
+        for (String key : allkeys){
+            for (String value : possibleCombination){
+                if ( value.equals(key) ){
+                    return error("There is a combination column-data that equals to a column name" +
+                                experiment.getName(), throwsException);
+                }
+            }
+        }
 
         return true;
     }
--- src/test/java/fr/ens/biologie/genomique/eoulsan/modules/diffana/DESeq2CheckerTest.java
+++ src/test/java/fr/ens/biologie/genomique/eoulsan/modules/diffana/DESeq2CheckerTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static fr.ens.biologie.genomique.eoulsan.design.DesignFactory.createEmptyDesign;
 import static fr.ens.biologie.genomique.eoulsan.modules.diffana.DESeq2Checker.checkExperimentDesign;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
 
 /**
@@ -20,38 +22,205 @@ public class DESeq2CheckerTest {
     @Test
     public void testCheckExperimentDesign() throws EoulsanException {
 
-        // Create a test experiment
-        Design d = DesignFactory.createEmptyDesign();
-        Experiment e1 = d.addExperiment("exp1");
+        /* Create test experiments */
 
-        e1.getMetadata().setSkip(false);
-        e1.getMetadata().setContrast(true);
-        e1.getMetadata().setBuildContrast(true);
-        e1.getMetadata().setModel("~Lignee+Culture+Lignee:Culture");
-        //e1.getMetadata().setComparisons("HT29:LigneeHT29%CultureD3_vs_LigneeHT29%CultureD2");
-        //e1.getMetadata().setComparisons("HT29:LigneeHT29%CultureD3_vs_LigneeHT29%CultureD2;LS513:LigneeLS513%CultureD3_vs_LigneeLS513%CultureD2;D3:CultureD3%LigneeHT29_vs_CultureD3%LigneeLS513");
 
-        List<String> h = Arrays.asList("RepTechGroup");
-        e1.addSample(addSample(d, h, "S1", "296-a"));
-        e1.addSample(addSample(d, h, "S2", "298-c"));
-        e1.addSample(addSample(d, h, "S3", "294-b"));
+        /* Test if the column Condition is missing for the experiment */
 
-        addExperimentSample(d,Arrays.asList("Condition"),"S1","exp1","KO");
-        addExperimentSample(d,Arrays.asList("Condition"),"S2","exp1","WT");
-        addExperimentSample(d,Arrays.asList("Condition"),"S3","exp1","WT");
-        addExperimentSample(d,Arrays.asList("Reference"),"S1","exp1","false");
-        addExperimentSample(d,Arrays.asList("Reference"),"S2","exp1","true");
-        addExperimentSample(d,Arrays.asList("Reference"),"S3","exp1","true");
-        addExperimentSample(d,Arrays.asList("Lignee"),"S1","exp1","HT29");
-        addExperimentSample(d,Arrays.asList("Lignee"),"S2","exp1","LS513");
-        addExperimentSample(d,Arrays.asList("Lignee"),"S3","exp1","HT29");
-        addExperimentSample(d,Arrays.asList("Culture"),"S1","exp1","D3");
-        addExperimentSample(d,Arrays.asList("Culture"),"S2","exp1","D2");
-        addExperimentSample(d,Arrays.asList("Culture"),"S3","exp1","D2");
+        // Working example
+        Design testCondiColumn = createEmptyDesign();
+        Experiment e1 = testCondiColumn.addExperiment("exp1");
+        List<String> h1 = asList("RepTechGroup", "Condition");
+        e1.addSample(addSample(testCondiColumn, h1, "S1", "296-a","KO"));
+        assertTrue("Condition column missing for experiment",
+                checkExperimentDesign(e1, false));
+        // Example containing a mistake: the column Condition is missing
+        Design testCondiColumnBis = createEmptyDesign();
+        Experiment e1bis = testCondiColumnBis.addExperiment("exp1");
+        List<String> h1bis = asList("RepTechGroup");
+        e1bis.addSample(addSample(testCondiColumnBis, h1bis, "S1", "296-a"));
+        assertFalse("Condition column missing for experiment",
+                checkExperimentDesign(e1bis, false));
 
-        // Run the checkExperimentDesign function for the experiment e1
-        assertTrue(checkExperimentDesign(e1, false));
 
+        /* Test if the column RepTechGroup is missing for the experiment */
+
+        // Working example
+        Design testReptechgroupColumn = createEmptyDesign();
+        Experiment e2 = testReptechgroupColumn.addExperiment("exp1");
+        List<String> h2 = asList("RepTechGroup", "Condition");
+        e2.addSample(addSample(testReptechgroupColumn, h2, "S1", "296-a","KO"));
+        assertTrue("RepTechGroup column missing for experiment",
+                checkExperimentDesign(e2, false));
+        // Example containing a mistake: the column RepTechGroup is missing
+        Design testReptechgroupColumnBis = createEmptyDesign();
+        Experiment e2bis = testReptechgroupColumnBis.addExperiment("exp1");
+        List<String> h2bis = asList("Condition");
+        e2bis.addSample(addSample(testReptechgroupColumnBis, h2bis, "S1", "KO"));
+        assertFalse("RepTechGroup column missing for experiment",
+                checkExperimentDesign(e2bis, false));
+
+
+        /* Test if the comparison string is correct */
+
+        // Working example
+        Design testCompString = createEmptyDesign();
+        Experiment e3 = testCompString.addExperiment("exp1");
+        e3.getMetadata().setComparisons("HT29:LigneeHT29%CultureD3_vs_LigneeHT29%CultureD2;" +
+                "D3:CultureD3%Etatvieille_vs_CultureD3%Etatjeune");
+        List<String> h3 = asList("RepTechGroup", "Condition");
+        e3.addSample(addSample(testCompString, h3, "S1", "296-a","KO"));
+        e3.addSample(addSample(testCompString, h3, "S2", "298-c","KO"));
+        addExperimentSample(testCompString, asList("Lignee"),"S1","exp1","HT29");
+        addExperimentSample(testCompString, asList("Lignee"),"S2","exp1","LS513");
+        addExperimentSample(testCompString, asList("Culture"),"S1","exp1","D3");
+        addExperimentSample(testCompString, asList("Culture"),"S2","exp1","D2");
+        addExperimentSample(testCompString, asList("Etat"),"S1","exp1","vieille");
+        addExperimentSample(testCompString, asList("Etat"),"S2","exp1","jeune");
+        assertTrue("The comparison string is not correct", checkExperimentDesign(e3, false));
+        // Example containing a mistake
+        Design testCompStringBis = createEmptyDesign();
+        Experiment e3bis = testCompStringBis.addExperiment("exp1");
+        e3bis.getMetadata().setComparisons("HT29:LigneeHT29%CultureD3_vs_LigneeHT29%CultureD1");
+        List<String> h3bis = asList("RepTechGroup", "Condition");
+        e3bis.addSample(addSample(testCompStringBis, h3bis, "S1", "296-a","KO"));
+        e3bis.addSample(addSample(testCompStringBis, h3bis, "S2", "298-c","KO"));
+        addExperimentSample(testCompStringBis, asList("Lignee"),"S1","exp1","HT29");
+        addExperimentSample(testCompStringBis, asList("Lignee"),"S2","exp1","LS513");
+        addExperimentSample(testCompStringBis, asList("Culture"),"S1","exp1","D3");
+        addExperimentSample(testCompStringBis, asList("Culture"),"S2","exp1","D2");
+        assertFalse("The comparison string is not correct", checkExperimentDesign(e3bis, false));
+
+
+        /* Test if there is no "-" in the column Condition when the contrast mode is activate */
+
+        // Working example
+        Design testDashInConditon = createEmptyDesign();
+        Experiment e4 = testDashInConditon.addExperiment("exp1");
+        e4.getMetadata().setContrast(true);
+        List<String> h4 = asList("RepTechGroup", "Condition");
+        e4.addSample(addSample(testDashInConditon, h4, "S1", "296-a","KO"));
+        assertTrue("There is a dash in the condition column while the contrast mode is activate",
+                checkExperimentDesign(e4, false));
+        // Example containing a mistake: "KO-" as Condition for S1 when the contrast mode is activate
+        Design testDashInConditonBis = createEmptyDesign();
+        Experiment e4bis = testDashInConditonBis.addExperiment("exp1");
+        e4bis.getMetadata().setContrast(true);
+        List<String> h4bis = asList("RepTechGroup", "Condition");
+        e4bis.addSample(addSample(testDashInConditonBis, h4bis, "S1", "296-a","KO-"));
+        assertFalse("There is a dash in the condition column while the contrast mode is activate",
+                checkExperimentDesign(e4bis, false));
+
+
+        /* Test if there is no numeric character at the begin of a row in the column Condition
+        for a complex design model */
+
+        // Working example
+        Design testCondiIfNumber = createEmptyDesign();
+        Experiment e5 = testCondiIfNumber.addExperiment("exp1");
+        e5.getMetadata().setComparisons("HT29:LigneeHT29%CultureD3_vs_LigneeHT29%CultureD2");
+        List<String> h5 = asList("RepTechGroup", "Condition");
+        e5.addSample(addSample(testCondiIfNumber, h5, "S1", "296-a","KO"));
+        e5.addSample(addSample(testCondiIfNumber, h5, "S2", "298-c","KO"));
+        addExperimentSample(testCondiIfNumber, asList("Lignee"),"S1","exp1","HT29");
+        addExperimentSample(testCondiIfNumber, asList("Lignee"),"S2","exp1","LS513");
+        addExperimentSample(testCondiIfNumber, asList("Culture"),"S1","exp1","D3");
+        addExperimentSample(testCondiIfNumber, asList("Culture"),"S2","exp1","D2");
+        assertTrue("There is a numeric character at the begin of a row in the column Condition " +
+                "for a complex design model", checkExperimentDesign(e5, false));
+        // Example containing a mistake: "1KO" as Condition for S1 with a complex model
+        Design testCondiIfNumberBis = createEmptyDesign();
+        Experiment e5bis = testCondiIfNumberBis.addExperiment("exp1");
+        e5bis.getMetadata().setComparisons("HT29:LigneeHT29%CultureD3_vs_LigneeHT29%CultureD2");
+        List<String> h5bis = asList("RepTechGroup", "Condition");
+        e5bis.addSample(addSample(testCondiIfNumberBis, h5bis, "S1", "296-a","1KO"));
+        e5bis.addSample(addSample(testCondiIfNumberBis, h5bis, "S2", "298-c","KO"));
+        addExperimentSample(testCondiIfNumberBis, asList("Lignee"),"S1","exp1","HT29");
+        addExperimentSample(testCondiIfNumberBis, asList("Lignee"),"S2","exp1","LS513");
+        addExperimentSample(testCondiIfNumberBis, asList("Culture"),"S1","exp1","D3");
+        addExperimentSample(testCondiIfNumberBis, asList("Culture"),"S2","exp1","D2");
+        assertFalse("There is a numeric character at the begin of a row in the column Condition " +
+                "for a complex design model", checkExperimentDesign(e5bis, false));
+
+
+        /* Test consistency between the values in the columns Reference and Condition for non complex mode */
+
+        // Working example
+        Design testConsistency = createEmptyDesign();
+        Experiment e6 = testConsistency.addExperiment("exp1");
+        List<String> h6 = asList("RepTechGroup");
+        e6.addSample(addSample(testConsistency, h6, "S1", "296-a"));
+        e6.addSample(addSample(testConsistency, h6, "S2", "298-c"));
+        e6.addSample(addSample(testConsistency, h6, "S3", "294-b"));
+        addExperimentSample(testConsistency, asList("Condition"),"S1","exp1","KO");
+        addExperimentSample(testConsistency, asList("Condition"),"S2","exp1","WT");
+        addExperimentSample(testConsistency, asList("Condition"),"S3","exp1","WT");
+        addExperimentSample(testConsistency, asList("Reference"),"S1","exp1","false");
+        addExperimentSample(testConsistency, asList("Reference"),"S2","exp1","true");
+        addExperimentSample(testConsistency, asList("Reference"),"S3","exp1","true");
+        assertTrue("There is inconstancy between the values in the columns Reference and Condition",
+                checkExperimentDesign(e6, false));
+        // Example containing a mistake: KO-false WT-true and KO-true
+        Design testConsistencyBis = createEmptyDesign();
+        Experiment e6bis = testConsistencyBis.addExperiment("exp1");
+        List<String> h6bis = asList("RepTechGroup");
+        e6bis.addSample(addSample(testConsistencyBis, h6bis, "S1", "296-a"));
+        e6bis.addSample(addSample(testConsistencyBis, h6bis, "S2", "298-c"));
+        e6bis.addSample(addSample(testConsistencyBis, h6bis, "S3", "294-b"));
+        addExperimentSample(testConsistencyBis, asList("Condition"),"S1","exp1","KO");
+        addExperimentSample(testConsistencyBis, asList("Condition"),"S2","exp1","WT");
+        addExperimentSample(testConsistencyBis, asList("Condition"),"S3","exp1","KO");
+        addExperimentSample(testConsistencyBis, asList("Reference"),"S1","exp1","false");
+        addExperimentSample(testConsistencyBis, asList("Reference"),"S2","exp1","true");
+        addExperimentSample(testConsistencyBis, asList("Reference"),"S3","exp1","true");
+        assertFalse("There is inconstancy between the values in the columns Reference and Condition",
+                checkExperimentDesign(e6bis, false));
+
+
+        /* Test to check if there is no combination column-data that equals to a column name */
+
+        // Working example
+        Design testCombinationUnique = createEmptyDesign();
+        Experiment e7 = testCombinationUnique.addExperiment("exp1");
+        List<String> h7 = asList("RepTechGroup", "Condition");
+        e7.addSample(addSample(testCombinationUnique, h7, "S1", "296-a","KO"));
+        e7.addSample(addSample(testCombinationUnique, h7, "S2", "298-c","KO"));
+        addExperimentSample(testCombinationUnique, asList("Toto"),"S1","exp1","D3");
+        addExperimentSample(testCombinationUnique, asList("Toto"),"S2","exp1","D2");
+        addExperimentSample(testCombinationUnique, asList("To"),"S1","exp1","ta");
+        addExperimentSample(testCombinationUnique, asList("To"),"S2","exp1","tu");
+        assertTrue("Combinations column-data are not unique",
+                checkExperimentDesign(e7, false));
+        // Example containing a mistake: column "Toto" == column "To"+ data "to"
+        Design testCombinationUniqueBis = createEmptyDesign();
+        Experiment e7bis = testCombinationUniqueBis.addExperiment("exp1");
+        List<String> h7bis = asList("RepTechGroup", "Condition");
+        e7bis.addSample(addSample(testCombinationUniqueBis, h7bis, "S1", "296-a","KO"));
+        e7bis.addSample(addSample(testCombinationUniqueBis, h7bis, "S2", "298-c","KO"));
+        addExperimentSample(testCombinationUniqueBis, asList("Toto"),"S1","exp1","D3");
+        addExperimentSample(testCombinationUniqueBis, asList("Toto"),"S2","exp1","D2");
+        addExperimentSample(testCombinationUniqueBis, asList("To"),"S1","exp1","to");
+        addExperimentSample(testCombinationUniqueBis, asList("To"),"S2","exp1","ta");
+        assertFalse("Combinations column-data are not unique",
+                checkExperimentDesign(e7bis, false));
+
+
+        /* Test if there is no empty cell in the experiment */
+
+        // Working example
+        Design testEmptyCell = createEmptyDesign();
+        Experiment e8 = testEmptyCell.addExperiment("exp1");
+        List<String> h8 = asList("RepTechGroup", "Condition");
+        e8.addSample(addSample(testEmptyCell, h8, "S1", "296-a","KO"));
+        e8.addSample(addSample(testEmptyCell, h8, "S2", "298-c","KO"));
+        assertTrue("There is empty cell in the experiment",checkExperimentDesign(e8, false));
+        // Example containing a mistake: empty cell in the Condition column for S1
+        Design testEmptyCellBis = createEmptyDesign();
+        Experiment e8bis = testEmptyCellBis.addExperiment("exp1");
+        List<String> h8bis = asList("RepTechGroup", "Condition");
+        e8bis.addSample(addSample(testEmptyCellBis, h8bis, "S1", "296-a"," "));
+        e8bis.addSample(addSample(testEmptyCellBis, h8bis, "S2", "298-c","KO"));
+        assertFalse("There is empty cell in the experiment",checkExperimentDesign(e8bis, false));
     }
 
     private static Sample addSample(Design d, List<String> header, String sampleId, String... values){
@@ -63,7 +232,8 @@ public class DESeq2CheckerTest {
         return s;
     }
 
-    private static ExperimentSample addExperimentSample(Design d, List<String> header, String sampleId, String experimentId, String... values){
+    private static ExperimentSample addExperimentSample(Design d, List<String> header, String sampleId,
+                                                        String experimentId, String... values){
         Sample s = d.getSample(sampleId);
         if (!d.containsExperiment(experimentId)) {
             d.addExperiment(experimentId);
