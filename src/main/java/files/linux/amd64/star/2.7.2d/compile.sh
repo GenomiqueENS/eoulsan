7692cfbd10a2011ee2407465f464f3bb1041319e
Laurent Jourdren <jourdren@biologie.ens.fr>
2019-10-08 10:43:39 +0200

    UPDATE: Replace STAR 2.7.2c by STAR 2.7.2d.

--- src/main/java/files/linux/amd64/star/2.7.2d/compile.sh
+++ src/main/java/files/linux/amd64/star/2.7.2d/compile.sh
@@ -3,7 +3,7 @@
 # This file contains the Docker instruction used to build the STAR executable 
 # for Eoulsan
 
-VERSION=2.7.2c
+VERSION=2.7.2d
 DOCKER_IMAGE="star:$VERSION"
 
 cat > Dockerfile << EOF
