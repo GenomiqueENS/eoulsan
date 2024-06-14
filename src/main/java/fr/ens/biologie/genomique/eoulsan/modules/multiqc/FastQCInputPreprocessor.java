3d31076d7e86eddcc35ff1dfd7f7b24de8a2f381
Laurent Jourdren <jourdren@biologie.ens.fr>
2024-06-14 11:09:21 +0200

    UPDATE: In FastQCInputPreprocessor, now rewrite the Filename entry of the fastqc_data.txt file in the FASTQC report file to ensure that samples for FastQC section in MultiQC have the same name as in the other sections.

--- src/main/java/fr/ens/biologie/genomique/eoulsan/modules/multiqc/FastQCInputPreprocessor.java
+++ src/main/java/fr/ens/biologie/genomique/eoulsan/modules/multiqc/FastQCInputPreprocessor.java
@@ -1,11 +1,25 @@
 package fr.ens.biologie.genomique.eoulsan.modules.multiqc;
 
+import static java.util.Objects.requireNonNull;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import fr.ens.biologie.genomique.eoulsan.core.TaskContext;
 import fr.ens.biologie.genomique.eoulsan.data.Data;
 import fr.ens.biologie.genomique.eoulsan.data.DataFile;
+import fr.ens.biologie.genomique.eoulsan.data.DataFiles;
 import fr.ens.biologie.genomique.eoulsan.data.DataFormat;
 import fr.ens.biologie.genomique.eoulsan.data.DataFormats;
 
@@ -40,17 +54,98 @@ public class FastQCInputPreprocessor implements InputPreprocessor {
     for (int i = 0; i < fileCount; i++) {
 
       // Define symbolic link path
-      DataFile symlink = new DataFile(multiQCInputDirectory,
+      DataFile outputFile = new DataFile(multiQCInputDirectory,
           name + (fileCount <= 1 ? "" : "_read" + i) + "_fastqc.zip");
 
       // Define target log file
       DataFile fastQCReportFile = data.getDataFile(i);
 
       // Create symbolic link
-      if (fastQCReportFile.exists()) {
-        fastQCReportFile.symlink(symlink);
+      if (!fastQCReportFile.exists()) {
+        DataFiles.copy(fastQCReportFile, outputFile);
       }
+
+      // Update report with the data name
+      updateFastqcResultZip(outputFile.toFile().toPath(), name);
     }
 
   }
+
+  /**
+   * Update fastqc_data.txt file inside the zip with the name of sample.
+   * @param reportfile the zip file
+   * @param name the name of the sample
+   * @throws IOException if an error occurs while updating the file
+   */
+  private static void updateFastqcResultZip(Path reportfile, String name)
+      throws IOException {
+
+    requireNonNull(reportfile);
+    requireNonNull(name);
+
+    String pathInZip = dataPathInZip(reportfile);
+
+    try (FileSystem fs = FileSystems.newFileSystem(reportfile, null)) {
+      Path source = fs.getPath(pathInZip);
+      Path temp = fs.getPath("/___fastqc_data___.txt");
+      Files.move(source, temp);
+      updateReport(temp, source, name);
+      Files.delete(temp);
+    }
+  }
+
+  /**
+   * Look for the path of fastqc_data.txt file inside the zip file
+   * @param reportfile the zip file
+   * @return the path of the fastqc_data.txt file inside the zip file
+   * @throws IOException if an error occurs while opening the zip file
+   */
+  private static String dataPathInZip(Path reportfile) throws IOException {
+
+    try (ZipInputStream zipIn =
+        new ZipInputStream(new FileInputStream(reportfile.toFile()))) {
+      ZipEntry entry = zipIn.getNextEntry();
+
+      // iterates over entries in the zip file
+      while (entry != null) {
+        if (entry.getName().endsWith("/fastqc_data.txt")) {
+          return entry.getName();
+        }
+
+        zipIn.closeEntry();
+        entry = zipIn.getNextEntry();
+      }
+    }
+
+    throw new IOException("No fastqc_data.txt found in " + reportfile);
+  }
+
+  /**
+   * Update the fastqc_data.txt file.
+   * @param src input file
+   * @param dst output file
+   * @param name name of the report
+   * @throws IOException if an error occurs while updating the file
+   */
+  private static void updateReport(Path src, Path dst, String name)
+      throws IOException {
+    try (
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(Files.newInputStream(src)));
+        BufferedWriter bw = new BufferedWriter(
+            new OutputStreamWriter(Files.newOutputStream(dst)))) {
+
+      String line;
+      while ((line = br.readLine()) != null) {
+
+        if (line.startsWith("Filename\t")) {
+          line = "Filename\t" + name + ".fastq.gz";
+        }
+
+        bw.write(line);
+        bw.newLine();
+      }
+    }
+  }
+
 }
